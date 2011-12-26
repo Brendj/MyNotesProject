b3c360338b7ba388826c9ad9122652906c48c5bd
kadyrov <kadyrov@example.com>
2011-12-26 12:04:25 +0000

    Правильно выбираются номера договоров. Именен резульататов если пользователь не выбрал ни одной организации.

--- src/core/java/ru/axetta/ecafe/processor/core/report/EnterEventReport.java
+++ src/core/java/ru/axetta/ecafe/processor/core/report/EnterEventReport.java
@@ -38,61 +38,30 @@ public class EnterEventReport extends BasicReport {
 
         public EnterEventReport build(Session session, Date startDate, Date endDate, List<Long> idOfOrgList)
                 throws Exception {
-            /*
-            * Выполняем запрос:
-            * SELECT
-              cf_enterevents.idofenterevent,
-              cf_enterevents.idoforg,
-              cf_orgs.officialname,
-              cf_enterevents.entername,
-              cf_enterevents.turnstileaddr,
-              cf_enterevents.passdirection,
-              cf_enterevents.eventcode,
-              cf_enterevents.docserialnum,
-              concat (cf_persons.firstname, ' ' ,
-              cf_persons.surname, ' ',
-              cf_persons.secondname ) as personFullName,
-              cf_enterevents.evtdatetime
-            FROM
-              public.cf_enterevents,
-              public.cf_clients,
-              public.cf_persons,
-              public.cf_orgs
-            WHERE
-              cf_enterevents.idofclient = cf_clients.idofclient AND
-              cf_enterevents.idoforg = cf_orgs.idoforg AND
-              cf_clients.idofperson = cf_persons.idofperson;
-            * */
+
             Date generateTime = new Date();
             List<EnterEventItem> enterEventItems = new LinkedList<EnterEventItem>();
-            /* String preparedQuery = "SELECT e.idofenterevent, e.idoforg, o.officialname,"
-                    + "e.entername, e.turnstileaddr,  e.passdirection,  e.eventcode,   e.docserialnum,"
-                    + " p.firstname as personFullName, e.evtdatetime "
-                    + "FROM CF_EnterEvents e,  CF_Clients c,  CF_Persons p, CF_Orgs o"
-                    + " WHERE e.idofclient = c.idofclient AND e.idoforg = o.idoforg AND c.idofperson = p.idofperson"
-                     + "  and e.evtdatetime >= :fromCreatedDate and e.evtdatetime <= :toCreatedDate";*/
-             /* Для этого необходимо изменить класс EnterEvent со связями */
-            Criteria criteria = session.createCriteria(EnterEvent.class);
-            if(idOfOrgList.isEmpty()) criteria.createAlias("org","o").add(in("o.idOfOrg", new Long[] {new Long(0)} ));
-            else criteria.createAlias("org","o").add(in("o.idOfOrg",idOfOrgList));
-            criteria.add((between("evtDateTime", startDate, endDate)));
-            List<EnterEvent> enterEventList = criteria.list();
-            for(EnterEvent enterEvent: enterEventList){
-                long id=enterEvent.getCompositeIdOfEnterEvent().getIdOfEnterEvent();
-                enterEvent.getOrg().getOfficialName();
-                StringBuilder stringBuilder = new StringBuilder();
-                String docId = null;
-                if (enterEvent.getClient() != null){
-                    stringBuilder.append(enterEvent.getClient().getPerson().getFirstName());
-                    stringBuilder.append(" ");
-                    stringBuilder.append(enterEvent.getClient().getPerson().getSecondName());
-                    docId=enterEvent.getClient().getPerson().getIdDocument();
-                }else{
-                    stringBuilder.append(" ");
-                    docId="";
-                }
-                String passdirection, color="black", personFullName=stringBuilder.toString();
-                switch (enterEvent.getPassDirection()){
+            if(!idOfOrgList.isEmpty()){
+                Criteria criteria = session.createCriteria(EnterEvent.class);
+                criteria.createAlias("org","o").add(in("o.idOfOrg",idOfOrgList));
+                criteria.add((between("evtDateTime", startDate, endDate)));
+                List<EnterEvent> enterEventList = criteria.list();
+                for(EnterEvent enterEvent: enterEventList){
+                    long id=enterEvent.getCompositeIdOfEnterEvent().getIdOfEnterEvent();
+                    enterEvent.getOrg().getOfficialName();
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String docId = null;
+                    if (enterEvent.getClient() != null){
+                        stringBuilder.append(enterEvent.getClient().getPerson().getFirstName());
+                        stringBuilder.append(" ");
+                        stringBuilder.append(enterEvent.getClient().getPerson().getSecondName());
+                        docId=String.valueOf(enterEvent.getClient().getContractId());
+                    }else{
+                        stringBuilder.append(" ");
+                        docId="";
+                    }
+                    String passdirection, color="black", personFullName=stringBuilder.toString();
+                    switch (enterEvent.getPassDirection()){
                         case 0: passdirection="вход"; color="green"; break;
                         case 1: passdirection="выход"; color="red"; break;
                         case 2: passdirection="проход запрещен"; break;
@@ -100,54 +69,15 @@ public class EnterEventReport extends BasicReport {
                         case 4: passdirection="событие без прохода"; break;
                         case 5: passdirection="отказ от прохода"; break;
                         default: passdirection="Ошибка обратитесь администратору";
-                }
-                 //if(enterEvent.getOrg().getIdOfOrg())
-                EnterEventItem  enterEventItem = new EnterEventItem(enterEvent.getCompositeIdOfEnterEvent().getIdOfEnterEvent(),
+                    }
+                    EnterEventItem  enterEventItem = new EnterEventItem(enterEvent.getCompositeIdOfEnterEvent().getIdOfEnterEvent(),
                             enterEvent.getOrg().getIdOfOrg(), enterEvent.getOrg().getOfficialName(),
                             enterEvent.getEnterName(), enterEvent.getTurnstileAddr(), passdirection,
                             enterEvent.getEventCode(),docId,
                             personFullName, enterEvent.getEvtDateTime(), color);
-                enterEventItems.add(enterEventItem);
-            }
-             /*
-            List resultList = null;
-            Query query = session.createSQLQuery(preparedQuery);
-            long startDateLong = startDate.getTime();
-            long endDateLong = endDate.getTime();
-            query.setLong("fromCreatedDate", startDateLong);
-            query.setLong("toCreatedDate", endDateLong);
-            resultList = query.list();
-            for (Object result : resultList) {
-                Object[] sale = (Object[]) result;
-                 Long idofenterevent = ((BigInteger) sale[0]).longValue();   //№ Идентификатор
-                 Long idoforg = ((BigInteger) sale[1]).longValue();  // Номер учреждения
-                 String officialname = (String) sale[2]; // Название учреждения
-                 String entername = (String) sale[3]; // Наименование входа
-                 String turnstileaddr = (String) sale[4];// Адрес турникета
-                Integer passdirectionInt = (Integer) sale[5]; // Направление прохода
-                 Integer eventCode = (Integer) sale[6]; // Код события
-                 Integer docserialnum = (Integer) sale[7];// Номер договора
-                 String personFullName = (String) sale[8];// Фамилия и Имя учащегося
-                Date evtdatetime = new Date(((BigInteger) sale[9]).longValue());// Дата события
-
-                //переопределение направление турникета, возможны ошибки с кодировкой.
-                String passdirection, color="black";
-                switch (passdirectionInt){
-                    case 0: passdirection="вход"; color="green"; break;
-                    case 1: passdirection="выход"; color="red"; break;
-                    case 2: passdirection="проход запрещен"; break;
-                    case 3: passdirection="взлом турникета"; break;
-                    case 4: passdirection="событие без прохода"; break;
-                    case 5: passdirection="отказ от прохода"; break;
-                    default: passdirection="Ошибка обратитесь администратору";
+                    enterEventItems.add(enterEventItem);
                 }
-
-                EnterEventItem  enterEvent = new EnterEventItem(idofenterevent, idoforg, officialname, entername,
-                        turnstileaddr, passdirection, eventCode, docserialnum,
-                        personFullName, evtdatetime, color);
-                enterEventItems.add(enterEvent);
-            }   */
-
+            }
             return new EnterEventReport(generateTime, new Date().getTime() - generateTime.getTime(), enterEventItems);
         }
 
