f99afa739ee756cf5fc3cfea715a03fbcd87c1da
kadyrov <kadyrov@example.com>
2012-02-15 07:27:07 +0000

    не зацикливает запрос при удалении

--- processor/src/web/java/ru/axetta/ecafe/processor/web/ui/client/rule/RuleDeletePage.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/ui/client/rule/RuleDeletePage.java
@@ -27,28 +27,6 @@ import java.util.Set;
 public class RuleDeletePage extends BasicPage {
     public void removeRule(Session session, Long id) throws Exception {
         DiscountRule discountRule = (DiscountRule) session.get(DiscountRule.class, id);
-        Set<CategoryDiscount> categoryDiscountSet= discountRule.getCategoriesDiscounts();
-        List<Long> listIdOfCategoryDiscount = new LinkedList<Long>();
-        for(CategoryDiscount categoryDiscount: categoryDiscountSet){
-            listIdOfCategoryDiscount.add(categoryDiscount.getIdOfCategoryDiscount());
-        }
-        Criteria criteriaCategoryDiscount=session.createCriteria(CategoryDiscount.class);
-        List categoryDiscountList = criteriaCategoryDiscount.add(Restrictions.in("idOfCategoryDiscount",listIdOfCategoryDiscount)).list();
-        for(Object object: categoryDiscountList){
-            discountRule.getCategoriesDiscounts().remove((CategoryDiscount) object);
-        }
-        //if(!discountRule.getCategoriesDiscounts().isEmpty()) discountRule.getCategoriesDiscounts().removeAll(categoryDiscountSet);
-        Set<CategoryOrg> categoryOrgSet = discountRule.getCategoryOrgs();
-        List<Long> listIdOfCategoryOrg = new LinkedList<Long>();
-        for(CategoryOrg categoryOrg: categoryOrgSet){
-            listIdOfCategoryOrg.add(categoryOrg.getIdOfCategoryOrg());
-        }
-        Criteria criteriaCategoryOrg=session.createCriteria(CategoryOrg.class);
-        List categoryOrgList = criteriaCategoryDiscount.add(Restrictions.in("idOfCategoryOrg",listIdOfCategoryOrg)).list();
-        for(Object object: categoryOrgList){
-            discountRule.getCategoryOrgs().remove((CategoryOrg) object);
-        }
-        //if(!discountRule.getCategoryOrgs().isEmpty()) discountRule.getCategoryOrgs().removeAll(categoryOrgSet);
         session.delete(discountRule);
     }
 }
