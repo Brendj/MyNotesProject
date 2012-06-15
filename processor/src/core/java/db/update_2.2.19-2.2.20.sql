74ad2f1fe2ef333e8efe67ac83207e97cf652b8b
kadyrov <kadyrov@example.com>
2012-06-15 13:22:26 +0000

    Добавлены таблицы Конфликтов и Версий для распределенных объектов

--- processor/src/core/java/db/update_2.2.19-2.2.20.sql
+++ processor/src/core/java/db/update_2.2.19-2.2.20.sql
@@ -3,16 +3,18 @@ CREATE TABLE cf_do_version
 (
   idofdoobject bigserial,
   distributedobjectclassname character varying(64),
-  currentversion bigint
+  currentversion bigint,
+  CONSTRAINT cf_do_version_pk PRIMARY KEY (idofdoobject )
 );
 -- Таблица конфликтов для распределенных объектов
 CREATE TABLE cf_do_conflicts
 (
-  idofdocconflict bigserial,
+  idofdoconflict bigserial,
   distributedobjectclassname character varying(64),
   createconflictdate bigint,
   gversion_inc bigint,
   gversion_cur bigint,
   val_inc character varying(16548),
-  val_cur character varying(16548)
+  val_cur character varying(16548),
+  CONSTRAINT cf_do_conflicts_pk PRIMARY KEY (idofdoconflict )
 );
