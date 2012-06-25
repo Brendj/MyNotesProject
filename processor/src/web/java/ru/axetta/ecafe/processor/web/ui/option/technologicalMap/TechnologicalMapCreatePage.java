9bd2887b683790a58da08746b732b5dbaf8cd99e
kadyrov <kadyrov@example.com>
2012-06-25 13:45:15 +0000

    Дописано добавление новой технологической карты, пока реализовано без добавления продуктов.

--- processor/src/web/java/ru/axetta/ecafe/processor/web/ui/option/technologicalMap/TechnologicalMapCreatePage.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/ui/option/technologicalMap/TechnologicalMapCreatePage.java
@@ -7,16 +7,22 @@ package ru.axetta.ecafe.processor.web.ui.option.technologicalMap;
 import ru.axetta.ecafe.processor.core.RuntimeContext;
 import ru.axetta.ecafe.processor.core.persistence.Products;
 import ru.axetta.ecafe.processor.core.persistence.TechnologicalMap;
+import ru.axetta.ecafe.processor.core.persistence.utils.DAOService;
 import ru.axetta.ecafe.processor.core.utils.HibernateUtils;
 import ru.axetta.ecafe.processor.web.ui.BasicWorkspacePage;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -26,10 +32,17 @@ import java.util.logging.Logger;
  * Time: 14:36
  * To change this template use File | Settings | File Templates.
  */
+@Component
 public class TechnologicalMapCreatePage extends BasicWorkspacePage {
 
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TechnologicalMapCreatePage.class);
     private TechnologicalMap technologicalMap = new TechnologicalMap();
+    private List<Products> productsList;
+
+    @Override
+    public void onShow() throws Exception {
+        technologicalMap.setProducts(new ArrayList<Products>());
+    }
 
     public TechnologicalMapCreatePage() {
         technologicalMap.setProducts(new ArrayList<Products>());
@@ -43,33 +56,14 @@ public class TechnologicalMapCreatePage extends BasicWorkspacePage {
         return technologicalMap;
     }
 
-    public void createTechnologicalMap(Session session) throws Exception {
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        RuntimeContext runtimeContext = null;
-        Session persistenceSession = null;
-        Transaction persistenceTransaction = null;
-        try {
-            runtimeContext = RuntimeContext.getInstance();
-            persistenceSession = runtimeContext.createPersistenceSession();
-            persistenceTransaction = persistenceSession.beginTransaction();
-
-            persistenceSession.save(technologicalMap);
-
-            persistenceTransaction.commit();
-            technologicalMap.setIdOfTechnologicalMap(null);
-            persistenceTransaction = null;
-            facesContext.addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Новая технологическая карта создана успешно.",
-                            null));
-        } catch (Exception e) {
-            logger.error("Failed create product.", e);
-            facesContext.addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ошибка при создании новой технологической карты.",
-                            null));
-        } finally {
-            HibernateUtils.rollback(persistenceTransaction, logger);
-            HibernateUtils.close(persistenceSession, logger);
-
+    @Transactional
+    public void createTechnologicalMap() {
+        try{
+            DAOService.getInstance().persistEntity(technologicalMap);
+            printMessage("Новая технологическая карта создана успешно.");
+        } catch (Exception e){
+            printError("Ошибка при создании новой технологической карты.");
+            logger.error("Error by create Technological Map.", e);
         }
     }
 
