78420ce6b399b88ded7fdddba4027aa639f24594
kadyrov <kadyrov@example.com>
2012-06-25 13:47:35 +0000

    Добавлены методы по работе с таблицей версии распеределнных объектов и изменены методв по работе с Распередленными объектами.

--- processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/utils/DAOService.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/utils/DAOService.java
@@ -10,6 +10,7 @@ import ru.axetta.ecafe.processor.core.persistence.Org;
 import ru.axetta.ecafe.processor.core.persistence.TransactionJournal;
 import ru.axetta.ecafe.processor.core.persistence.User;
 import ru.axetta.ecafe.processor.core.persistence.distributedobjects.DOConflict;
+import ru.axetta.ecafe.processor.core.persistence.distributedobjects.DOVersion;
 import ru.axetta.ecafe.processor.core.persistence.distributedobjects.DistributedObject;
 import ru.axetta.ecafe.processor.core.persistence.distributedobjects.ProductGuide;
 
@@ -79,23 +80,76 @@ public class DAOService {
     }
 
     @Transactional
+    public DOVersion updateVersionByDistributedObjects(String name) {
+        TypedQuery<DOVersion> query = em.createQuery("from DOVersion where UPPER(distributedObjectClassName)=:distributedObjectClassName",DOVersion.class);
+        query.setParameter("distributedObjectClassName",name.toUpperCase());
+        List<DOVersion> doVersionList = query.getResultList();
+        DOVersion doVersion = null;
+        if(doVersionList.size()==0) {
+            doVersion = new DOVersion();
+            doVersion.setCurrentVersion(0);
+        } else {
+            doVersion = doVersionList.get(0);
+            doVersion.setCurrentVersion(doVersionList.get(0).getCurrentVersion()+1);
+        }
+        doVersion.setDistributedObjectClassName(name);
+        return em.merge(doVersion);
+    }
+
+    @Transactional
+    public void saveVersionByDistributedObjects(String name, long v) {
+        TypedQuery<DOVersion> query = em.createQuery("from DOVersion where UPPER(distributedObjectClassName)=:distributedObjectClassName",DOVersion.class);
+        query.setParameter("distributedObjectClassName",name.toUpperCase());
+        List<DOVersion> doVersionList = query.getResultList();
+        DOVersion doVersion = null;
+        if(doVersionList.size()==0) {
+            doVersion = new DOVersion();
+        } else {
+            doVersion = doVersionList.get(0);
+        }
+        doVersion.setDistributedObjectClassName(name);
+        doVersion.setCurrentVersion(v);
+        em.persist(doVersion);
+    }
+
+    @Transactional
+    public DistributedObject getDistributedObject(DistributedObject distributedObject) {
+        return em.find(distributedObject.getClass(), distributedObject.getGlobalId());
+    }
+
+    @Transactional
     public Long getDistributedObjectVersion(DistributedObject distributedObject) {
-        distributedObject = em.find(distributedObject.getClass(), distributedObject.getGlobalId());
-        return distributedObject.getGlobalVersion();
+        DistributedObject currDo = em.find(distributedObject.getClass(), distributedObject.getGlobalId());
+        return currDo.getGlobalVersion();
     }
 
     @Transactional
     public DistributedObject createDistributedObject(DistributedObject distributedObject){
         distributedObject.setCreateTime(new Date());
+        TypedQuery<DOVersion> query = em.createQuery("from DOVersion where UPPER(distributedObjectClassName)=:distributedObjectClassName",DOVersion.class);
+        query.setParameter("distributedObjectClassName",distributedObject.getClass().getSimpleName().toUpperCase());
+        List<DOVersion> doVersionList = query.getResultList();
+        if(doVersionList.size()==0) {
+            distributedObject.setGlobalVersion(0L);
+        } else {
+            distributedObject.setGlobalVersion(doVersionList.get(0).getCurrentVersion()-1);
+        }
         return em.merge(distributedObject);
     }
 
     @Transactional
     public DistributedObject mergeDistributedObject(DistributedObject distributedObject, Long globalVersion){
-        Query q = em.createQuery("update " + distributedObject.getClass().getSimpleName() + " set globalVersion=:globalVersion");
+        Query q = em.createQuery("update " + distributedObject.getClass().getSimpleName() + " set globalVersion=:globalVersion, editTime=:editTime where globalId = :globalId ");
         q.setParameter("globalVersion", globalVersion);
+        q.setParameter("editTime", new Date());
+        q.setParameter("globalId", distributedObject.getGlobalId());
         q.executeUpdate();
-        return em.find(distributedObject.getClass(),distributedObject.getGlobalId());
+        return em.merge(distributedObject);
+    }
+
+    @Transactional
+    public void persistEntity(Object entity) throws Exception{
+        em.persist(entity);
     }
 
     @Transactional
