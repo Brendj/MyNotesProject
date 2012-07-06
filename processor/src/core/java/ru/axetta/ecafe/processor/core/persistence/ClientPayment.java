d1eefb27d6fb37182aecad03e7b08cc3e01106e4
yashin <yashin@example.com>
2012-07-06 14:49:15 +0000

    Добавлен еще один конструктор в класс ClientPayment

--- processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/ClientPayment.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/ClientPayment.java
@@ -77,6 +77,18 @@ public class ClientPayment {
         this.clientPaymentOrder = clientPaymentOrder;
     }
 
+    public ClientPayment(Long contragentSum, AccountTransaction transaction, ClientPaymentOrder clientPaymentOrder, Date createTime, String addIdOfPayment) {
+        this.transaction = transaction;
+        this.paymentMethod = clientPaymentOrder.getPaymentMethod();
+        this.paySum = contragentSum;
+        this.payType = CLIENT_TO_ACCOUNT_PAYMENT;
+        this.createTime = createTime;
+        this.idOfPayment = clientPaymentOrder.getIdOfPayment();
+        this.contragent = clientPaymentOrder.getContragent();
+        this.clientPaymentOrder = clientPaymentOrder;
+        this.addIdOfPayment = addIdOfPayment;
+    }
+
     public Long getIdOfClientPayment() {
         return idOfClientPayment;
     }
