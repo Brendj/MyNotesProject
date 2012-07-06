b3e69f6f5f1e1e4c7ae085c6f5cae552a6fe016f
kadyrov <kadyrov@example.com>
2012-07-06 14:15:25 +0000

    Распределена логика работы с диалоговыми окнами выбора несколких продуктов технологических карт

---
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/ui/option/technologicalMap/technologicalMapProduct/ProductSelect.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2012. Axetta LLC. All Rights Reserved.
+ */
+
+package ru.axetta.ecafe.processor.web.ui.option.technologicalMap.technologicalMapProduct;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: damir
+ * Date: 06.07.12
+ * Time: 16:14
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ProductSelect {
+
+    public void select(List<ProductItem> productItemList);
+
+}
