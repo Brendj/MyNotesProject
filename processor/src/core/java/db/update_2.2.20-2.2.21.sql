f996e84d4869ac7e133d7b4229cbebfe8f9ab790
kadyrov <kadyrov@example.com>
2012-07-09 14:27:45 +0000

    Добавлены скрипты создания таблиц групп Технол.Карт и продуктов Убрал с таблицы Продукт техно карт имя продукта, так как оно берется из таблицы продуктов (справочника продуктов)

--- processor/src/core/java/db/update_2.2.20-2.2.21.sql
+++ processor/src/core/java/db/update_2.2.20-2.2.21.sql
@@ -1,15 +1,33 @@
 DROP TABLE cf_product_guide;
 DROP TABLE cf_products;
+DROP TABLE cf_product;
+DROP TABLE cf_technologicalmap;
 
+-- Таблица групп продуктов
+CREATE TABLE cf_product_groups
+(
+   IdOfProductGroups BigSerial,
+   NameOfGroup character varying(128) NOT NULL UNIQUE,
+   GUID character varying(36) NOT NULL,
+   GlobalVersion BIGINT DEFAULT NULL,
+   DeletedState boolean NOT NULL DEFAULT false,
+   OrgOwner BIGINT DEFAULT NULL,
+   CreatedDate bigint NOT NULL,
+   LastUpdate bigint,
+   DeleteDate bigint,
+   CONSTRAINT cf_product_groups_pk PRIMARY KEY (IdOfProductGroups )
+);
+
+-- Таблица справочника продуктов
 CREATE TABLE cf_products
 (
-  IdOfProducts bigserial,
+  IdOfProducts BigSerial,
+  IdOfProductGroups BigINT,
   Code character varying(16) NOT NULL,
   FullName character varying(1024),
   ProductName character varying(512),
   OkpCode character varying(32),
   GUID character varying(36) NOT NULL,
-  GroupName character varying(512),
   GlobalVersion BIGINT DEFAULT NULL,
   OrgOwner BIGINT DEFAULT NULL,
   DeletedState boolean NOT NULL DEFAULT false,
@@ -20,17 +38,35 @@ CREATE TABLE cf_products
   LastUpdate bigint,
   DeleteDate bigint,
   IdOfConfigurationProvider bigint,
-  CONSTRAINT cf_products_pk PRIMARY KEY (idOfProducts )
+  CONSTRAINT cf_products_pk PRIMARY KEY (idOfProducts ),
+  CONSTRAINT cf_products_product_groups_fk FOREIGN KEY (IdOfProductGroups)
+      REFERENCES cf_product_groups (IdOfProductGroups)
 );
+
+-- Таблица групп технологических карт
+CREATE TABLE cf_technological_map_groups
+(
+   IdOfTechMapGroups BigSerial,
+   NameOfGroup character varying(128) NOT NULL UNIQUE,
+   GUID character varying(36) NOT NULL,
+   GlobalVersion BIGINT DEFAULT NULL,
+   DeletedState boolean NOT NULL DEFAULT false,
+   OrgOwner BIGINT DEFAULT NULL,
+   CreatedDate bigint NOT NULL,
+   LastUpdate bigint,
+   DeleteDate bigint,
+   CONSTRAINT cf_technological_map_groups_pk PRIMARY KEY (IdOfTechMapGroups )
+);
+
 -- Таблица (справочник) технологических карт
 CREATE TABLE  cf_technological_map(
-  IdOfTechnologicalMaps bigserial,
+  IdOfTechnologicalMaps BigSerial,
+  IdOfTechMapGroups bigint,
   NameOfTechnologicalMap character varying(128) NOT NULL,
   NumberOfTechnologicalMap BIGINT NOT NULL,
   TechnologyOfPreparation character varying(4096) NOT NULL,
   TermOfRealization character varying(128) DEFAULT NULL,
   TempOfPreparation character varying(32) DEFAULT NULL,
-  GroupName character varying(512),
   EnergyValue FLOAT DEFAULT NULL,
   Proteins FLOAT DEFAULT NULL,
   Carbohydrates FLOAT DEFAULT NULL,
@@ -52,12 +88,15 @@ CREATE TABLE  cf_technological_map(
   CreatedDate bigint NOT NULL,
   LastUpdate bigint,
   DeleteDate bigint,
-  CONSTRAINT cf_technological_map_pk PRIMARY KEY (IdOfTechnologicalMaps )
+  CONSTRAINT cf_technological_map_pk PRIMARY KEY (IdOfTechnologicalMaps ),
+  CONSTRAINT cf_technological_map_technological_map_groups_fk FOREIGN KEY (IdOfTechMapGroups)
+      REFERENCES cf_technological_map_groups (IdOfTechMapGroups)
 );
 
+-- продукты технологических карт
 CREATE TABLE cf_technological_map_products
 (
-  IdOfTechnoMapProducts bigserial NOT NULL,
+  IdOfTechnoMapProducts BigSerial NOT NULL,
   IdOfTechnologicalMaps bigint NOT NULL,
   IdOfProducts bigint NOT NULL,
   NetWeight double precision NOT NULL DEFAULT 0,
@@ -67,7 +106,7 @@ CREATE TABLE cf_technological_map_products
   OrgOwner bigint,
   DeletedState boolean NOT NULL DEFAULT false,
   CreatedDate bigint NOT NULL,
-  LastupDate bigint,
+  LastUpDate bigint,
   DeleteDate bigint,
   NameOfProduct character varying(256),
   CONSTRAINT cf_technological_map_products_pk PRIMARY KEY (IdOfTechnoMapProducts ),
