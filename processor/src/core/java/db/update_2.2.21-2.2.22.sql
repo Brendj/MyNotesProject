20799e184613483cf7a878594353fb477f45788f
yashin <yashin@example.com>
2012-07-11 10:25:03 +0000

    Добавлены новые опции в таблицу cf_options, отвечающие за вкладку "Оплата он-лайн" в личном кабинете

---
+++ processor/src/core/java/db/update_2.2.21-2.2.22.sql
@@ -0,0 +1,4 @@
+INSERT INTO cf_options(idofoption, optiontext)VALUES (1000,'1');
+INSERT INTO cf_options(idofoption, optiontext)VALUES (1001,'3.3');
+INSERT INTO cf_options(idofoption, optiontext)VALUES (1002,'1');
+INSERT INTO cf_options(idofoption, optiontext)VALUES (1003,'3');
