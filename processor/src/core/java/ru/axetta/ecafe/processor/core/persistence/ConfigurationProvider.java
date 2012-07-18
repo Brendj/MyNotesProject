919e4fb5bde8b8fbe5af2219595b58498b928c8c
kadyrov <kadyrov@example.com>
2012-07-18 05:13:24 +0000

    Изменен генератор идентификатора Производственной конфигурации (теперь генерируется бд), добавлены поля пользователя создавшего/ изминившего объект и время. Добавлена саскадное изминение сущностей Org и ConfigurationProvider

--- processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/ConfigurationProvider.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/ConfigurationProvider.java
@@ -4,10 +4,8 @@
 
 package ru.axetta.ecafe.processor.core.persistence;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,14 +18,47 @@ public class ConfigurationProvider {
     private Long idOfConfigurationProvider;
     private String name;
     private Set products;
-    private Set<Org> orgInternal;
+    /* Пользователь бэк-оффиса котрый создал изменил конфигурацию */
+    private User userCreate;
+    private User userEdit;
+    /* дата создания объекта */
+    private Date createdDate;
+    /* дата мзминения объекта */
+    private Date lastUpdate;
+    private Set<Org> orgInternal = new HashSet<Org>();
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
+    public String getHistory(){
+        if (this.userCreate==null)
+            return "";
+        String stringUserCreate = "";
+        String stringUserEdit = "";
+        if (getUserCreate()!=null){
+            stringUserCreate = getUserCreate().getUserName();
+        }
+        if (getUserEdit()!=null){
+            stringUserEdit = getUserEdit().getUserName();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<span  style=\"white-space:nowrap\">");
+        sb.append("Создан: ").append(userCreate).append("<br/>")
+                .append("Дата создания: ").append(dateFormat.format(this.createdDate));
+        if (this.userEdit!=null)
+            sb.append("<br/>").append("Изменен: ").append(userEdit)
+                    .append("<br/>").append("Дата изменения: ").append(dateFormat.format(this.lastUpdate));
+        /*if (this.userDelete!=null)
+            sb.append("<br/>").append('\n').append("Удален: ").append(this.userDelete.getUserName())
+                    .append("<br/>").append("Дата удаления: ").append(dateFormat.format(this.deleteTime));*/
+        sb.append("</span>");
+        return sb.toString();
+    }
 
     public List<Org> getOrgs(){
         return new ArrayList<Org>(Collections.unmodifiableSet(getOrgInternal()));
     }
 
-    public void add(Org org){
+    public void addOrg(Org org){
         orgInternal.add(org);
     }
 
@@ -61,5 +92,37 @@ public class ConfigurationProvider {
 
     public void setProducts(Set products) {
         this.products = products;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public User getUserCreate() {
+        return userCreate;
+    }
+
+    public void setUserCreate(User userCreate) {
+        this.userCreate = userCreate;
+    }
+
+    public User getUserEdit() {
+        return userEdit;
+    }
+
+    public void setUserEdit(User userEdit) {
+        this.userEdit = userEdit;
     }
 }
