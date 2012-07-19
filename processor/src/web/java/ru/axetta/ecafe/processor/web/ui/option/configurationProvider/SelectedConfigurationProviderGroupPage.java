b8d6e7825db4437d401dce3b8d14bbc3b3a091f2
kadyrov <kadyrov@example.com>
2012-07-19 08:56:49 +0000

    Изменил бины используя спринговские анотации

--- processor/src/web/java/ru/axetta/ecafe/processor/web/ui/option/configurationProvider/SelectedConfigurationProviderGroupPage.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/ui/option/configurationProvider/SelectedConfigurationProviderGroupPage.java
@@ -7,7 +7,15 @@ package ru.axetta.ecafe.processor.web.ui.option.configurationProvider;
 import ru.axetta.ecafe.processor.core.persistence.utils.DAOUtils;
 import ru.axetta.ecafe.processor.web.ui.BasicWorkspacePage;
 import ru.axetta.ecafe.processor.core.persistence.ConfigurationProvider;
+import ru.axetta.ecafe.processor.web.ui.MainPage;
+
 import org.hibernate.Session;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,23 +24,36 @@ import org.hibernate.Session;
  * Time: 22:11
  * To change this template use File | Settings | File Templates.
  */
+@Component
+@Scope("session")
 public class SelectedConfigurationProviderGroupPage extends BasicWorkspacePage {
 
-    //selectItems
-
     private String title;
+    private Long idOfConfigurationProvider;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void onShow() throws Exception {
+        ConfigurationProvider configurationProvider = entityManager.find(ConfigurationProvider.class, idOfConfigurationProvider);
+        if (null == configurationProvider) {
+            this.title = null;
+        } else {
+            this.title = String.format("%d: %s", configurationProvider.getIdOfConfigurationProvider(),
+                    configurationProvider.getName());
+        }
+    }
 
     public String getTitle() {
         return title;
     }
 
-    public void fill(Session session, Long id) throws Exception {
-        ConfigurationProvider cp = (ConfigurationProvider)DAOUtils.findConfigurationProvider(session, id);
-        if (null == cp) {
-            this.title = null;
-        } else {
-            this.title = String.format("%d: %s", cp.getIdOfConfigurationProvider(), cp.getName());
-        }
+    public Long getIdOfConfigurationProvider() {
+        return idOfConfigurationProvider;
     }
 
+    public void setIdOfConfigurationProvider(Long idOfConfigurationProvider) {
+        this.idOfConfigurationProvider = idOfConfigurationProvider;
+    }
 }
