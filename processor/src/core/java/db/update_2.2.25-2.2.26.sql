17c826aa6da94565b2edc12f95786f76395a1f3b
kadyrov <kadyrov@example.com>
2012-09-21 12:59:49 +0000

    скрипт библиотеки

--- processor/src/core/java/db/update_2.2.25-2.2.26.sql
+++ processor/src/core/java/db/update_2.2.25-2.2.26.sql
@@ -1,40 +1,125 @@
 -- Бибилиотека
-DROP TABLE cf_circulations;
-DROP TABLE cf_issuable;
+-- Проводим очистку старых таблиц
+--! Данные не валидны
+DROP TABLE IF EXISTS cf_publications CASCADE;
+DROP TABLE IF EXISTS cf_circuls CASCADE;
+DROP TABLE IF EXISTS cf_publs CASCADE;
+DROP TABLE IF EXISTS cf_issuable CASCADE;
+DROP TABLE IF EXISTS cf_typesofaccompanyingdocuments CASCADE;
+DROP TABLE IF EXISTS cf_sources CASCADE;
+DROP TABLE IF EXISTS cf_accompanyingdocuments CASCADE;
+DROP TABLE IF EXISTS cf_readers CASCADE;
+DROP TABLE IF EXISTS cf_funds CASCADE;
+DROP TABLE IF EXISTS cf_inventorybooks CASCADE;
+DROP TABLE IF EXISTS cf_ksu1records CASCADE;
+DROP TABLE IF EXISTS cf_retirementreasons CASCADE;
+DROP TABLE IF EXISTS cf_ksu2records CASCADE;
+DROP TABLE IF EXISTS cf_instances CASCADE;
+DROP TABLE IF EXISTS cf_journals CASCADE;
+DROP TABLE IF EXISTS cf_journalitems CASCADE;
+DROP TABLE IF EXISTS cf_readerreg CASCADE;
+DROP TABLE IF EXISTS cf_libvisits CASCADE;
+DROP TABLE IF EXISTS cf_issuable CASCADE;
+DROP TABLE IF EXISTS cf_circulations;
 
-ALTER TABLE cf_publs DROP COLUMN hash;
-ALTER TABLE cf_publs DROP COLUMN isbn;
-ALTER TABLE cf_publs DROP COLUMN data;
-ALTER TABLE cf_publs ADD COLUMN isbn character varying(255);
-ALTER TABLE cf_publs ADD COLUMN hash integer NOT NULL;
-ALTER TABLE cf_publs ADD COLUMN data bytea NOT NULL;
+CREATE TABLE cf_publications
+(
+  IdOfPublication BigSerial NOT NULL,
+  DataOfPublication bytea NOT NULL,
+  Author character varying(255),
+  Title character varying(512),
+  ExtTitle character varying(255),
+  PublicationDate character varying(15),
+  Publisher character varying(255),
+  Version bigint NOT NULL,
+  ISBN character varying(255),
+  Hash integer NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
+  CONSTRAINT cf_publications_pk PRIMARY KEY (idofpubl ),
+  CONSTRAINT cf_publications_GUID_key UNIQUE (GUID )
+);
+
+CREATE TABLE cf_circulations
+(
+  IdOfCirculation BigSerial NOT NULL,
+  IdOfClient bigint NOT NULL,
+  IdOfPublication bigint NOT NULL,
+  IdOfOrg bigint NOT NULL,
+  IdOfIssuable bigint NOT NULL,
+  IssuanceDate bigint NOT NULL DEFAULT 0,
+  RefundDate bigint NOT NULL DEFAULT 0,
+  RealRefundDate bigint,
+  status integer NOT NULL DEFAULT 0,
+  Version bigint NOT NULL DEFAULT 0,
+  Quantity integer NOT NULL DEFAULT 0,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
+  CONSTRAINT cf_circulation_pk PRIMARY KEY (IdOfCirculation ),
+  CONSTRAINT cf_circulation_client_fk FOREIGN KEY (IdOfClient)
+      REFERENCES cf_clients (IdOfClient),
+  CONSTRAINT cf_circulation_org_fk FOREIGN KEY (IdOfOrg)
+      REFERENCES cf_orgs (IdOfOrg),
+  CONSTRAINT cf_circulation_publication_fk FOREIGN KEY (IdOfPublication)
+      REFERENCES cf_publications (IdOfPublication),
+  CONSTRAINT cf_circulation_GUID_key UNIQUE (GUID )
+);
+
+CREATE TABLE cf_issuable
+(
+  IdOfIssuable BigSerial NOT NULL,
+  BarCode bigint,
+  TypeOfIssuable character(1) NOT NULL DEFAULT 'i',
+  IdOfPublication bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
+  CONSTRAINT cf_issuable_pk PRIMARY KEY (IdOfIssuable ),
+  CONSTRAINT cf_issuable_publication_fkey FOREIGN KEY (IdOfPublication)
+      REFERENCES cf_publications (IdOfPublication)
+);
 
 --тип сопр.документа
 --TypeOfAccompanyingDocumentName - название (акт, накладная, т.п.)
 CREATE TABLE cf_typesofaccompanyingdocuments (
   IdOfTypeOfAccompanyingDocument bigserial NOT NULL,
   TypeOfAccompanyingDocumentName varchar(45) default NULL;
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfTypeOfAccompanyingDocument)
 );
+
 --источник поступления книг
 --SourceName - название источника (минобр, т.п.)
 CREATE TABLE cf_sources (
   IdOfSource bigserial NOT NULL,
   SourceName varchar(127) default NULL,
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfSource)
 );
 
@@ -47,13 +132,13 @@ CREATE TABLE cf_accompanyingdocuments (
   IdOfTypeOfAccompanyingDocument bigint NOT NULL REFERENCES cf_typesofaccompanyingdocument(IdOfTypeOfAccompanyingDocument),
   AccompanyingDocumentNumber varchar(32) NOT NULL,
   IdOfSource bigint default NULL REFERENCES cf_sources(IdOfSource),
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfAccompanyingDocument)
 );
 
@@ -61,29 +146,29 @@ CREATE TABLE cf_accompanyingdocuments (
 CREATE TABLE cf_readers (
   IdOfReader bigserial NOT NULL,
   IdOfClient bigint NOT NULL REFERENCES cf_clients(idofclient),
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfReader)
 );
 
 --фонд
 --FundName - название
+--! `InvBook` bigint(20) NOT NULL,--инвентарная книга фонда
 CREATE TABLE cf_funds (
   IdOfFund bigserial NOT NULL,
   FundName varchar(127) default NULL,
- --! `InvBook` bigint(20) NOT NULL,--инвентарная книга фонда
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfFund)
 );
 
@@ -92,13 +177,13 @@ CREATE TABLE cf_funds (
 CREATE TABLE cf_inventorybooks (
   IdOfBook bigserial NOT NULL,
   BookName varchar(255) default NULL,
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfBook)
 );
 
@@ -113,13 +198,13 @@ CREATE TABLE cf_ksu1records (
   IdOfFund bigint NOT NULL REFERENCES cf_funds(IdOfFund),
   IncomeDate date default NULL,
   AccompanyingDocument bigint default '0' REFERENCES cf_accompanyingdocuments(IdOfAccompanyingDocument),
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfKSU1Record)
 );
 --причина выбытия из фонда
@@ -127,13 +212,14 @@ CREATE TABLE cf_ksu1records (
 CREATE TABLE cf_retirementreasons (
   IdOfRetirementReason bigserial NOT NULL,
   RetirementReasonName varchar(45) default NULL,
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
+  HashCode integer NOT NULL,
   PRIMARY KEY  (IdOfRetirementReason)
 );
 
@@ -150,13 +236,13 @@ CREATE TABLE cf_ksu2records (
   IdOfFund bigint NOT NULL REFERENCES cf_funds(IdOfFund),
   RetirementDate date NOT NULL,
   IdOfRetirementReason bigint NOT NULL REFERENCES cf_retirementreasons(IdOfRetirementReason),
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfKSU2Record),
   UNIQUE (IdOfFund,RecordNumber)
 );
@@ -173,7 +259,7 @@ CREATE TABLE cf_ksu2records (
 --в одной инв. книге номера уникальны
 CREATE TABLE cf_instances (
   IdOfInstance bigserial NOT NULL,
-  IdOfPublication bigint NOT NULL REFERENCES cf_publs(idofpubl) ,
+  IdOfPublication bigint NOT NULL REFERENCES cf_publications(idofpubl) ,
   InGroup bit(1) NOT NULL default '0',
   IdOfFund bigint default NULL REFERENCES cf_funds(IdOfFund),
   InvNumber varchar(10) default NULL,
@@ -181,13 +267,13 @@ CREATE TABLE cf_instances (
   IdOfKSU1Record bigint default NULL REFERENCES cf_ksu1records(IdOfKSU1Record),
   IdOfKSU2Record bigint default NULL REFERENCES cf_ksu2records(IdOfKSU2Record),
   Cost int NOT NULL default '0',
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfInstance),
   UNIQUE (InvBook,InvNumber)
 );
@@ -202,16 +288,16 @@ CREATE TABLE cf_journals (
   IdOfJournal bigserial NOT NULL,
   IdOfFund bigint default NULL REFERENCES cf_funds(IdOfFund),
   IsNewspaper bit(1) NOT NULL default '0',
-  IdOfPublication bigint NOT NULL REFERENCES cf_publs(idofpubl),
+  IdOfPublication bigint NOT NULL REFERENCES cf_publications(idofpubl),
   MonthCount int NOT NULL default '0',
   Count int NOT NULL default '0',
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfJournal)
 );
 
@@ -232,31 +318,30 @@ CREATE TABLE cf_journalitems (
   Cost int NOT NULL default '0',
   IdOfKSU1Record bigint default NULL REFERENCES cf_ksu1records(IdOfKSU1Record),
   IdOfKSU2Record bigint default NULL REFERENCES cf_ksu2records(IdOfKSU2Record),
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfJournalItem)
 );
 
 --регистрация читателя (перерег. после перехода в другой класс)
---IdOfClientGroupHist - ссылка на историю переходов по классам
 --IdOfClientGroupHist - ссылка на историю переходов по классам
 CREATE TABLE cf_readerreg (
   IdOfReg bigserial NOT NULL,
   IdOfReader bigint NOT NULL REFERENCES cf_readers(IdOfReader),
   IdOfClientGroupHist bigint default NULL,
   Date date NOT NULL,
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfReg)
 );
 
@@ -269,13 +354,13 @@ CREATE TABLE cf_libvisits (
   IdOfClient bigint default NULL REFERENCES cf_clients(idofclient),
   Source int NOT NULL default '0',
   Date timestamp NOT NULL,
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfLibVisit)
 );
 --выдаваемая сущность
@@ -292,13 +377,13 @@ CREATE TABLE cf_issuable (
   IdOfInstance bigint default NULL REFERENCES cf_instances(IdOfInstance),
   IdOfJournalItem bigint default NULL REFERENCES cf_journalitems(IdOfJournalItem),
   Issuance bigint default NULL,
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfIssuable),
   UNIQUE  (BarCode)
 );
@@ -320,13 +405,13 @@ CREATE TABLE cf_circulations (
   RefundDate timestamp NOT NULL,
   RealRefundDate timestamp NULL default NULL,
   Status int NOT NULL default '0',
-  globalversion bigint,
-  orgowner bigint,
-  deletedstate boolean NOT NULL DEFAULT false,
-  guid character varying(36) NOT NULL,
-  lastupdate bigint,
-  deletedate bigint,
-  createddate bigint NOT NULL,
+  GlobalVersion bigint,
+  OrgOwner bigint,
+  DeletedState boolean NOT NULL DEFAULT false,
+  GUID character varying(36) NOT NULL,
+  LastUpdate bigint,
+  DeleteDate bigint,
+  CreatedDate bigint NOT NULL,
   PRIMARY KEY  (IdOfCirculation)
 );
 
