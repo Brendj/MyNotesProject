a698920cf39289e41e96958d008f85a5245f5406
kadyrov <kadyrov@example.com>
2012-10-09 14:09:25 +0000

    Исправлено падение страницы с Информацией о Крипто провайдера

--- processor/src/web/java/ru/axetta/ecafe/processor/web/ui/option/CryptoInfoPage.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/ui/option/CryptoInfoPage.java
@@ -10,9 +10,11 @@ import ru.axetta.ecafe.processor.web.ui.BasicWorkspacePage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("session")
 public class CryptoInfoPage extends BasicWorkspacePage {
 
     private static final Logger logger = LoggerFactory.getLogger(CryptoInfoPage.class);
