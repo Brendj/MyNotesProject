186a42d19e961e83b8a493616315274231cc102f
rodionov <rodionov@example.com>
2012-11-01 09:58:27 +0000

    Изменения структуры базы данных для введения динамических комплексов

---
+++ processor/src/core/java/db/update_2.2.27-2.2.28.sql
@@ -0,0 +1,57 @@
+-- Table: cf_complexinfo_discountdetail
+
+-- DROP TABLE cf_complexinfo_discountdetail;
+
+--торговая скидка
+--idofdiscountdetail id
+--size размер скидки
+--isallgroups 0 – применяется для конкретной группы, 1 – применяются для всех групп
+--idofclientgroup id группы клиентов
+--maxcount Максимальное количество применений скидки: -1 – не ограничено,--  >=0 указанное число раз
+--idoforg id организации (нужен только для создания foreign key на cf_clientgroups)
+CREATE TABLE cf_complexinfo_discountdetail
+(
+  idofdiscountdetail bigserial NOT NULL,
+  size double precision NOT NULL,
+  isallgroups integer NOT NULL,
+  idofclientgroup bigint NOT NULL,
+  maxcount integer NOT NULL,
+  idoforg bigint NOT NULL,
+  CONSTRAINT cf_complexinfo_discountdetail_pk PRIMARY KEY (idofdiscountdetail ),
+  CONSTRAINT cf_complexinfo_discountdetail_idofclientgroup_fk FOREIGN KEY (idoforg, idofclientgroup)
+      REFERENCES cf_clientgroups (idoforg, idofclientgroup) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT cf_complexinfo_discountdetail_idoforg_fk FOREIGN KEY (idoforg)
+      REFERENCES cf_orgs (idoforg) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE cf_complexinfo_discountdetail
+  OWNER TO postgres;
+
+-- Column: usetrdiscount
+
+-- ALTER TABLE cf_complexinfo DROP COLUMN usetrdiscount;
+-- userdiscount 0 – торговые скидки не применяются, 1 – торговые скидки применяются (только для динамических комплексов)
+ALTER TABLE cf_complexinfo ADD COLUMN usetrdiscount integer;
+ALTER TABLE cf_complexinfo ALTER COLUMN usetrdiscount SET DEFAULT 0;
+
+-- Column: idofdiscountdetail
+
+-- ALTER TABLE cf_complexinfo DROP COLUMN idofdiscountdetail;
+-- idofdiscountdetail ссылка на элемент торговой скидки
+ALTER TABLE cf_complexinfo ADD COLUMN idofdiscountdetail bigint;
+
+-- Column: idoforg
+
+-- ALTER TABLE cf_complexinfo_discountdetail DROP COLUMN idoforg;
+--idoforg id организации (нужен только для создания foreign key на cf_clientgroups)
+ALTER TABLE cf_complexinfo_discountdetail ALTER COLUMN idoforg SET NULL;
+
+-- Column: idofclientgroup
+
+-- ALTER TABLE cf_complexinfo_discountdetail DROP COLUMN idofclientgroup;
+--idofclientgroup id группы клиентов
+ALTER TABLE cf_complexinfo_discountdetail ALTER COLUMN idofclientgroup SET NULL;
