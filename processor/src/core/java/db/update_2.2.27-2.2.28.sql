777d0d14f08422fb465949f0955df91623c5aa3c
kadyrov <kadyrov@example.com>
2012-11-23 13:46:09 +0000

    Скрипт 28-29 финализирован в понедельник обновлю процесинг на тестовом сервер для тестирования Во вторник после тестирования обновлю в Питере

--- processor/src/core/java/db/update_2.2.27-2.2.28.sql
+++ processor/src/core/java/db/update_2.2.27-2.2.28.sql
@@ -1,24 +1,21 @@
-create or replace function add_column(_tbl regclass, _col text, _type regtype, _params text) returns bool as '
-declare success bool;
-begin
-success := true;
-if exists (
-    SELECT 1 FROM pg_attribute
-    WHERE attrelid = _tbl
-    AND attname = _col
-    AND NOT attisdropped)
-then
-    RAISE NOTICE ''Column % already exists in %.'', _col, _tbl;
-else
-    EXECUTE ''ALTER TABLE '' || _tbl || '' ADD COLUMN '' || quote_ident(_col) || '' '' || _type || '' '' || _params;
-end if;
-return success;
-end;' language 'plpgsql';
+--! create or replace function add_column(_tbl regclass, _col text, _type regtype, _params text) returns bool as '
+--! declare success bool;
+--! begin
+--! success := true;
+--! if exists (
+--!     SELECT 1 FROM pg_attribute
+--!     WHERE attrelid = _tbl
+--!     AND attname = _col
+--!     AND NOT attisdropped)
+--! then
+--!     RAISE NOTICE ''Column % already exists in %.'', _col, _tbl;
+--! else
+--!     EXECUTE ''ALTER TABLE '' || _tbl || '' ADD COLUMN '' || quote_ident(_col) || '' '' || _type || '' '' || _params;
+--! end if;
+--! return success;
+--! end;' language 'plpgsql';
 
--- Table: cf_complexinfo_discountdetail
-
--- DROP TABLE cf_complexinfo_discountdetail;
-
+-- cf_complexinfo_discountdetail
 --торговая скидка
 --idofdiscountdetail id
 --size размер скидки
@@ -31,81 +28,49 @@ CREATE TABLE cf_complexinfo_discountdetail
   idofdiscountdetail bigserial NOT NULL,
   size double precision NOT NULL,
   isallgroups integer NOT NULL,
-  idofclientgroup bigint NOT NULL,
-  maxcount integer NOT NULL,
-  idoforg bigint NOT NULL,
+  idofclientgroup bigint,
+  maxcount integer,
+  idoforg bigint,
   CONSTRAINT cf_complexinfo_discountdetail_pk PRIMARY KEY (idofdiscountdetail ),
-  CONSTRAINT cf_complexinfo_discountdetail_idofclientgroup_fk FOREIGN KEY (idoforg, idofclientgroup)
-      REFERENCES cf_clientgroups (idoforg, idofclientgroup) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT cf_complexinfo_discountdetail_idoforg_fk FOREIGN KEY (idoforg)
-      REFERENCES cf_orgs (idoforg) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
+  CONSTRAINT cf_complexinfo_discountdetail_idofclientgroup_fk FOREIGN KEY (idoforg, idofclientgroup) REFERENCES cf_clientgroups (idoforg, idofclientgroup),
+  CONSTRAINT cf_complexinfo_discountdetail_idoforg_fk FOREIGN KEY (idoforg) REFERENCES cf_orgs (idoforg)
 );
-ALTER TABLE cf_complexinfo_discountdetail
-  OWNER TO postgres;
 
--- Column: usetrdiscount
-
--- ALTER TABLE cf_complexinfo DROP COLUMN usetrdiscount;
+--! ALTER TABLE cf_complexinfo DROP COLUMN usetrdiscount;
 -- userdiscount 0 – торговые скидки не применяются, 1 – торговые скидки применяются (только для динамических комплексов)
-SELECT add_column('cf_complexinfo', 'usetrdiscount', 'integer', 'DEFAULT 0');
---ALTER TABLE cf_complexinfo ADD COLUMN usetrdiscount integer;
---ALTER TABLE cf_complexinfo ALTER COLUMN usetrdiscount SET DEFAULT 0;
+--! SELECT add_column('cf_complexinfo', 'usetrdiscount', 'integer', 'DEFAULT 0');
+ALTER TABLE cf_complexinfo ADD COLUMN usetrdiscount integer  DEFAULT 0;
+--!ALTER TABLE cf_complexinfo ALTER COLUMN usetrdiscount SET DEFAULT 0;
 
 -- Column: idofdiscountdetail
 
--- ALTER TABLE cf_complexinfo DROP COLUMN idofdiscountdetail;
--- idofdiscountdetail ссылка на элемент торговой скидки
-SELECT add_column('cf_complexinfo', 'idofdiscountdetail', 'bigint', '');
---ALTER TABLE cf_complexinfo ADD COLUMN idofdiscountdetail bigint;
+--! ALTER TABLE cf_complexinfo DROP COLUMN idofdiscountdetail;
+--! idofdiscountdetail ссылка на элемент торговой скидки
+-- Добавлена ссылка на элемент торговой скидки
+--! SELECT add_column('cf_complexinfo', 'idofdiscountdetail', 'bigint', '');
+ALTER TABLE cf_complexinfo ADD COLUMN idofdiscountdetail bigint;
 
--- Column: idoforg
-
--- ALTER TABLE cf_complexinfo_discountdetail DROP COLUMN idoforg;
---idoforg id организации (нужен только для создания foreign key на cf_clientgroups)
-ALTER TABLE cf_complexinfo_discountdetail ALTER COLUMN idoforg DROP NOT NULL;
-
--- Column: idofclientgroup
-
--- ALTER TABLE cf_complexinfo_discountdetail DROP COLUMN idofclientgroup;
---idofclientgroup id группы клиентов
-ALTER TABLE cf_complexinfo_discountdetail ALTER COLUMN idofclientgroup DROP NOT NULL;
-
--- Column: maxcount
-
--- ALTER TABLE cf_complexinfo_discountdetail DROP COLUMN maxcount;
-
-ALTER TABLE cf_complexinfo_discountdetail ALTER COLUMN maxcount DROP  NOT NULL;
-
--- Column: idofitem
-
--- ALTER TABLE cf_complexinfodetail DROP COLUMN idofitem;
-SELECT add_column('cf_complexinfodetail', 'idofitem', 'bigint', '');
---ALTER TABLE cf_complexinfodetail ADD COLUMN idofitem bigint;
+--! ALTER TABLE cf_complexinfodetail DROP COLUMN idofitem;
+-- SELECT add_column('cf_complexinfodetail', 'idofitem', 'bigint', '');
+ALTER TABLE cf_complexinfodetail ADD COLUMN idofitem bigint;
 
 -- Column: idofmenudetail
 
--- ALTER TABLE cf_complexinfo DROP COLUMN idofmenudetail;
-SELECT add_column('cf_complexinfo', 'idofmenudetail', 'bigint', '');
---ALTER TABLE cf_complexinfo ADD COLUMN idofmenudetail bigint;
+--! ALTER TABLE cf_complexinfo DROP COLUMN idofmenudetail;
+-- SELECT add_column('cf_complexinfo', 'idofmenudetail', 'bigint', '');
+ALTER TABLE cf_complexinfo ADD COLUMN idofmenudetail bigint;
 
 -- New FK to cf_menudetail
-ALTER TABLE cf_complexinfo DROP CONSTRAINT IF EXISTS cf_complexinfo_idofmenudetail_fk;
-ALTER TABLE cf_complexinfo ADD CONSTRAINT cf_complexinfo_idofmenudetail_fk FOREIGN KEY (idofmenudetail)
-      REFERENCES cf_menudetails (idofmenudetail) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION;
+--! ALTER TABLE cf_complexinfo DROP CONSTRAINT cf_complexinfo_idofmenudetail_fk;
+ALTER TABLE cf_complexinfo ADD CONSTRAINT cf_complexinfo_idofmenudetail_fk FOREIGN KEY (idofmenudetail) REFERENCES cf_menudetails (idofmenudetail);
 
 -- Changes to cf_menudetails
 ALTER TABLE cf_menudetails ALTER COLUMN menudetailoutput DROP NOT NULL;
 ALTER TABLE cf_menudetails ALTER COLUMN price DROP NOT NULL;
-SELECT add_column('cf_menudetails', 'flags', 'integer', 'NOT NULL DEFAULT 1');
---ALTER TABLE cf_menudetails ADD COLUMN flags integer NOT NULL DEFAULT 1;
-SELECT add_column('cf_menudetails', 'priority', 'integer', 'DEFAULT 0');
---ALTER TABLE cf_menudetails ADD COLUMN priority integer DEFAULT 0;
+--! SELECT add_column('cf_menudetails', 'flags', 'integer', 'NOT NULL DEFAULT 1');
+ALTER TABLE cf_menudetails ADD COLUMN flags integer NOT NULL DEFAULT 1;
+--! SELECT add_column('cf_menudetails', 'priority', 'integer', 'DEFAULT 0');
+ALTER TABLE cf_menudetails ADD COLUMN priority integer DEFAULT 0;
 
 -- Настройки ECafe администратора
 CREATE TABLE cf_ECafeSettings
--- processor/src/core/java/db/update_2.2.28-2.2.29.sql
+++ processor/src/core/java/db/update_2.2.28-2.2.29.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS cf_projectstate_data
-  (
+(
   Period bigint NOT NULL,
   Type int NOT NULL,
   StringKey character varying(128),
   StringValue character varying(128),
   generationdate bigint NOT NULL,
   CONSTRAINT cf_projectstate_data_pk PRIMARY KEY (Period, Type, StringKey)
-  );
+);
 
 -- Добавление колонки currentprice к таблице cf_complexinfo
 ALTER TABLE cf_complexinfo ADD COLUMN currentprice bigint;
@@ -14,83 +14,8 @@ ALTER TABLE cf_complexinfo ADD COLUMN currentprice bigint;
 -- Добавление колонки count к таблице cf_complexinfodetail
 ALTER TABLE cf_complexinfodetail ADD COLUMN count integer;
 
--- Добавлена связка к документу
+-- Добавленна свяска к документу
 ALTER TABLE cf_internal_disposing_document_positions ADD COLUMN IdOfGood bigint NOT NULL;
 ALTER TABLE cf_internal_disposing_document_positions ADD CONSTRAINT cf_internal_disposing_document_positions_fk_good FOREIGN KEY (IdOfGood) REFERENCES cf_goods (IdOfGood);
 
--- Добавление ссылки на таблицу товаров
-ALTER TABLE cf_menudetails ADD COLUMN idofgood bigint;
-ALTER TABLE cf_menudetails ADD CONSTRAINT cf_menudetail_idofgood_fk FOREIGN KEY (idofgood) REFERENCES cf_goods(idofgood);
-
--- Выставление ссылок на продукты и технокарты таблице товаров
-ALTER TABLE cf_goods ADD CONSTRAINT cf_goods_idoftechnologicalmaps_fk FOREIGN KEY (idoftechnologicalmaps) REFERENCES cf_technological_map (idoftechnologicalmaps);
-ALTER TABLE cf_goods ADD CONSTRAINT cf_goods_idofproducts_fk FOREIGN KEY (idofproducts) REFERENCES cf_products (idofproducts);
-
--- Таблица родительских запретов на определенные блюда
-CREATE TABLE cf_dish_prohibitions
-(
-  idofprohibition bigserial NOT NULL,
-  idofclient bigint NOT NULL,
-  idofproducts bigint,
-  idofproductgroups bigint,
-  idofgood bigint,
-  idofgoodsgroup bigint,
-  CONSTRAINT cf_dish_prohibitions_pk PRIMARY KEY (idofprohibition),
-  CONSTRAINT cf_dish_prohibitions_idofclient_fk FOREIGN KEY (idofclient)
-      REFERENCES cf_clients (idofclient) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT cf_dish_prohibitions_idofgood_fk FOREIGN KEY (idofgood)
-      REFERENCES cf_goods (idofgood) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT cf_dish_prohibitions_idofgoodsgroup_fk FOREIGN KEY (idofgoodsgroup)
-      REFERENCES cf_goods_groups (idofgoodsgroup) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT cf_dish_prohibitions_idofproductgroups_fk FOREIGN KEY (idofproductgroups)
-      REFERENCES cf_product_groups (idofproductgroups) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT cf_dish_prohibitions_idofproducts_fk FOREIGN KEY (idofproducts)
-      REFERENCES cf_products (idofproducts) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT cf_dish_prohibitions_check_onlyoneisnotnull CHECK ((
-CASE
-    WHEN idofproducts IS NOT NULL THEN 1
-    ELSE 0
-END +
-CASE
-    WHEN idofproductgroups IS NOT NULL THEN 1
-    ELSE 0
-END +
-CASE
-    WHEN idofgood IS NOT NULL THEN 1
-    ELSE 0
-END +
-CASE
-    WHEN idofgoodsgroup IS NOT NULL THEN 1
-    ELSE 0
-END) = 1)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE cf_dish_prohibitions
-  OWNER TO postgres;
-
--- Таблица исключений из запретов для товаров либо групп товаров
-CREATE TABLE cf_dish_prohibition_exclusions
-(
-  idofexclusion bigserial NOT NULL,
-  idofprohibition bigint NOT NULL,
-  idofgood bigint,
-  idofgoodsgroup bigint,
-  CONSTRAINT cf_dish_prohibition_exclusions_pk PRIMARY KEY (idofexclusion),
-  CONSTRAINT cf_dish_prohibition_exclusions_idofprohibition_fk FOREIGN KEY (idofprohibition)
-      REFERENCES cf_dish_prohibitions (idofprohibition) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT cf_dish_prohibition_exclusions_check_oneisnotnull CHECK (COALESCE(idofgood, idofgoodsgroup) IS NOT NULL),
-  CONSTRAINT cf_dish_prohibition_exclusions_check_oneisnull CHECK ((idofgood + idofgoodsgroup) IS NULL)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE cf_dish_prohibition_exclusions
-  OWNER TO postgres;
+--! ФИНАЛИЗИРОВАН (Кадыров, 121123) НЕ МЕНЯТЬ
