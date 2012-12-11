8f1005eb13c44adfd47033b715bdec112ac1b672
kadyrov <kadyrov@example.com>
2012-12-11 13:56:19 +0000

    Востановил удаленные классы (без них не собирается проект)

---
+++ processor/src/core/java/generated/nsiws/TypeCodeType.java
@@ -0,0 +1,38 @@
+
+package generated.nsiws;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for TypeCodeType.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="TypeCodeType">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="GSRV"/>
+ *     &lt;enumeration value="GFNC"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "TypeCodeType", namespace = "http://smev.gosuslugi.ru/rev110801")
+@XmlEnum
+public enum TypeCodeType {
+
+    GSRV,
+    GFNC;
+
+    public String value() {
+        return name();
+    }
+
+    public static TypeCodeType fromValue(String v) {
+        return valueOf(v);
+    }
+
+}
