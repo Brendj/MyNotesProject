936818a559f257293553ca11d39ac428b0ea56b1
kadyrov <kadyrov@example.com>
2013-04-03 06:36:45 +0000

    Финаллизирован скрипт обновления 2.2.39.130403

--- processor/src/core/java/db/update_2.2.38-2.2.39.sql
+++ processor/src/core/java/db/update_2.2.38-2.2.39.sql
@@ -1,3 +1,9 @@
+--! Пока скрипт не винализирован рекоментовано писать очистку добавляемых колонок таблиц.
+--! после финализации они уберутся
+--! Информация для разработчика -- информация для пользователя
+
+-- Пакет обновлений 2.2.39
+
 -- Расширение поля source в связи с подключением платежей из РНИП (размер идентификатора = 32 символам)
 ALTER TABLE cf_transactions ALTER COLUMN source TYPE VARCHAR(50);
 
@@ -5,3 +11,5 @@ ALTER TABLE cf_transactions ALTER COLUMN source TYPE VARCHAR(50);
 --! РНИП требует наличия данных полей в запросах на создание каталогов
 ALTER TABLE cf_contragents ADD COLUMN kpp VARCHAR(10) NOT NULL DEFAULT '';
 ALTER TABLE cf_contragents ADD COLUMN ogrn VARCHAR(15) NOT NULL DEFAULT '';
+
+--! ФИНАЛИЗИРОВАН (Кадыров, 130403) НЕ МЕНЯТЬ
