2efa0da7d57aba3795bb5f0aaf97e1d906123796
kadyrov <kadyrov@example.com>
2013-08-28 08:04:33 +0000

    ECAFE-1111 Реализовать отчет "Организации по контрактам" Поправлены пути в страницах jsf Все бэкин бени сущности Контракт перенесены в обну библиотеку (под папку в контрагентах)

---
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/ui/contragent/contract/ContractEditPage.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2012. Axetta LLC. All Rights Reserved.
+ */
+
+package ru.axetta.ecafe.processor.web.ui.contragent.contract;
+
+import ru.axetta.ecafe.processor.core.persistence.Contract;
+import ru.axetta.ecafe.processor.core.persistence.Contragent;
+import ru.axetta.ecafe.processor.core.persistence.utils.DAOService;
+import ru.axetta.ecafe.processor.web.ui.MainPage;
+import ru.axetta.ecafe.processor.web.ui.abstractpage.AbstractEditPage;
+import ru.axetta.ecafe.processor.web.ui.ccaccount.CCAccountFilter;
+import ru.axetta.ecafe.processor.web.ui.contragent.ContragentSelectPage;
+import ru.axetta.ecafe.processor.web.ui.org.OrgListSelectPage;
+
+import org.hibernate.Session;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+@Component
+@Scope("session")
+public class ContractEditPage extends AbstractEditPage<ContractItem> implements ContragentSelectPage.CompleteHandler, OrgListSelectPage.CompleteHandlerList{
+    protected final CCAccountFilter contragentFilter = new CCAccountFilter();
+
+    @Component(value = "contractCreatePage")
+    @Scope("session")
+    public static class ContractCreatePage extends ContractEditPage {
+        @Override
+        public boolean isCreateMode() {
+            return true;
+        }
+        @Override
+        public boolean isEditMode() {
+            return false;
+        }
+        @Override
+        public void onShow() throws Exception {
+            currentItem = new ContractItem();
+        }
+    }
+    @Component(value = "contractViewPage")
+    @Scope("session")
+    public static class ContractViewPage extends ContractEditPage {
+        @Override
+        public boolean isReadonly() {
+            return true;
+        }
+        @Override
+        public boolean isEditMode() {
+            return false;
+        }
+    }
+    
+    public ContractEditPage getAp() {
+        return (ContractEditPage)MainPage.getSessionInstance().getCurrentWorkspacePage();
+    }
+
+    @Override
+    public String getPageFilename() {
+        return "contragent/contract/edit";
+    }
+
+    @Override
+    public void completeOrgListSelection(Map<Long, String> orgMap) throws Exception {
+        if (orgMap != null) {
+            currentItem.setIdOfOrgList(new ArrayList<Long>(0));
+            if (orgMap.isEmpty())
+                currentItem.setOrgNames("Не выбрано");
+            else {
+                String filter = "";
+                for(Long idOfOrg : orgMap.keySet()) {
+                    currentItem.getIdOfOrgList().add(idOfOrg);
+                    filter = filter.concat(orgMap.get(idOfOrg) + "; ");
+                }
+                filter = filter.substring(0, filter.length() - 1);
+                currentItem.setOrgNames(filter);
+            }
+        }
+    }
+
+    @Override
+    public void completeContragentSelection(Session session, Long idOfContragent, int multiContrFlag, String classTypes)
+            throws Exception {
+        contragentFilter.completeContragentSelection(session, idOfContragent);
+        Contragent contragent = DAOService.getInstance().getContragentById(idOfContragent);
+        currentItem.setContragent(contragent);
+        currentItem.setPerformer(contragent.getContragentName());
+    }
+
+    public CCAccountFilter getContragentFilter() {
+        if ((contragentFilter.getContragent() == null ||
+             contragentFilter.getContragent().getContragentName() == null ||
+             contragentFilter.getContragent().getContragentName().length() < 1) &&
+            currentItem.getContragent() != null) {
+            try {
+                contragentFilter.completeContragentSelection(currentItem.getContragent());
+            } catch (Exception e) {
+
+            }
+        }
+        if (currentItem.getContragent() == null) {
+            contragentFilter.clear();
+        }
+        return contragentFilter;
+    }
+
+    public String getStringIdOfOrgList() {
+        return currentItem.getIdOfOrgList().toString().replaceAll("[^0-9,]","");
+    }
+
+    @Override
+    public void onShow() throws Exception {
+        super.onShow();
+        Long id = this.getSelectedEntityGroupPage().getCurrentEntityItemId();
+        if(this.currentItem==null && id!=null){
+            currentItem = new ContractItem();
+            Contract contract = entityManager.find(Contract.class, id);
+            if(contract!=null){
+                currentItem.fill(entityManager, contract);
+                this.getSelectedEntityGroupPage().setCurrentEntityItem(currentItem);
+            }
+        }
+    }
+}
---
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/ui/contragent/contract/ContractFilter.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright (c) 2013. Axetta LLC. All Rights Reserved.
+ */
+
+package ru.axetta.ecafe.processor.web.ui.contragent.contract;
+
+import ru.axetta.ecafe.processor.core.persistence.Contract;
+import ru.axetta.ecafe.processor.core.persistence.Contragent;
+import ru.axetta.ecafe.processor.core.persistence.ContragentClientAccount;
+import ru.axetta.ecafe.processor.core.persistence.Org;
+
+import org.hibernate.Criteria;
+import org.hibernate.FetchMode;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Developer
+ * Date: 17.07.2009
+ * Time: 12:30:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class ContractFilter {
+
+    public static class ContractItem {
+
+        private final Long idOfContract;
+        private final String contractName;
+
+        public ContractItem() {
+            this.idOfContract = null;
+            this.contractName = null;
+        }
+
+        public boolean isEmpty() {
+            return null == idOfContract;
+        }
+
+        public ContractItem(Contract contract) {
+            this.idOfContract = contract.getIdOfContract();
+            this.contractName = contract.getContractNumber();
+        }
+
+        public ContractItem(Long idOfContract, String contractName) {
+            this.idOfContract = idOfContract;
+            this.contractName = contractName;
+        }
+
+        public Long getIdOfContract() {
+            return idOfContract;
+        }
+
+        public String getContractName() {
+            return contractName;
+        }
+    }
+
+    public static class OrgItem {
+
+        private final Long idOfOrg;
+        private final String shortName;
+        private final String officialName;
+
+        public OrgItem() {
+            this.idOfOrg = null;
+            this.shortName = null;
+            this.officialName = null;
+        }
+
+        public boolean isEmpty() {
+            return null == idOfOrg;
+        }
+
+        public OrgItem(Org org) {
+            this.idOfOrg = org.getIdOfOrg();
+            this.shortName = org.getShortName();
+            this.officialName = org.getOfficialName();
+        }
+
+        public Long getIdOfOrg() {
+            return idOfOrg;
+        }
+
+        public String getShortName() {
+            return shortName;
+        }
+
+        public String getOfficialName() {
+            return officialName;
+        }
+    }
+
+    private Long idOfAccount = null;
+    private ContractItem contract = new ContractItem();
+    private OrgItem org = new OrgItem();
+
+    public Long getIdOfAccount() {
+        return idOfAccount;
+    }
+
+    public void setIdOfAccount(Long idOfAccount) {
+        if (0L == idOfAccount) {
+            this.idOfAccount = null;
+        } else {
+            this.idOfAccount = idOfAccount;
+        }
+    }
+
+    public ContractItem getContract() {
+        return contract;
+    }
+
+    public OrgItem getOrg() {
+        return org;
+    }
+
+    public boolean isEmpty() {
+        return null == idOfAccount && contract.isEmpty() && org.isEmpty();
+    }
+
+    public String getStatus() {
+        if (isEmpty()) {
+            return "нет";
+        }
+        return "установлен";
+    }
+
+    public void completeContractSelection (Contract contract) throws Exception {
+        completeContractSelection(contract, 0, "");
+    }
+
+    public void completeContractSelection (Long idofContract, String contractName) throws Exception {
+        if (null != idofContract && null != contractName && contractName.length() > 0) {
+            this.contract = new ContractItem(idofContract, contractName);
+        }
+    }
+
+    public void completeContractSelection(Session session, Long idOfContract, int multiContrFlag, String classTypes) throws Exception {
+        if (null != idOfContract) {
+            Contract contract = (Contract) session.load(Contract.class, idOfContract);
+            //this.contract = new ContractItem(contract);
+            completeContractSelection(contract, multiContrFlag, classTypes);
+        } else {
+            clear ();
+        }
+    }
+
+    public void completeContractSelection(Contract contract, int multiContrFlag, String classTypes) throws Exception {
+        if (null != contract) {
+            this.contract = new ContractItem(contract);
+        }
+    }
+
+    public void completeOrgSelection(Session session, Long idOfOrg) throws Exception {
+        if (null != idOfOrg) {
+            Org org = (Org) session.load(Org.class, idOfOrg);
+            this.org = new OrgItem(org);
+        }
+    }
+
+    public void clear() {
+        idOfAccount = null;
+        contract = new ContractItem();
+        org = new OrgItem();
+    }
+
+    public List retrieveCCAccounts(Session session) throws Exception {
+        /*Criteria criteria = session.createCriteria(ContragentClientAccount.class);
+        criteria.setFetchMode("client", FetchMode.JOIN);
+        if (!this.isEmpty()) {
+            if (null != this.idOfAccount) {
+                criteria.add(Restrictions.eq("idOfAccount", this.idOfAccount));
+            }
+            if (!this.contract.isEmpty()) {
+                Contract contract = (Contract) session
+                        .load(Contract.class, this.contract.getIdOfContract());
+                criteria.add(Restrictions.eq("contract", contract));
+            }
+            if (!this.org.isEmpty()) {
+                Org org = (Org) session.load(Org.class, this.org.getIdOfOrg());
+                criteria.createCriteria("client").add(Restrictions.eq("org", org));
+            }
+        }
+        return criteria.list();*/
+        return Collections.EMPTY_LIST;
+    }
+}
