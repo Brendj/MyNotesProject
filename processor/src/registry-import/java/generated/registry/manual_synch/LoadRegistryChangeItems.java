e756657f058d284f2988a2564345e3142837cb5e
chirikov <chirikov@example.com>
2013-11-05 14:26:41 +0000

    ECAFE-1183 	Добавление возможности "Принимать", "Отклонять", "Сообщать об ошибке" при интеграции с АИС Реестры

---
+++ processor/src/registry-import/java/generated/registry/manual_synch/LoadRegistryChangeItems.java
@@ -0,0 +1,71 @@
+
+package generated.registry.manual_synch;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for loadRegistryChangeItems complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="loadRegistryChangeItems">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="idOfOrg" type="{http://www.w3.org/2001/XMLSchema}long"/>
+ *         &lt;element name="revisionDate" type="{http://www.w3.org/2001/XMLSchema}long"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "loadRegistryChangeItems", propOrder = {
+    "idOfOrg",
+    "revisionDate"
+})
+public class LoadRegistryChangeItems {
+
+    protected long idOfOrg;
+    protected long revisionDate;
+
+    /**
+     * Gets the value of the idOfOrg property.
+     * 
+     */
+    public long getIdOfOrg() {
+        return idOfOrg;
+    }
+
+    /**
+     * Sets the value of the idOfOrg property.
+     * 
+     */
+    public void setIdOfOrg(long value) {
+        this.idOfOrg = value;
+    }
+
+    /**
+     * Gets the value of the revisionDate property.
+     * 
+     */
+    public long getRevisionDate() {
+        return revisionDate;
+    }
+
+    /**
+     * Sets the value of the revisionDate property.
+     * 
+     */
+    public void setRevisionDate(long value) {
+        this.revisionDate = value;
+    }
+
+}
---
+++ processor/src/registry-import/java/generated/registry/manual_synch/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://ru.axetta.ecafe")
+package generated.registry.manual_synch;
