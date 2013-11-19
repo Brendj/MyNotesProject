28ef1dd5119ba9021c00c237cd049f9c3d58e085
kadyrov <kadyrov@example.com>
2013-11-19 11:35:40 +0000

    Поправлены скрипты

--- processor/src/core/java/db/update_2.2.50-2.2.51.sql
+++ processor/src/core/java/db/update_2.2.50-2.2.51.sql
@@ -2,9 +2,14 @@
 --! после финализации они уберутся
 --! Информация для разработчика -- информация для пользователя
 
+-- Пакет обновлений 2.2.50
+
 -- Добавление протоколирования контрагента в БД для возможности последующего фильтра
 alter table CF_ReportInfo add column IdOfContragent bigint default null;
 alter table CF_ReportInfo add column Contragent varchar(128) default null;
 
 -- Добавлено поле ИНН в накладной
 ALTER TABLE CF_WayBills ADD COLUMN inn character varying(32);
+
+-- Добавлен хэшкод для сранения меню при синхронизации
+ALTER TABLE CF_Menu ADD COLUMN DetailsHashCode int;
