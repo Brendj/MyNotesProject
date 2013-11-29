215f2d2b2396c1e00c0172337468aa61d2321032
kadyrov <kadyrov@example.com>
2013-11-29 11:58:08 +0000

    https://bugtracker.axetta.ru:9901/browse/ECAFE-1426 Поправил отчет для Детских садиков (Появляется ФИО представителя если он присутствует в проходе) https://bugtracker.axetta.ru:9901/browse/ECAFE-1425 Поправлена логика выборки отчета AutoEnterEventReport, ранее строго шла выборка от начало до конца текущего месяца. Теперь период выборки необходимо указывать.

--- processor/src/core/java/ru/axetta/ecafe/processor/core/report/AutoEnterEventReport.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/report/AutoEnterEventReport.java
@@ -10,15 +10,16 @@ import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 import ru.axetta.ecafe.processor.core.RuleProcessor;
-import ru.axetta.ecafe.processor.core.persistence.ClientGroup;
-import ru.axetta.ecafe.processor.core.persistence.EnterEvent;
-import ru.axetta.ecafe.processor.core.persistence.Org;
-import ru.axetta.ecafe.processor.core.persistence.RuleCondition;
+import ru.axetta.ecafe.processor.core.persistence.*;
 import ru.axetta.ecafe.processor.core.utils.CalendarUtils;
 
 
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,10 +47,12 @@ public class AutoEnterEventReport extends BasicReportForOrgJob {
         public class Event implements Comparable<Event>{
             private Long time; // время события
             private int passdirection; // направление (вошел, вышел)
+            private String guardianFIO; // ФИО представителя для садиков
 
-            public Event(Long time, int passdirection) {
+            public Event(Long time, int passdirection, String guardianFIO) {
                 this.time = time;
                 this.passdirection = passdirection;
+                this.guardianFIO = guardianFIO;
             }
 
             public Long getTime() {
@@ -68,12 +71,19 @@ public class AutoEnterEventReport extends BasicReportForOrgJob {
                 this.passdirection = passdirection;
             }
 
+            public String getGuardianFIO() {
+                return guardianFIO;
+            }
+
+            public void setGuardianFIO(String guardianFIO) {
+                this.guardianFIO = guardianFIO;
+            }
+
             @Override
             public int compareTo(Event event) {
                 return this.getTime().compareTo(event.getTime());
             }
         }
-
 
         public static class ReportItem {
 
@@ -121,7 +131,7 @@ public class AutoEnterEventReport extends BasicReportForOrgJob {
 
             /**
              * Метод доступа к eventList. Отличается от getEventList() тем, что проверяет флаг упорядоченности.
-             * Если флан установлен в значение fasle, выполняет сортировку.
+             * Если флан установлен в значение false, выполняет сортировку.
              * @return
              */
             public ArrayList<Event> getSortedEventList() {
@@ -180,7 +190,12 @@ public class AutoEnterEventReport extends BasicReportForOrgJob {
                 StringBuilder sb = new StringBuilder();
                 for (Event e : this.getSortedEventList()) {
                     if (e.getPassdirection() == EnterEvent.ENTRY || e.getPassdirection() == EnterEvent.EXIT) {
-                        sb.append(timeFormat.format(new Date(e.getTime()))).append((e.getPassdirection() == EnterEvent.ENTRY?" (+)":" (-)")).append(", ");
+                        if(StringUtils.isNotEmpty(e.getGuardianFIO())){
+                            sb.append(e.getGuardianFIO()).append(" ");
+                        }
+                        sb.append(timeFormat.format(new Date(e.getTime())));
+                        sb.append((e.getPassdirection() == EnterEvent.ENTRY?" (+)":" (-)"));
+                        sb.append(", ");
                     }
                 }
                 if (sb.length()>2)
@@ -230,15 +245,17 @@ public class AutoEnterEventReport extends BasicReportForOrgJob {
             }
 
             Query query = session.createSQLQuery(
-                    "SELECT p.firstname, p.surname, p.secondname, e.evtdatetime, e.passdirection, c.idOfClient, g.groupname "
-                    + "FROM cf_enterevents e, cf_clients c, cf_persons p, cf_clientgroups g "
-                    + "WHERE e.idofclient=c.idofclient and p.idofperson=c.idofperson AND e.idoforg=:idOfOrg AND g.idofclientgroup=c.idofclientgroup AND g.idoforg=c.idoforg "
-                    + "AND e.evtdatetime>=:startTime AND e.evtdatetime<=:endTime "
+                    "SELECT p.firstname, p.surname, p.secondname, e.evtdatetime, e.passdirection, c.idOfClient, g.groupname, e.guardianid "
+                    + " FROM cf_enterevents e, cf_clients c, cf_persons p, cf_clientgroups g "
+                    + " WHERE e.idofclient=c.idofclient and p.idofperson=c.idofperson AND e.idoforg=:idOfOrg AND g.idofclientgroup=c.idofclientgroup AND g.idoforg=c.idoforg "
+                    + " AND e.evtdatetime>=:startTime AND e.evtdatetime<=:endTime "
                     + typeCondition+ " AND c.idOfClientGroup!=1100000060 "
-                    + "order by g.groupname, e.evtdatetime/(1000*60*60*24), p.surname, p.firstname, p.secondname;");
+                    + " order by g.groupname, e.evtdatetime/(1000*60*60*24), p.surname, p.firstname, p.secondname;");
 
-            query.setParameter("startTime", CalendarUtils.getTimeFirstDayOfMonth(startTime.getTime()));
-            query.setParameter("endTime", CalendarUtils.getTimeLastDayOfMonth(startTime.getTime()));
+            //query.setParameter("startTime", CalendarUtils.getTimeFirstDayOfMonth(startTime.getTime()));
+            //query.setParameter("endTime", CalendarUtils.getTimeLastDayOfMonth(startTime.getTime()));
+            query.setParameter("startTime", startTime.getTime());
+            query.setParameter("endTime", endTime.getTime());
             query.setParameter("idOfOrg", org.getIdOfOrg());
 
             List resultList = query.list();
@@ -253,6 +270,31 @@ public class AutoEnterEventReport extends BasicReportForOrgJob {
                 Integer passdirection = Integer.parseInt(vals[4].toString());
                 Long idOfClient = Long.parseLong(vals[5].toString());
                 String groupName = (String)vals[6];
+                String guardianFIO=null;
+                if(vals[7]!=null && StringUtils.isNotEmpty(vals[7].toString())){
+                    Long guardianId = Long.parseLong(vals[7].toString());
+                    Criteria guardianCriteria = session.createCriteria(Client.class);
+                    guardianCriteria.createAlias("person","p");
+                    guardianCriteria.add(Restrictions.eq("idOfClient",guardianId));
+                    guardianCriteria.add(Restrictions.isNotNull("person"));
+                    guardianCriteria.setProjection(Projections.projectionList().add(Projections.property("p.surname"))
+                            .add(Projections.property("p.firstName")).add(Projections.property("p.secondName")));
+                    Object o1 = guardianCriteria.uniqueResult();
+                    if(o1!=null) {
+                        Object[] fields = (Object[]) o1;
+                        StringBuilder stringBuilder = new StringBuilder();
+                        String guardianSurname = fields[0]==null?"":fields[0].toString();
+                        String guardianFirstname = fields[1]==null?"":fields[1].toString();
+                        String guardianSecondname = fields[2]==null?"":fields[2].toString();
+                        if (StringUtils.isNotEmpty(guardianSurname))
+                            stringBuilder.append(guardianSurname).append(' ');
+                        if (StringUtils.isNotEmpty(guardianFirstname))
+                            stringBuilder.append(guardianFirstname.trim().charAt(0)).append(". ");
+                        if (StringUtils.isNotEmpty(guardianSecondname))
+                            stringBuilder.append(guardianSecondname.trim().charAt(0)).append('.');
+                        guardianFIO = stringBuilder.toString();
+                    }
+                }
                 c.setTimeInMillis(time);
                 int day = c.get(Calendar.DAY_OF_MONTH);
 
@@ -274,7 +316,7 @@ public class AutoEnterEventReport extends BasicReportForOrgJob {
                     tmpMap.put(day, reportItem);
                     resultRows.add(reportItem);
                 }
-                reportItem.getEventList().add(new Event(time, passdirection));
+                reportItem.getEventList().add(new Event(time, passdirection, guardianFIO));
             }
             return new JRBeanCollectionDataSource(resultRows);
         }
