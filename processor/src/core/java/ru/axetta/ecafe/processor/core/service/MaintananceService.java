c5fcaf347cd8debff5e2c59677676c7bacae68a2
kalimullin <kalimullin@example.com>
2013-12-03 13:19:54 +0000

    ECAFE-1451 На московском сервере подвисает запрос на очистку меню.
    Исправлено.

--- processor/src/core/java/ru/axetta/ecafe/processor/core/service/MaintananceService.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/service/MaintananceService.java
@@ -18,8 +18,8 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import java.util.Calendar;
-import java.util.Date;
+import java.math.BigInteger;
+import java.util.*;
 
 @Component
 @Scope("singleton")
@@ -78,7 +78,8 @@ public class MaintananceService {
         }
     }
 
-    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
+    @SuppressWarnings("unchecked")
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.SUPPORTS, readOnly = true)
     public String clean(boolean isSource) throws Exception {
         long menuDaysForDeletion = runtimeContext.getOptionValueInt(
                 isSource ? Option.OPTION_SRC_ORG_MENU_DAYS_FOR_DELETION : Option.OPTION_MENU_DAYS_FOR_DELETION);
@@ -89,27 +90,53 @@ public class MaintananceService {
 
         String orgFilter = (isSource ? "" : "not") + " in (select distinct mer.idOfSourceOrg from CF_MenuExchangeRules mer)";
 
-        logger.info("Cleaning menu details...");
-        Query query = entityManager.createNativeQuery("delete from CF_MenuDetails md where md.IdOfMenu in \n" +
-                "(select m.IdOfMenu from CF_Menu m where m.IdOfOrg " + orgFilter + "and m.MenuDate < :date)")
+        Query query = entityManager.createNativeQuery(
+                "select m.IdOfMenu, m.IdOfOrg from CF_Menu m where m.IdOfOrg " + orgFilter + " and m.MenuDate < :date")
                 .setParameter("date", timeToClean);
-        int menuDetailDeletedCount = query.executeUpdate();
+        List<Object[]> records = query.getResultList();
+        Set<Long> orgIds = new HashSet<Long>();
+        MaintananceService proxy = getProxy();
 
-        logger.info("Cleaning menu...");
-        query = entityManager
-                .createNativeQuery("delete from CF_Menu m where m.IdOfOrg " + orgFilter + " and m.MenuDate < :date")
-                .setParameter("date", timeToClean);
-        int menuDeletedCount = query.executeUpdate();
+        logger.info("Cleaning menu details and menu...");
+        int menuDetailDeletedCount = 0;
+        int menuDeletedCount = 0;
+        for (Object[] row : records) {
+            Long idOfMenu = ((BigInteger) row[0]).longValue();
+            orgIds.add(((BigInteger) row[1]).longValue());
+            int[] res = proxy.cleanMenuInternal(idOfMenu);
+            menuDetailDeletedCount += res[0];
+            menuDeletedCount += res[1];
+        }
 
         logger.info("Cleaning menu exchange...");
-        query = entityManager.createNativeQuery("delete from CF_MenuExchange me where me.IdOfOrg " + orgFilter
-                + " and me.MenuDate < :date and me.menuDate <> :nullDate")
-                .setParameter("date", timeToClean).setParameter("nullDate", 0);
-        // исключаем меню с секцией Settings, у него нулевая дата
-        int menuExchangeDeletedCount = query.executeUpdate();
+        int menuExchangeDeletedCount = 0;
+        for (Long idOfOrg : orgIds) {
+            menuExchangeDeletedCount += proxy.cleanMenuExchange(idOfOrg, timeToClean);
+        }
 
         return ("Deleted all records before - " + new Date(timeToClean) + ", deleted records count: menu - "
                 + menuDeletedCount +
                 ", menu detail - " + menuDetailDeletedCount + ", menu exchange - " + menuExchangeDeletedCount);
+    }
+
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
+    public int[] cleanMenuInternal(Long idOfMenu) {
+        int[] res = new int[2];
+        Query detailsQuery = entityManager
+                .createNativeQuery("DELETE FROM CF_MenuDetails md WHERE md.IdOfMenu = :idOfMenu");
+        Query menuQuery = entityManager.createNativeQuery("DELETE FROM CF_Menu m WHERE m.IdOfMenu = :idOfMenu");
+        detailsQuery.setParameter("idOfMenu", idOfMenu);
+        res[0] = detailsQuery.executeUpdate();
+        menuQuery.setParameter("idOfMenu", idOfMenu);
+        res[1] = menuQuery.executeUpdate();
+        return res;
+    }
+
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
+    public int cleanMenuExchange(Long idOfOrg, long timeToClean) {
+        Query query = entityManager.createNativeQuery("DELETE FROM CF_MenuExchange me WHERE me.IdOfOrg = :idOfOrg"
+                + " AND me.MenuDate < :date AND me.menuDate <> :nullDate");
+        query.setParameter("idOfOrg", idOfOrg).setParameter("date", timeToClean).setParameter("nullDate", 0);
+        return query.executeUpdate();
     }
 }
