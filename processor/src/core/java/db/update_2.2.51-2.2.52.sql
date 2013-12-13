b967d48042bd99e5a7e4c050400363031bc243c9
kadyrov <kadyrov@example.com>
2013-12-13 10:23:46 +0000

    Финализирована 52 версия процесинга

--- processor/src/core/java/db/update_2.2.51-2.2.52.sql
+++ processor/src/core/java/db/update_2.2.51-2.2.52.sql
@@ -9,12 +9,6 @@ alter table cf_goods_requests_positions add column DailySampleCount bigint defau
 alter table cf_goods_requests_positions add column UpdateHistory text default null;
 alter table cf_contragents add column RequestNotifyMailList character varying(1024) default null;
 
-CREATE TABLE cf_do_org_current_version (
-  IdDOOrgCurrentVersion bigserial NOT NULL,
-  ObjectId integer not null,
-  IdOfOrg bigint not null,
-  LastVersion bigint not null,
-  CONSTRAINT cf_do_org_current_version_pk PRIMARY KEY (IdDOOrgCurrentVersion)
-);
-
 alter table cf_users add column region varchar(10) default null;
+
+--! ФИНАЛИЗИРОВАН (Кадыров, 131213) НЕ МЕНЯТЬ
