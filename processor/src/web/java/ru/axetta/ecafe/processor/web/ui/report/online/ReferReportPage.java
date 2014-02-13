9973413407434a0b500357499dd70493daedad7b
chirikov <chirikov@example.com>
2014-02-13 11:45:50 +0000

    ECAFE-1557 Реализовать сводный отчет по бюджетному питанию в городе

--- processor/src/web/java/ru/axetta/ecafe/processor/web/ui/report/online/ReferReportPage.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/ui/report/online/ReferReportPage.java
@@ -138,6 +138,8 @@ public class ReferReportPage extends OnlineReportPage {
             cal.set(Calendar.SECOND, 0);
             cal.set(Calendar.MILLISECOND, 0);
 
+            endDate = updateEndDate(endDate);
+
             session = (Session) entityManager.getDelegate();
             BasicReportJob.OrgShortItem orgItem = getOrgItem();
             switch (reportType) {
@@ -178,6 +180,15 @@ public class ReferReportPage extends OnlineReportPage {
         } catch (Exception e) {
             logger.error("Failed to generate daily report", e);
         }
+    }
+
+    public Date updateEndDate(Date endDate) {
+        Calendar cal = new GregorianCalendar();
+        cal.setTimeInMillis(endDate.getTime());
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        return cal.getTime();
     }
 
     public BasicReportJob.OrgShortItem getOrgItem() {
