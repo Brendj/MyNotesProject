d78c416237735bcade48a0e3fc2837c1509d7e7d
kadyrov <kadyrov@example.com>
2014-06-09 14:34:39 +0000

    Финализирован скрипт 2.2.65.140609

--- processor/src/core/java/db/update_2.2.64-2.2.65.sql
+++ processor/src/core/java/db/update_2.2.64-2.2.65.sql
@@ -8,8 +8,6 @@
 ALTER TABLE cf_userorgs ADD COLUMN usernotificationtype integer;
 UPDATE cf_UserOrgs SET UserNotificationType = 0;
 ALTER TABLE cf_userorgs ALTER COLUMN usernotificationtype SET NOT NULL;
-
-
 ALTER TABLE cf_UserOrgs DROP CONSTRAINT cf_userorgs_uq;
-
 ALTER TABLE cf_UserOrgs ADD CONSTRAINT cf_userorgs_uq UNIQUE (idofuser, idoforg, userNotificationType);
+--! ФИНАЛИЗИРОВАН (Кадыров, 140609) НЕ МЕНЯТЬ
