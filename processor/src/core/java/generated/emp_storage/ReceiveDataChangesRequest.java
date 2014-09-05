d3227e1bcc0f025f0ea95c5f39302f59fd87b7fd
chirikov <chirikov@example.com>
2014-09-05 14:45:38 +0000

    ECAFE-2023 	Перевод на новое взаимодествие с ЕМП

--- processor/src/core/java/generated/emp_storage/ReceiveDataChangesRequest.java
+++ processor/src/core/java/generated/emp_storage/ReceiveDataChangesRequest.java
@@ -1,10 +1,7 @@
 
 package generated.emp_storage;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 
 
 /**
@@ -28,6 +25,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  * 
  */
+@XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "ReceiveDataChangesRequest", namespace = "http://emp.mos.ru/schemas/storage/request/entry.xsd", propOrder = {
     "catalogName",
