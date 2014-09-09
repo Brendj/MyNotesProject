d8cf8d1e857636083edf3bf79be5bbf9a4ebdaec
anvarov <anvarov@example.com>
2014-09-09 08:39:28 +0000

    ELIBRARY-239 Синхронизация таблицы Instances(переделан ключ уникальности полей)

--- processor/src/core/java/db/update_2.2.71-2.2.72.sql
+++ processor/src/core/java/db/update_2.2.71-2.2.72.sql
@@ -1,2 +1,12 @@
+--! Пока скрипт не финализирован рекомендовано писать очистку добавляемых колонок таблиц.
+--! после финализации они уберутся
+--! Информация для разработчика -- информация для пользователя
+
+-- Пакет обновлений 2.2.72
+
 -- Добавление поля даты последней загрузки платежей из РНИП
 ALTER TABLE cf_contragents ADD COLUMN LastRNIPUpdate VARCHAR(15) NOT NULL DEFAULT '';
+
+-- Новое условие уникальности по полям
+ALTER TABLE cf_instances DROP CONSTRAINT cf_instances_invbook_invnumber_key;
+ALTER TABLE cf_instances ADD CONSTRAINT cf_instances_invbook_invnumber_key UNIQUE (invbook , invnumber, deletedstate);
