bc55d34802d9ea54a209c867be5593c1c140f409
sungatov <sungatov@example.com>
2014-10-08 18:12:17 +0000

    Release 2.2.75

--- processor/src/core/java/db/update_2.2.74-2.2.75.sql
+++ processor/src/core/java/db/update_2.2.74-2.2.75.sql
@@ -1,3 +1,5 @@
 
 --! Расширение колонки - некоторые данные не влезают в колонку
 ALTER TABLE cf_contragents ALTER COLUMN LastRNIPUpdate TYPE VARCHAR(30);
+
+--! ФИНАЛИЗИРОВАН (Сунгатов, 141008) НЕ МЕНЯТЬ
