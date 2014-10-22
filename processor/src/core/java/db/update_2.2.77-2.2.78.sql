077881fe0cf7c70c6e843fcbff655c9867e90f1a
sungatov <sungatov@example.com>
2014-10-22 08:53:07 +0000

    Release 2.2.76

--- processor/src/core/java/db/update_2.2.77-2.2.78.sql
+++ processor/src/core/java/db/update_2.2.77-2.2.78.sql
@@ -1,10 +1,9 @@
--- Изменение таблицы соответсвия оборудования школам
-alter table cf_org_accessories add column IdOfAccessory BIGINT NOT NULL;
-alter table cf_org_accessories rename column ACCESSORY_TYPE to AccessoryType;
-alter table cf_org_accessories rename column ACCESSORY_NUMBER to AccessoryNumber;
-alter table cf_org_accessories alter column AccessoryNumber type VARCHAR(40);
-alter table cf_org_accessories drop constraint cf_org_accessories_pk;
-alter table cf_org_accessories add constraint cf_org_accessories_pk PRIMARY KEY (IdOfAccessory);
-alter table cf_org_accessories add CONSTRAINT cf_org_accessories_unique UNIQUE (IdOfAccessory, IdOfSourceOrg, IdOfTargetOrg, AccessoryType, AccessoryNumber);
+--! Пока скрипт не финализирован рекомендовано писать очистку добавляемых колонок таблиц.
+--! после финализации они уберутся
+--! Информация для разработчика -- информация для пользователя
+
+-- Пакет обновлений 2.2.78
 -- Добавление генератора идентифиактора для оборудования корпусов
 ALTER TABLE CF_Generators ADD COLUMN IdOfAccessory BIGINT NOT NULL DEFAULT 0;
+
+--! ФИНАЛИЗИРОВАН (Сунгатов, 141021) НЕ МЕНЯТЬ
