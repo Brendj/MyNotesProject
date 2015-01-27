a2afc90d7f1b47bb733b868b080a1398f850ff4b
anvarov <anvarov@example.com>
2015-01-27 16:29:10 +0000

    ECAFE-2207 Реализовать отчетную форму "Детализированный отчет отклонений оплаты льготного питания". (без корпусов)

---
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/report/statistics/discrepancies/deviations/payment/without/corps/DetailedDeviationsWithoutCorpsJasperReport.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015. Axetta LLC. All Rights Reserved.
+ */
+
+package ru.axetta.ecafe.processor.core.report.statistics.discrepancies.deviations.payment.without.corps;
+
+import ru.axetta.ecafe.processor.core.report.BasicReportForAllOrgJob;
+
+import org.slf4j.Logger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Алмаз
+ * Date: 27.01.15
+ * Time: 19:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class DetailedDeviationsWithoutCorpsJasperReport extends BasicReportForAllOrgJob {
+
+    @Override
+    public BasicReportForAllOrgJob createInstance() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Builder createBuilder(String templateFilename) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Logger getLogger() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
