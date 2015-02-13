33e939871f1c9dc2fbe0a4def20212ff42798345
sungatov <sungatov@example.com>
2015-02-13 12:31:08 +0000

    ECAFE-2468     Processing. String index out of range: 0

--- processor/src/core/java/ru/axetta/ecafe/processor/core/report/model/autoenterevent/StClass.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/report/model/autoenterevent/StClass.java
@@ -30,20 +30,43 @@ public class StClass implements Comparable<StClass> {
 
     @Override
     public int compareTo(StClass o) {
-        String numThisString = getName().replaceAll("[^\\d]", "");
-        String numOString = o.getName().replaceAll("[^\\d]", "");
-        if(numThisString.length() < 1) return -1;
-        if(numOString.length() < 1) return 1;
+        if(this.getName().length() < o.getName().length()) {
+            return -1;
+        }else if(this.getName().length() < o.getName().length()) {
+            return 1;
+        }
 
+        String oOrgName = o.name;
+        String thisOrgName = this.name;
+        String numThisString = thisOrgName.replaceAll("[^\\d]", "");
+        String numOString = oOrgName.replaceAll("[^\\d]", "");
+        int stringCompareResult = ((Integer)numThisString.length()).compareTo(numOString.length());
+        if( stringCompareResult!= 0){
+            return stringCompareResult;
+        }
 
         Integer numThis = Integer.valueOf(numThisString);
         Integer numO = Integer.valueOf(numOString);
-        String sThis = getName().replaceAll("[^\\D]", "").toUpperCase();
-        String sO = o.getName().replaceAll("[^\\D]", "").toUpperCase();
-        if ( numThis.compareTo(numO) != 0) {
+        if(numThis.equals(numO)){
+            String sThis = thisOrgName.replaceAll("[^\\D]", "").toUpperCase();
+            String sO = oOrgName.replaceAll("[^\\D]", "").toUpperCase();
+            Integer letterThis = printSum(sThis);
+            Integer letterO = printSum(sO);
+            return  letterThis.compareTo(letterO);
+        }else{
             return numThis.compareTo(numO);
         }
-        return Character.getNumericValue(sThis.charAt(0)) > Character.getNumericValue(sO.charAt(0)) ? 1 : -1;
+    }
+
+    private static int printSum(String original){
+        int sum = 0;
+        if(original!=null){
+            char[] arr = original.toLowerCase().toCharArray();
+            for(int x :arr){
+                sum+= (x-96);
+            }
+        }
+        return sum;
     }
 
     public String getName() {
