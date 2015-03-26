317788c7c9e565aa7a5c01eb491c1539bc448389
anvarov <anvarov@example.com>
2015-03-26 08:10:33 +0000

    generateXmlReport - изменения в базе

--- processor/src/core/java/db/update_2.2.92-2.2.93.sql
+++ processor/src/core/java/db/update_2.2.92-2.2.93.sql
@@ -10,3 +10,7 @@ ALTER TABLE cf_account_operations ADD COLUMN IdOfClientPayment bigint;
 
 -- --! Добавление поля notifyviapush в таблицу cf_clients
 ALTER TABLE cf_clients ADD COLUMN notifyviapush smallint NOT NULL DEFAULT 0;
+
+--!отчет xml
+alter table cf_daily_org_registries alter column officialname type VARCHAR(256);
+alter table cf_daily_org_registries alter column address type VARCHAR(256);
