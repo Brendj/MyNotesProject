aa435f37ec96eda61ae90c33086da1a0de12084b
anvarov <anvarov@example.com>
2015-07-23 11:38:59 +0000

    EP-167 Реализация нового метода для вывода состава проданных блюд - getPurchaseListWithDetails

---
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/partner/integra/dataflow/PurchaseListWithDetailsExt.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2015. Axetta LLC. All Rights Reserved.
+ */
+
+package ru.axetta.ecafe.processor.web.partner.integra.dataflow;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: anvarov
+ * Date: 23.07.15
+ * Time: 13:35
+ */
+
+public class PurchaseListWithDetailsExt {
+
+}
