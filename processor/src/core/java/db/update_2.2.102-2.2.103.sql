8ede06b604fa8fb5b42a7462ac4889a4ee7519e4
Ivan Semenov <semenov@axetta.ru>
2015-10-05 20:33:54 +0000

    EP-256 Требуется устранить проблему обновления PostgreSQL версии 9.0.4

--- processor/src/core/java/db/update_2.2.102-2.2.103.sql
+++ processor/src/core/java/db/update_2.2.102-2.2.103.sql
@@ -4,9 +4,9 @@
 
 -- Пакет обновлений 2.2.102
 
-CREATE INDEX cf_clients_phone_idx ON cf_clients USING btree (phone COLLATE pg_catalog."default");
+CREATE INDEX cf_clients_phone_idx ON cf_clients USING btree (phone);
 
-CREATE INDEX cf_clients_mobile_idx ON cf_clients USING btree (mobile COLLATE pg_catalog."default");
+CREATE INDEX cf_clients_mobile_idx ON cf_clients USING btree (mobile);
 
 -- Увеличение размера поля статистики
 alter table cf_external_system_stats alter column StatisticValue type DECIMAL(20, 4);
