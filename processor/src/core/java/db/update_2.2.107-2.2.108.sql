755080c5b6ddcf4394b228688a2c403c45b7cd36
anvarov <anvarov@example.com>
2015-12-15 14:19:27 +0000

    EP-272 Дорабатываем форму "Расписание отчетов"

--- processor/src/core/java/db/update_2.2.107-2.2.108.sql
+++ processor/src/core/java/db/update_2.2.107-2.2.108.sql
@@ -32,7 +32,5 @@ CREATE TABLE CF_JobRules
   idOfReportHandleRule bigint NOT NULL,
   idOfSchedulerJob bigint NOT NULL,
   CONSTRAINT cf_IdOfJobRule_pk PRIMARY KEY (idOfJobRule),
-  CONSTRAINT cf_JobRules_ReportHandleRule_fk FOREIGN KEY (idOfReportHandleRule) REFERENCES CF_ReportHandleRules (idOfReportHandleRule),
-  CONSTRAINT cf_JobRules_SchedulerJob_fk FOREIGN KEY (idOfSchedulerJob) REFERENCES CF_SchedulerJobs (idOfSchedulerJob),
   CONSTRAINT cf_JobRules_uq UNIQUE (idOfReportHandleRule, idOfSchedulerJob)
 );
