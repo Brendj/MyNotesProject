64eafeaac17465c7cf54101bfb690c4abac8d851
anvarov <anvarov@example.com>
2016-02-15 12:24:17 +0000

    EP-407 Добавить новое поле в cf_orgs Короткий адрес

--- processor/src/core/java/db/update_2.2.110-2.2.111.sql
+++ processor/src/core/java/db/update_2.2.110-2.2.111.sql
@@ -20,3 +20,14 @@ ALTER TABLE cf_clients ADD COLUMN uselasteemodeforplan integer;
 alter table cf_orgs ADD COLUMN lastupdate bigint;
 --! Изначальная дата последнего изменения установлена на 10.02.2016 00:00:00.000
 update cf_orgs set lastupdate=1455051600000;
+
+
+--! Добавление нового поля в cf_orgs  'Короткий адрес' - shortaddress
+ALTER TABLE cf_orgs ADD COLUMN shortaddress CHARACTER VARYING(128);
+
+UPDATE cf_orgs AS o
+SET shortaddress = i.shortaddress FROM (SELECT
+                                          idoforg,
+                                          trim(FROM substring(address FROM '%/%/#"%/%#"' FOR '#')) AS shortaddress
+                                        FROM cf_orgs) i
+WHERE i.idoforg = o.idoforg;
