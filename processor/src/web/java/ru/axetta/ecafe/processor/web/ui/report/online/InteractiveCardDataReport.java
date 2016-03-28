5aaa80b1b46fc46bc76bdfd4f6810ff1d4120d6a
anvarov <anvarov@example.com>
2016-03-28 13:03:50 +0000

    ECAFE-2989 Синхронизировать атрибуты формы учета карт с клиентом

--- processor/src/web/java/ru/axetta/ecafe/processor/web/ui/report/online/InteractiveCardDataReport.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/ui/report/online/InteractiveCardDataReport.java
@@ -4,14 +4,19 @@
 
 package ru.axetta.ecafe.processor.web.ui.report.online;
 
+import ru.axetta.ecafe.processor.core.RuntimeContext;
 import ru.axetta.ecafe.processor.core.report.BasicReportForAllOrgJob;
+import ru.axetta.ecafe.processor.core.report.BasicReportJob;
+import ru.axetta.ecafe.processor.core.report.TransactionsReport;
 
+import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -25,21 +30,75 @@ public class InteractiveCardDataReport extends BasicReportForAllOrgJob {
     private final static Logger logger = LoggerFactory.getLogger(InteractiveCardDataReport.class);
     public static DateFormat dailyItemsFormat = new SimpleDateFormat("dd.MM.yyyy");
 
-    private Date reportDate;
+    //protected List<InteractiveCardDataReportItem> items;
+    private String htmlReport;
+
+    public class AutoReportBuildJob extends BasicReportJob.AutoReportBuildJob {
+    }
+
+    public static class Builder extends BasicReportForAllOrgJob.Builder {
+        private final String templateFilename;
+
+        public Builder(String templateFilename) {
+            this.templateFilename = templateFilename;
+        }
+
+        public Builder() {
+            String templateName = InteractiveCardDataReport.class.getSimpleName();
+            templateFilename = RuntimeContext.getInstance().getAutoReportGenerator().getReportsTemplateFilePath() + templateName + ".jasper";
+        }
+
+        @Override
+        public InteractiveCardDataReport build(Session session, Date startTime, Date endTime, Calendar calendar) throws Exception {
+            return doBuild(session, calendar);
+        }
+
+        public InteractiveCardDataReport doBuild(Session session, Calendar calendar) throws Exception {
+                        Date generateTime = new Date();
+
+            /* Строим параметры для передачи в jasper */
+            Map<String, Object> parameterMap = new HashMap<String, Object>();
+            calendar.setTime(generateTime);
+            int month = calendar.get(Calendar.MONTH);
+            parameterMap.put("reportDate", dailyItemsFormat.format(generateTime));
+
+            return null;
+        }
+
+
+    }
 
     @Override
     public BasicReportForAllOrgJob createInstance() {
-        return null;
+        return new TransactionsReport();
     }
 
     @Override
     public Builder createBuilder(String templateFilename) {
-        return null;
+        return new Builder(templateFilename);
     }
 
     @Override
     public Logger getLogger() {
         return logger;
+    }
+
+    @Override
+    public int getDefaultReportPeriod() {
+        return REPORT_PERIOD_PREV_MONTH;
+    }
+
+    public String getHtmlReport() {
+        return htmlReport;
+    }
+
+    public InteractiveCardDataReport setHtmlReport(String htmlReport) {
+        this.htmlReport = htmlReport;
+        return this;
+    }
+
+    public static final class InteracticeCardDataReportItem {
+        protected long idOfRule;
     }
 }
 
--- processor/src/web/java/ru/axetta/ecafe/processor/web/ui/report/online/InteractiveCardDataReportPage.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/ui/report/online/InteractiveCardDataReportPage.java
@@ -5,14 +5,21 @@
 package ru.axetta.ecafe.processor.web.ui.report.online;
 
 
+import ru.axetta.ecafe.processor.core.RuntimeContext;
+import ru.axetta.ecafe.processor.core.report.BasicReportJob;
+
+import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.awt.event.ActionEvent;
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -40,12 +47,48 @@ public class InteractiveCardDataReportPage extends OnlineReportPage {
     }
 
     public void doGenerate() {
-
+        RuntimeContext.getAppContext().getBean(InteractiveCardDataReportPage.class).generate();
     }
 
     public void doGenerateXLS(ActionEvent actionEvent) {
-
+        RuntimeContext.getAppContext().getBean(InteractiveCardDataReportPage.class).generateXLS();
     }
+
+    @Transactional
+    public void generate() {
+        Session session;
+        try {
+            session = (Session) entityManager.getDelegate();
+            generateReport(session, null);
+        } catch (Exception e) {
+               logger.error("Failed to load clients data", e);
+        } finally {
+            //HibernateUtils.close(session, logger);
+        }
+    }
+
+    @Transactional
+    public void generateXLS() {
+        Session session = null;
+        try {
+            session = (Session) entityManager.getDelegate();
+            generateXLS(session);
+        } catch (Exception e) {
+            logger.error("Failed to load clients data", e);
+        } finally {
+            //HibernateUtils.close(session, logger);
+        }
+    }
+
+    public void generateReport(Session session, String templateFile) throws Exception {
+        InteractiveCardDataReport.Builder reportBuilder = null;
+        if (templateFile != null) {
+            reportBuilder = new InteractiveCardDataReport.Builder(templateFile);
+        }
+    }
+
+    public void generateXLS(Session session) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();}
 
 
 }
