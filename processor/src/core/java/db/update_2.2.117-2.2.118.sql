3ae0ea8dba10ea55a3982f85628007b8bb56d4b2
Ivan Semenov <semenov@axetta.ru>
2016-08-24 15:46:02 +0000

    Release v118

--- processor/src/core/java/db/update_2.2.117-2.2.118.sql
+++ processor/src/core/java/db/update_2.2.117-2.2.118.sql
@@ -7,8 +7,8 @@
 ALTER TABLE CF_RegistryChange ADD COLUMN guardiansCount integer;
 
 ALTER TABLE cf_clientphoto
-  ADD COLUMN iscanceled INTEGER NOT NULL DEFAULT FALSE,
-  ADD COLUMN isapproved INTEGER NOT NULL DEFAULT FALSE,
+  ADD COLUMN iscanceled INTEGER NOT NULL DEFAULT 0,
+  ADD COLUMN isapproved INTEGER NOT NULL DEFAULT 0,
   ADD COLUMN idofclientguardian BIGINT,
   ADD COLUMN lastproceederror CHARACTER VARYING(256),
   ADD COLUMN version BIGINT;
@@ -16,3 +16,5 @@ CREATE INDEX cf_clientphoto_version_idx ON cf_clientphoto USING BTREE (version);
 
 --Флаг "Сверка фотографий" у организации
 ALTER TABLE cf_orgs ADD COLUMN photoregistrydirective integer NOT NULL DEFAULT 0;
+
+--! ФИНАЛИЗИРОВАН (Семенов, 240816) НЕ МЕНЯТЬ
