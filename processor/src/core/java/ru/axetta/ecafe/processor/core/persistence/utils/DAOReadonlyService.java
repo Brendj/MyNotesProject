e8dbe98bf8b4de8d02a264a58cb499937896d75a
blieva <blieva@example.com>
2016-09-05 14:27:35 +0000

    EP-683 Оптимизация методов .getAccountTransactionsForOrgSinceTimeV2 и .findClientByGuid

--- processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/utils/DAOReadonlyService.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/utils/DAOReadonlyService.java
@@ -50,13 +50,14 @@ public class DAOReadonlyService {
     }
 
     public List<AccountTransactionExtended> getAccountTransactionsForOrgSinceTimeV2(Org org,
-            Date fromDateTime, Date toDateTime) {
+            Date fromDateTime, Date toDateTime) throws Exception {
         String str_query = "select t.idOfTransaction, t.source, t.transactionDate, " +
                 "t.sourceType, t.transactionSum,  " +
                 "coalesce(t.transactionSubBalance1Sum, 0) as transactionSubBalance1Sum, coalesce(query.complexsum, 0) as complexsum, " +
                 "coalesce(query.discountsum, 0) as discountsum, coalesce(query.orderType, 0) as ordertype, t.idOfClient " +
                 "from cf_transactions t left join " +
-                "(select coalesce(sum(dd.qty * dd.rprice), 0) as complexsum, coalesce(sum(dd.socDiscount), 0) as discountsum, oo.orderType, oo.idOfTransaction " +
+                "(select coalesce(sum(dd.qty * dd.rprice), 0) as complexsum, coalesce(sum(dd.socDiscount), 0) as discountsum,"
+                + " oo.orderType, oo.idOfTransaction " +
                 "from cf_orders oo join cf_orderdetails dd on oo.idOfOrder = dd.idOfOrder and oo.idOfOrg = dd.idOfOrg " +
                 "where oo.createdDate > :orders_begDate AND oo.createddate <= :orders_endDate AND oo.idOfOrg in (:orgs) " +
                 "AND dd.idOfOrg in (:orgs) AND dd.menuType between :menuMin and :menuMax " +
@@ -65,7 +66,7 @@ public class DAOReadonlyService {
                 "where t.idOfOrg in (:orgs) AND t.transactionDate > :trans_begDate AND t.transactionDate <= :trans_endDate " +
                 "order by t.idOfClient";
         Session session = entityManager.unwrap(Session.class);
-        org = (Org) session.merge(org);
+        //org = (Org) session.merge(org);
         SQLQuery q = session.createSQLQuery(str_query);
         // заказы будем искать за последние 24 часа от времени запроса
         q.setParameter("orders_begDate", CalendarUtils.addDays(toDateTime, -1).getTime());
@@ -73,12 +74,14 @@ public class DAOReadonlyService {
         // транзакции будем искать строго от запрашиваемого времени
         q.setParameter("trans_begDate", fromDateTime.getTime());
         q.setParameter("trans_endDate", toDateTime.getTime());
-        q.setParameterList("orgs", org.getFriendlyOrg());
+        q.setParameterList("orgs",DAOUtils.findFriendlyOrgIds(session, org.getIdOfOrg()));
         q.setParameter("menuMin", OrderDetail.TYPE_COMPLEX_MIN);
         q.setParameter("menuMax", OrderDetail.TYPE_COMPLEX_MAX);
         q.setResultTransformer(Transformers.aliasToBean(AccountTransactionExtended.class));
-        q.addScalar("idoftransaction").addScalar("source").addScalar("transactiondate").addScalar("sourcetype").addScalar("transactionsum").addScalar("transactionsubbalance1sum")
-                .addScalar("complexsum", StandardBasicTypes.BIG_DECIMAL).addScalar("discountsum", StandardBasicTypes.BIG_DECIMAL).addScalar("ordertype")
+        q.addScalar("idoftransaction").addScalar("source").addScalar("transactiondate").addScalar("sourcetype")
+                .addScalar("transactionsum").addScalar("transactionsubbalance1sum")
+                .addScalar("complexsum", StandardBasicTypes.BIG_DECIMAL)
+                .addScalar("discountsum", StandardBasicTypes.BIG_DECIMAL).addScalar("ordertype")
                 .addScalar("idofclient");
         return q.list();
     }
