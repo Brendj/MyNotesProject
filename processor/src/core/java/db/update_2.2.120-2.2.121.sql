3624319f269f2e75882a8240be597ea195a9e3d0
blieva <blieva@example.com>
2016-09-13 11:24:06 +0000

    Индекс lastprocesssections date на idoforg

--- processor/src/core/java/db/update_2.2.120-2.2.121.sql
+++ processor/src/core/java/db/update_2.2.120-2.2.121.sql
@@ -8,4 +8,7 @@
 ALTER TABLE cf_org_accessories
   ADD COLUMN usedsinceseptember boolean NOT NULL DEFAULT false;
 
+CREATE INDEX cf_lastprocesssectionsdates_idoforg_idx
+ON cf_lastprocesssectionsdates USING btree (idoforg);
+
 --! ФИНАЛИЗИРОВАН (Семенов, 120916) НЕ МЕНЯТЬ
