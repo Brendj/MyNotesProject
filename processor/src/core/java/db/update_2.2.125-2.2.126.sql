3f70a69f7bf7046ef337e9719c3e0abfaa388408
Ivan Semenov <semenov@axetta.ru>
2016-11-14 15:39:37 +0000

    Release v126

--- processor/src/core/java/db/update_2.2.125-2.2.126.sql
+++ processor/src/core/java/db/update_2.2.125-2.2.126.sql
@@ -6,3 +6,7 @@
 
 --Индексы на таблицу истории синхронизации
 CREATE INDEX cf_synchistory_daily_syncdate_idx ON cf_synchistory_daily USING btree (syncdate);
+
+CREATE INDEX cf_groupnames_to_orgs_idx ON cf_groupnames_to_orgs USING btree (version);
+
+--! ФИНАЛИЗИРОВАН (Семенов, 141116) НЕ МЕНЯТЬ
