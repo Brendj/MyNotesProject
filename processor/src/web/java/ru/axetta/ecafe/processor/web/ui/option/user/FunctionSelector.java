c291a5977846c501a41c31082511b4011baf5d1d
anvarov <anvarov@example.com>
2017-01-20 13:05:59 +0000

    EP-833 Создать новую роль Отчетность поставщика питания

--- processor/src/web/java/ru/axetta/ecafe/processor/web/ui/option/user/FunctionSelector.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/ui/option/user/FunctionSelector.java
@@ -177,22 +177,12 @@ public class FunctionSelector {
         Criteria allFunctionsCriteria = session.createCriteria(Function.class);
         List allFunctions = allFunctionsCriteria.list();
         Set<Function> supplierReportFunctions = new HashSet<Function>();
-
-        Set<Item> selectedItems = new HashSet<Item>();
-
-        for (Item item: onlineReportItems) {
-            if (item.isSelected()) {
-                selectedItems.add(item);
-            }
-        }
-
         for (Object object : allFunctions) {
             Function function = (Function) object;
 
-            for (Item item: selectedItems) {
-                if (function.getFunctionName().equals(item.getFunctionName())) {
-                    supplierReportFunctions.add(function);
-                }
+            if (function.getFunctionName()
+                    .equalsIgnoreCase(Function.FUNC_WORK_ONLINE_REPORT)) {
+                supplierReportFunctions.add(function);
             }
         }
         return supplierReportFunctions;
