89f664f11b27146c44e7a8e7f9ef31582080dc40
Ivan Semenov <semenov@axetta.ru>
2017-04-03 09:49:38 +0000

    EP-947 Добавляем вкладку "Параметры сверки"

--- processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/utils/DAOReadonlyService.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/utils/DAOReadonlyService.java
@@ -125,7 +125,7 @@ public class DAOReadonlyService {
             User user = findUserById(idOfUser);
             return user;
         } catch (Exception e) {
-            //logger.error(String.format("Can't retrieve user from current Session. Message: %s", e.getMessage()));
+            logger.info(String.format("Can't retrieve user from current Session. Message: %s", e.getMessage()));
             return null;
         }
     }
