9f4e0ee54bab09a596e80374f129e6a110a58d86
Ivan Semenov <semenov@axetta.ru>
2017-04-19 08:13:49 +0000

    EP-977 Указание временного интервала в ручном запросе импорта платежей РНИП

--- processor/src/web/java/ru/axetta/ecafe/processor/web/ui/service/ServiceRNIPPage.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/ui/service/ServiceRNIPPage.java
@@ -4,15 +4,20 @@
 
 package ru.axetta.ecafe.processor.web.ui.service;
 
+import ru.axetta.ecafe.processor.core.RuntimeContext;
 import ru.axetta.ecafe.processor.core.service.RNIPLoadPaymentsService;
 import ru.axetta.ecafe.processor.core.utils.CalendarUtils;
 import ru.axetta.ecafe.processor.web.ui.BasicWorkspacePage;
 
+import org.apache.commons.lang.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -26,24 +31,64 @@ public class ServiceRNIPPage extends BasicWorkspacePage {
 
     private static final Logger logger = LoggerFactory.getLogger(ServiceRNIPPage.class);
 
-    private Date startDate = new Date();
-    private Date endDate = new Date();
+    //private Date startDate;// = new Date();
+    //private Date endDate;// = new Date();
 
+    private String startDate;
+    private String endDate;
 
+    public ServiceRNIPPage() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        Calendar localCalendar = RuntimeContext.getInstance()
+                .getDefaultLocalCalendar((HttpSession) facesContext.getExternalContext().getSession(false));
+
+        localCalendar.setTime(new Date());
+        Date sDate = DateUtils.truncate(localCalendar, Calendar.DAY_OF_MONTH).getTime();
+        this.setStartDate(CalendarUtils.dateTimeToString(sDate));
+
+        localCalendar.setTime(sDate);
+        localCalendar.add(Calendar.DAY_OF_MONTH, 1);
+        localCalendar.add(Calendar.SECOND, -1);
+        this.setEndDate(CalendarUtils.dateTimeToString(localCalendar.getTime()));
+    }
 
     public void run(){
-        endDate = CalendarUtils.endOfDay(endDate);
-        logger.error("Manual launch RNIPService startDate:" + startDate.toString() + ", endDate:" + endDate.toString());
+        //endDate = CalendarUtils.endOfDay(endDate);
+        Date sDate = null, eDate = null;
+        try {
+            sDate = CalendarUtils.parseFullDateTimeWithLocalTimeZone(getStartDate());
+            eDate = CalendarUtils.parseFullDateTimeWithLocalTimeZone(getEndDate());
+        } catch (Exception e) {
+            printError(String.format("При запуске операции произошла ошибка: %s", e.getMessage()));
+            logger.error("Error run manual RNIPLoadPayment", e);
+        }
+        logger.error("Manual launch RNIPService startDate:" + getStartDate() + ", endDate:" + getEndDate());
 
         RNIPLoadPaymentsService rnipLoadPaymentsService = RNIPLoadPaymentsService.getInstance();
-        rnipLoadPaymentsService.run(startDate,endDate);
+        rnipLoadPaymentsService.run(sDate,eDate);
     }
 
     public String getPageFilename() {
         return "service/rnip_service";
     }
 
-    public Date getStartDate() {
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    /*public Date getStartDate() {
         return startDate;
     }
 
@@ -57,5 +102,5 @@ public class ServiceRNIPPage extends BasicWorkspacePage {
 
     public void setEndDate(Date endDate) {
         this.endDate = endDate;
-    }
+    }*/
 }
--- processor/src/web/web/back-office/include/service/rnip_service.jsp
+++ processor/src/web/web/back-office/include/service/rnip_service.jsp
@@ -8,13 +8,21 @@
 <%@ taglib prefix="rich" uri="http://richfaces.org/rich" %>
 <%@ taglib prefix="a4j" uri="http://richfaces.org/a4j" %>
 
-Автоматически устанавливается 23-59 для второй даты
 <%--@elvariable id="RNIPLaunchService" type="ru.axetta.ecafe.processor.controllers.RNIPLaunchService"--%>
 <h:panelGrid columns="4">
     <h:outputLabel value="Период: "/>
-    <rich:calendar value="#{mainPage.serviceRNIPPage.startDate}" />
+    <h:inputText value="#{mainPage.serviceRNIPPage.startDate}" />
     <h:outputLabel value=" - "/>
-    <rich:calendar value="#{mainPage.serviceRNIPPage.endDate}" />
+    <h:inputText value="#{mainPage.serviceRNIPPage.endDate}" />
 
-    <a4j:commandButton value="Run" action="#{mainPage.serviceRNIPPage.run()}"/>
+    <a4j:commandButton value="Run" action="#{mainPage.serviceRNIPPage.run()}" status="serviceRNIPStatus"/>
+    <a4j:status id="serviceRNIPStatus">
+        <f:facet name="start">
+            <h:graphicImage value="/images/gif/waiting.gif" alt="waiting" />
+        </f:facet>
+    </a4j:status>
+</h:panelGrid>
+<h:panelGrid columns="1">
+<rich:messages styleClass="messages" errorClass="error-messages" infoClass="info-messages"
+               warnClass="warn-messages" />
 </h:panelGrid>
