859e9c235ad4e7b4c6bd4828df4af8a8774f7117
Ivan Semenov <semenov@axetta.ru>
2017-04-27 08:55:58 +0000

    EP-981 Метод getCurrentSubscriptionFeeding - добавляем параметр feedingType

--- processor/src/core/java/ru/axetta/ecafe/processor/core/service/SubscriptionFeedingService.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/service/SubscriptionFeedingService.java
@@ -333,7 +333,9 @@ public class SubscriptionFeedingService {
         subQuery.add(Restrictions.eq("client", c));
         subQuery.add(Restrictions.eq("deletedState", false));
         subQuery.add(Restrictions.le("dateCreateService", currentDate));
-        subQuery.add(Restrictions.eq("feedingType", SubscriptionFeedingType.fromInteger(type)));
+        if (type != null) {
+            subQuery.add(Restrictions.eq("feedingType", SubscriptionFeedingType.fromInteger(type)));
+        }
         subQuery.add(Restrictions.or(
               Restrictions.isNull("dateDeactivateService"),
               Restrictions.gt("dateDeactivateService", currentDate)
