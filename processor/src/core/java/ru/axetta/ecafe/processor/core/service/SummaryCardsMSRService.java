f14a6e2f56cdb49f4728e3dd5e64e4306a0b0d9d
Alexandr Pronin <av.pronin@i-teco.ru>
2018-04-11 12:27:28 +0000

    EP-1017 Вход в музеи города Москвы

--- processor/src/core/java/ru/axetta/ecafe/processor/core/service/SummaryCardsMSRService.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/service/SummaryCardsMSRService.java
@@ -126,8 +126,8 @@ public class SummaryCardsMSRService extends SummaryDownloadBaseService {
                 hex = "0".concat(hex);
             }
             StringBuilder sb = new StringBuilder(hex).reverse();
-            hex = "0x" + sb.charAt(13) + sb.charAt(12) + sb.charAt(11) + sb.charAt(10) +
-                    sb.charAt(9) + sb.charAt(8) + sb.charAt(7) + sb.charAt(6);
+            hex = "0x" + sb.charAt(1) + sb.charAt(0) + sb.charAt(3) + sb.charAt(2) +
+                    sb.charAt(5) + sb.charAt(4) + sb.charAt(7) + sb.charAt(6);
             return Long.decode(hex);
         } else {
             while (hex.length() < 14) {
