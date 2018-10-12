9feab90c06820db436a2f48c53d617e28b915da6
Ivan Semenov <semenov@axetta.ru>
2018-10-12 13:11:15 +0000

    fix

--- processor/src/core/java/db/update_2.2.126-2.2.127.sql
+++ processor/src/core/java/db/update_2.2.126-2.2.127.sql
@@ -41,9 +41,6 @@ ALTER TABLE CF_Clients ADD COLUMN ageTypeGroup character varying (128);
 
 ALTER TABLE CF_RegistryChange ADD COLUMN ageTypeGroupFrom character varying (128);
 
---удаление неиспользуемого индекса
-DROP INDEX cf_cf_registrychange_notificationId_idx;
-
 --Идентификатор организации, в которой создали позицию
 ALTER TABLE cf_taloon_approval ADD COLUMN idoforgcreated bigint;
 
