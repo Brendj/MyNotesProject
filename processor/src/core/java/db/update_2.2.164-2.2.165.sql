d92c6cefae410a8fd7d5d82385e11b5905380a5d
Ivan Semenov <semenov@axetta.ru>
2018-10-15 14:15:32 +0000

    fix по рнипу

--- processor/src/core/java/db/update_2.2.164-2.2.165.sql
+++ processor/src/core/java/db/update_2.2.164-2.2.165.sql
@@ -7,6 +7,9 @@
 --индекс по дате создания миграции клиента в другую ОО
 CREATE INDEX cf_clientmigrationhistory_registrationdate_idx ON cf_clientmigrationhistory USING btree(registrationdate);
 
+--индекс по идентификатору платежа
+CREATE INDEX cf_clientpayments_idofpayment_idx ON cf_clientpayments USING btree(idofpayment);
+
 INSERT INTO cf_options(idofoption, optiontext)VALUES (100093,'2777058000000');
 
 --таблица блокировок баланса после перехода к другому поставщику
