8917ff4c614318456398b8d1b0b39bc710b5fcfd
Ivan Semenov <semenov@axetta.ru>
2018-12-19 17:10:27 +0000

    Release v169

--- processor/src/core/java/db/update_2.2.168-2.2.169.sql
+++ processor/src/core/java/db/update_2.2.168-2.2.169.sql
@@ -6,10 +6,12 @@
 
 -- поиск льгот по GUID для АРМ
 alter table cf_categorydiscounts_dszn
-add column GUID character varying(80) not null unique default uuid_in(md5(random()::text || clock_timestamp()::text)::cstring)::varchar;
+  add column GUID character varying(80) not null unique default cast(uuid_in(cast(md5(cast(random() as text) || cast(clock_timestamp() as text)) as cstring)) as varchar);
 
 
 -- c v88.1 ARM редактирует льготы по GUID, необходимо поднять версию для отправки GUID для уже имеющихся льгот
 UPDATE cf_categorydiscounts_dszn
 SET version = (SELECT MAX(version) FROM cf_categorydiscounts_dszn) + 1
 WHERE deleted <> 1;
+
+--! ФИНАЛИЗИРОВАН 19.12.2018, НЕ МЕНЯТЬ
