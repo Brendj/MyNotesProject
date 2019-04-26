07db776858dbe4fa7e3a7a9e8fc9a8431987b6b6
Ivan Semenov <semenov@axetta.ru>
2019-04-26 11:28:35 +0300

    Баг: некорректно отправляются письма с заявками по платному питанию (предзаказ) на почту (https://gitlab.iteco.mobi/ispp/processor/issues/133)

--- processor/src/core/java/ru/axetta/ecafe/processor/core/service/PreorderRequestsReportService.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/service/PreorderRequestsReportService.java
@@ -218,7 +218,7 @@ public class PreorderRequestsReportService extends RecoverableService {
                                 calendarEnd.add(Calendar.MINUTE, 1);
                                 final Date endGenerateTime = calendarEnd.getTime();
                                 RuntimeContext.getAppContext().getBean(GoodRequestsChangeAsyncNotificationService.class)
-                                        .notifyOrg(orgItem, fireTime, endGenerateTime, lastCreateOrUpdateDate, guids);
+                                        .notifyOrg(orgItem, fireTime, endGenerateTime, lastCreateOrUpdateDate, guids, dateWork);
                             }
 
                             OrgGoodRequest orgGoodRequest = new OrgGoodRequest(idOfOrg, dateWork);
