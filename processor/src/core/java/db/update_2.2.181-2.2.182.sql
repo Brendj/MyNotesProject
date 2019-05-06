1d73ac980961f3f2837b5e3568d47acfc8eb4c49
Artem <voynov@i-teco.ru>
2019-05-06 13:19:15 +0300

    Добавлена миграция

---
+++ processor/src/core/java/db/update_2.2.181-2.2.182.sql
@@ -0,0 +1,11 @@
+--! Пока скрипт не финализирован рекомендовано писать очистку добавляемых колонок таблиц.
+--! после финализации они уберутся
+--! Информация для разработчика -- информация для пользователя
+
+-- Пакет обновлений 2.2.181
+
+--97
+ALTER TABLE cf_card_signs ADD COLUMN publickeyprovider bytea;
+ALTER TABLE cf_card_signs ADD COLUMN privatekeycard bytea;
+ALTER TABLE cf_card_signs ADD COLUMN signtypeprov int4;
+ALTER TABLE cf_card_signs ADD COLUMN newtypeprovider bool;
