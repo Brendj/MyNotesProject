df99c97c5031a48948fe34665be3721dd3f1279d
Alexandr Pronin <av.pronin@i-teco.ru>
2019-06-17 15:30:28 +0300

    добавлено обновление истории перемещения по группам и закрытие заявки(#160)

--- processor/src/core/java/ru/axetta/ecafe/processor/core/service/ESZMigrantsUpdateService.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/service/ESZMigrantsUpdateService.java
@@ -6,9 +6,8 @@ package ru.axetta.ecafe.processor.core.service;
 
 import ru.axetta.ecafe.processor.core.RuntimeContext;
 import ru.axetta.ecafe.processor.core.logic.ClientManager;
-import ru.axetta.ecafe.processor.core.persistence.Client;
-import ru.axetta.ecafe.processor.core.persistence.ClientGroup;
-import ru.axetta.ecafe.processor.core.persistence.VisitReqResolutionHist;
+import ru.axetta.ecafe.processor.core.persistence.*;
+import ru.axetta.ecafe.processor.core.persistence.utils.DAOUtils;
 import ru.axetta.ecafe.processor.core.utils.HibernateUtils;
 import ru.axetta.ecafe.processor.core.utils.PropertyUtils;
 
@@ -33,21 +32,24 @@ import java.util.List;
 @Component("ESZMigrantsUpdateService")
 @Scope("singleton")
 public class ESZMigrantsUpdateService {
+
     private static final Logger logger = LoggerFactory.getLogger(ESZMigrantsUpdateService.class);
     public final String CRON_EXPRESSION_PROPERTY = "ecafe.processor.esz.migrants.update.cronExpression";
 
     public void run() throws Exception {
-        if (!isOn())
+        if (!isOn()) {
             return;
+        }
         updateMigrants();
     }
 
     public static boolean isOn() {
         RuntimeContext runtimeContext = RuntimeContext.getInstance();
         String instance = runtimeContext.getNodeName();
-        String reqInstance = runtimeContext.getConfigProperties().getProperty("ecafe.processor.esz.migrants.update.service.node", "1");
-        if (StringUtils.isBlank(instance) || StringUtils.isBlank(reqInstance) || !instance.trim().equals(
-                reqInstance.trim())) {
+        String reqInstance = runtimeContext.getConfigProperties()
+                .getProperty("ecafe.processor.esz.migrants.update.service.node", "1");
+        if (StringUtils.isBlank(instance) || StringUtils.isBlank(reqInstance) || !instance.trim()
+                .equals(reqInstance.trim())) {
             return false;
         }
         return true;
@@ -77,13 +79,21 @@ public class ESZMigrantsUpdateService {
             disjunction.add(Restrictions.eq("resolution", VisitReqResolutionHist.RES_CANCELED));
             disjunction.add(Restrictions.eq("resolution", VisitReqResolutionHist.RES_REJECTED));
             criteria.add(disjunction);
-            criteria.setProjection(Projections.distinct(Projections.property("m.clientMigrate")));
+            criteria.setProjection(
+                    Projections.projectionList().add(Projections.distinct(Projections.property("m.clientMigrate")))
+                            .add(Projections.property("m.compositeIdOfMigrant")));
 
-            List<Client> list = criteria.list();
-            for (Client client : list) {
+            List list = criteria.list();
+            for (Object o : list) {
+                Object[] row = (Object[]) o;
+                Client client = (Client) row[0];
+                CompositeIdOfMigrant idOfMigrant = (CompositeIdOfMigrant) row[1];
+                closeMigrantRequest(session, idOfMigrant, client);
                 ClientManager.ClientFieldConfigForUpdate fieldConfig = new ClientManager.ClientFieldConfigForUpdate();
-                fieldConfig.setValue(ClientManager.FieldId.GROUP, ClientGroup.Predefined.CLIENT_LEAVING.getNameOfGroup());
+                fieldConfig
+                        .setValue(ClientManager.FieldId.GROUP, ClientGroup.Predefined.CLIENT_LEAVING.getNameOfGroup());
                 ClientManager.modifyClientTransactionFree(fieldConfig, null, "", client, session);
+                addGroupHistory(session, client, ClientGroup.Predefined.CLIENT_LEAVING);
             }
 
             transaction.commit();
@@ -96,8 +106,41 @@ public class ESZMigrantsUpdateService {
         }
     }
 
+    private void closeMigrantRequest(Session session, CompositeIdOfMigrant idOfMigrant, Client client) {
+        Migrant migrant = (Migrant) session.load(Migrant.class, idOfMigrant);
+        if (null != migrant) {
+            migrant.setSyncState(Migrant.CLOSED);
+            session.merge(migrant);
+            session.save(ImportMigrantsService.createResolutionHistory(session, client, idOfMigrant.getIdOfRequest(),
+                    VisitReqResolutionHist.RES_OVERDUE_SERVER, new Date()));
+        }
+    }
+
+    private void addGroupHistory(Session session, Client client, ClientGroup.Predefined predefinedClientGroup)
+            throws Exception {
+        ClientGroupMigrationHistory migrationHistory = new ClientGroupMigrationHistory(client.getOrg(), client);
+        migrationHistory.setComment(ClientGroupMigrationHistory.MODIFY_IN_ISPP);
+        if (client.getClientGroup() != null) {
+            migrationHistory.setOldGroupId(client.getClientGroup().getCompositeIdOfClientGroup().getIdOfClientGroup());
+            migrationHistory.setOldGroupName(client.getClientGroup().getGroupName());
+        }
+
+        ClientGroup clientGroup = DAOUtils.findClientGroup(session,
+                new CompositeIdOfClientGroup(client.getOrg().getIdOfOrg(), predefinedClientGroup.getValue()));
+        if (null == clientGroup) {
+            logger.error(String.format("Unable to find client group: idOfOrg=%d, idOfClientGroup=%d",
+                    client.getOrg().getIdOfOrg(), predefinedClientGroup.getValue()));
+            return;
+        }
+        migrationHistory.setNewGroupId(clientGroup.getCompositeIdOfClientGroup().getIdOfClientGroup());
+        migrationHistory.setNewGroupName(clientGroup.getGroupName());
+
+        session.save(migrationHistory);
+    }
+
     public void scheduleSync() throws Exception {
-        String syncSchedule = RuntimeContext.getInstance().getConfigProperties().getProperty(CRON_EXPRESSION_PROPERTY, "");
+        String syncSchedule = RuntimeContext.getInstance().getConfigProperties()
+                .getProperty(CRON_EXPRESSION_PROPERTY, "");
         if (syncSchedule.equals("")) {
             return;
         }
@@ -114,12 +157,13 @@ public class ESZMigrantsUpdateService {
             }
             scheduler.scheduleJob(job, trigger);
             scheduler.start();
-        } catch(Exception e) {
+        } catch (Exception e) {
             logger.error("Failed to schedule update esz migrants service job:", e);
         }
     }
 
     public static class ESZMigrantsUpdateJob implements Job {
+
         @Override
         public void execute(JobExecutionContext arg0) throws JobExecutionException {
             try {
