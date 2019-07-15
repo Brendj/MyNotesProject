af232bd4ff8cc5862de0b8350b58abfd407148d1
Artem Voinov <voynov@i-teco.ru>
2019-07-15 15:30:42 +0300

    Миграция для задачи

---
+++ processor/src/core/java/db/update_2.2.187-2.2.188.sql
@@ -0,0 +1,9 @@
+--! Пока скрипт не финализирован рекомендовано писать очистку добавляемых колонок таблиц.
+--! после финализации они уберутся
+--! Информация для разработчика -- информация для пользователя
+
+-- Пакет обновлений 2.2.188
+
+--188
+ALTER TABLE cf_externalevents ADD COLUMN address varchar;
+ALTER TABLE cf_externalevents ADD COLUMN orgShortName varchar;
