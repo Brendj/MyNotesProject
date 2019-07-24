17a8e7ef735e688c025d76d8e5924595baa6a3a3
Artem Voinov <voynov@i-teco.ru>
2019-07-24 16:58:48 +0300

    Убраны лишние updates

--- processor/src/core/java/db/update_2.2.187-2.2.188.sql
+++ processor/src/core/java/db/update_2.2.187-2.2.188.sql
@@ -8,12 +8,4 @@
 ALTER TABLE cf_externalevents ADD COLUMN address varchar;
 ALTER TABLE cf_externalevents ADD COLUMN orgShortName varchar;
 
---179
-INSERT INTO cf_clientgroups VALUES (2411,1100000080,'Перемещенные');
-INSERT INTO cf_clientgroups VALUES (96,1100000080,'Перемещенные');
-INSERT INTO cf_clientgroups VALUES (2504,1100000080,'Перемещенные');
-INSERT INTO cf_clientgroups VALUES (100,1100000080,'Перемещенные');
-INSERT INTO cf_clientgroups VALUES (324,1100000080,'Перемещенные');
-INSERT INTO cf_clientgroups VALUES (57,1100000080,'Перемещенные');
-
 --! ФИНАЛИЗИРОВАН 24.07.2019, НЕ МЕНЯТЬ
