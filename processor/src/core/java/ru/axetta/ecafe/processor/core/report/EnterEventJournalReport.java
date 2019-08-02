eebad7422a80aa425cdd33677cf583f8dfad974d
Artem Voinov <voynov@i-teco.ru>
2019-08-02 15:12:12 +0300

    Убрано ограничение, что группа клиента должна быть обязательно в ОУ, куда совершен проход

--- processor/src/core/java/ru/axetta/ecafe/processor/core/report/EnterEventJournalReport.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/report/EnterEventJournalReport.java
@@ -173,7 +173,9 @@ public class EnterEventJournalReport extends BasicReportForAllOrgJob {
                             + " left join cf_visitors v on v.idOfVisitor = ee.idofvisitor "
                             + " left join cf_persons p on p.idofperson = c.idofperson or p.idofperson = v.idofperson "
                             + joinMigrants
-                            + " left join cf_clientgroups gr on gr.idoforg = c.idoforg and gr.idofclientgroup = c.idofclientgroup "
+                            + " left join cf_clientgroups gr on gr.idoforg = c.idoforg "
+                                   //Убрано ограничение, что группа клиента должна быть обязательно в ОУ, куда совершен проход
+                                   //+ "and gr.idofclientgroup = c.idofclientgroup "
                             + " where ee.evtdatetime between :startTime and :endTime "
                             + orgCondition
                             + clientIdWhere
