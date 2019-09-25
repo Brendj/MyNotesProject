1d88135d412ec09bb6037605a366671c13b241e5
Artem Voinov <voynov@i-teco.ru>
2019-09-25 10:36:03 +0300

    fix

--- processor/src/web/java/ru/axetta/ecafe/processor/web/partner/ezd/EzdController.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/partner/ezd/EzdController.java
@@ -23,7 +23,6 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.HttpURLConnection;
-import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
