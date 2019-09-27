b34cd99607b18bd385a7617d0820741701341e76
Ivan Semenov <semenov@axetta.ru>
2019-09-27 15:29:54 +0300

    Release v194

--- processor/src/core/java/db/update_2.2.193-2.2.194.sql
+++ processor/src/core/java/db/update_2.2.193-2.2.194.sql
@@ -9,3 +9,5 @@ alter table cf_preorder_menudetail
   add CONSTRAINT cf_preorder_menudetail_idofgood_fk FOREIGN KEY (idofgood)
     REFERENCES cf_goods (idofgood) MATCH SIMPLE
     ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+--! ФИНАЛИЗИРОВАН 27.09.2019, НЕ МЕНЯТЬ
