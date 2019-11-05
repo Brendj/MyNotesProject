e84b32fa9f6f0b4f85670ceedcbf4f692d1fe7b4
Vitaly Kalashnikov <v.kalashnikov@i-teco.ru>
2019-11-05 15:19:07 +0300

    fix regex

--- processor/src/web/java/ru/axetta/ecafe/processor/web/ui/option/OptionPage.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/ui/option/OptionPage.java
@@ -46,7 +46,7 @@ public class OptionPage extends BasicWorkspacePage {
     private DateFormat biDataDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
     private final Logger logger = LoggerFactory.getLogger(BasicWorkspacePage.class);
     private static final Pattern SYNC_EXPRESSION_PATTERN =
-            Pattern.compile("^(!*([0-1][0-9]|2[0-3]):[0-5][0-9]-([0-1][0-9]|2[0-3]):[0-5][0-9];)+(!*([0-1][0-9]|2[0-3]):[0-5][0-9]-([0-1][0-9]|2[0-3]):[0-5][0-9]);*$");
+            Pattern.compile("^(!?([0-1][0-9]|2[0-3]):[0-5][0-9]-([0-1][0-9]|2[0-3]):[0-5][0-9];)*(!?([0-1][0-9]|2[0-3]):[0-5][0-9]-([0-1][0-9]|2[0-3]):[0-5][0-9]);?$");
     private Boolean notifyBySMSAboutEnterEvent;
     private Boolean withOperator;
     private Boolean cleanMenu;
