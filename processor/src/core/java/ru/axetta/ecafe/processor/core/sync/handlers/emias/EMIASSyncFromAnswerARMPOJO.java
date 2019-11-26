d0c6b08cdc4561dc47de498e54095fa0677d2c4f
Artem Voinov <voynov@i-teco.ru>
2019-11-26 16:30:53 +0300

    Сортировка

--- processor/src/core/java/ru/axetta/ecafe/processor/core/sync/handlers/emias/EMIASSyncFromAnswerARMPOJO.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/sync/handlers/emias/EMIASSyncFromAnswerARMPOJO.java
@@ -22,6 +22,8 @@ public class EMIASSyncFromAnswerARMPOJO {
 
 
     public Long getIdEventEMIAS() {
+        if (idEventEMIAS == null)
+            return 0L;
         return idEventEMIAS;
     }
 
--- processor/src/core/java/ru/axetta/ecafe/processor/core/sync/handlers/emias/EmiasSectionForARMAnswer.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/sync/handlers/emias/EmiasSectionForARMAnswer.java
@@ -11,6 +11,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -33,6 +35,11 @@ public class EmiasSectionForARMAnswer implements AbstractToElement {
         if (items == null) {
             items = new LinkedList<>();
         }
+        Collections.sort(items, new Comparator<EMIASSyncFromAnswerARMPOJO>() {
+            public int compare(EMIASSyncFromAnswerARMPOJO o1, EMIASSyncFromAnswerARMPOJO o2) {
+                return o1.getIdEventEMIAS().compareTo(o2.getIdEventEMIAS());
+            }
+        });
         return items;
     }
 
