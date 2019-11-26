777f9494da56c4999db9eb4e9d487f19de2a3d8d
Vitaly Kalashnikov <v.kalashnikov@i-teco.ru>
2019-11-26 12:23:43 +0300

    add update_sql scripts

---
+++ processor/src/core/java/db/update_2.2.200-2.2.201.sql
@@ -0,0 +1,36 @@
+--! Пока скрипт не финализирован рекомендовано писать очистку добавляемых колонок таблиц.
+--! после финализации они уберутся
+--! Информация для разработчика -- информация для пользователя
+
+-- Пакет обновлений 201
+
+-- 203: Сущность настройкии расписания синхранизации для ОО
+CREATE TABLE public.cf_syncsettings
+(
+    idofsyncsetting bigserial primary key,
+    idoforg bigint NOT NULL references cf_orgs(idoforg),
+    contenttype varchar(128) NOT NULL,
+    everysecond integer,
+    limitstarthour integer,
+    limitendhour integer,
+    monday boolean NOT NULL DEFAULT false,
+    tuesday boolean NOT NULL DEFAULT false,
+    wednesday boolean NOT NULL DEFAULT false,
+    thursday boolean NOT NULL DEFAULT false,
+    friday boolean NOT NULL DEFAULT false,
+    saturday boolean NOT NULL DEFAULT false,
+    sunday boolean NOT NULL DEFAULT false,
+    version bigint NOT NULL DEFAULT 0,
+    deletestate boolean NOT NULL DEFAULT false,
+    createddate bigint NOT NULL DEFAULT (date_part('epoch'::text, now()) * (1000)::double precision),
+    lastupdate bigint,
+    concretetime varchar(256),
+
+    CONSTRAINT cf_syncsettings_limitendhour_check CHECK (limitendhour >= 0 AND limitendhour <= 24),
+    CONSTRAINT cf_syncsettings_limitstarthour_check CHECK (limitstarthour >= 0 AND limitstarthour <= 24),
+    CONSTRAINT cf_syncsettings_everysecond_check CHECK (everysecond <> 0)
+);
+
+CREATE UNIQUE INDEX cf_syncsettings_idoforg_contenttype
+    ON cf_syncsettings (idoforg, contenttype)
+    WHERE deletestate IS FALSE;
