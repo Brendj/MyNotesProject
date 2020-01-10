de2167e2e2b8e0057d35cf7b22412c37df988fbb
Ivan Semenov <semenov@axetta.ru>
2020-01-10 12:56:01 +0300

    #355

--- processor/src/core/java/db/update_issue_355.sql
+++ processor/src/core/java/db/update_issue_355.sql
@@ -6,9 +6,14 @@
 
 alter table cf_orgregistrychange_item
   add column ekisId bigint,
-  add column ekisIdFrom bigint;
+  add column ekisIdFrom bigint,
+  add column egissoId character varying(128),
+  add column egissoIdFrom character varying(128);
 
 alter table cf_orgregistrychange
   add column ekisId bigint,
-  add column ekisIdFrom bigint;
+  add column ekisIdFrom bigint,
+  add column egissoId character varying(128),
+  add column egissoIdFrom character varying(128);
 
+alter table cf_orgs add column egissoId character varying(128);
