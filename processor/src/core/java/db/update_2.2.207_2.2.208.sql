887437ff61be614aaea193edca0aa16a67a6992c
Vitaly Kalashnikov <v.kalashnikov@i-teco.ru>
2020-01-31 11:28:26 +0300

    Add migrate sql-script

--- processor/src/core/java/db/update_2.2.207_2.2.208.sql
+++ processor/src/core/java/db/update_2.2.207_2.2.208.sql
@@ -2,7 +2,24 @@
 --! после финализации они уберутся
 --! Информация для разработчика -- информация для пользователя
 
--- Пакет обновлений 207
+-- Пакет обновлений 208
 
 -- флаг "Использовать Web-АРМ"
 ALTER TABLE cf_orgs ADD COLUMN useWebArm BOOLEAN NOT NULL DEFAULT FALSE;
+
+-- Таблица журнала отправки событий в Геопланер
+CREATE TABLE cf_geoplaner_notifications_journal
+(
+    idofnotification BIGSERIAL PRIMARY KEY,
+    idofclient BIGINT REFERENCES cf_clients(idofclient),
+    idoforg BIGINT REFERENCES cf_orgs(idoforg),
+    idofenterevents BIGINT,
+    idoforder BIGINT,
+    idofclientpayment BIGINT REFERENCES cf_clientpayments(idofclientpayment),
+    eventtype INTEGER NOT NULL,
+    response INTEGER,
+    issend BOOLEAN NOT NULL DEFAULT FALSE,
+    createdate BIGINT NOT NULL,
+    errortext TEXT,
+    nodename VARCHAR(32)
+);
