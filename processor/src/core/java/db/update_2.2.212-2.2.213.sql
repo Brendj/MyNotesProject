32a0249148c16108d667baeb9d874d7334b0ee67
Ivan Semenov <semenov@axetta.ru>
2020-03-26 16:01:08 +0300

    merge update scripts v213

--- processor/src/core/java/db/update_2.2.212-2.2.213.sql
+++ processor/src/core/java/db/update_2.2.212-2.2.213.sql
@@ -2,11 +2,13 @@
 --! после финализации они уберутся
 --! Информация для разработчика -- информация для пользователя
 
--- Пакет обновлений issue 414
+-- Пакет обновлений 213
 
 ALTER TABLE cf_preorder_menudetail
   ADD COLUMN usedsum BIGINT not null default 0,
   ADD COLUMN usedamount BIGINT not null default 0;
 
 alter table cf_preorder_linkod
-    add column itemcode character varying(32);
+  add column itemcode character varying(32);
+
+ALTER TABLE cf_kf_organization_registry ADD COLUMN xaIsActive INTEGER DEFAULT 1;
