f5b27be73846332c4baecf46f1b2bd8344bc8f01
Ivan Semenov <semenov@axetta.ru>
2020-04-10 11:01:01 +0300

    #465 Передача сведений о представителях обучающегося при переходе обучающегося из одной ОО в другую

--- processor/src/core/java/ru/axetta/ecafe/processor/core/service/ImportRegisterClientsService.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/service/ImportRegisterClientsService.java
@@ -1546,14 +1546,8 @@ public class ImportRegisterClientsService {
 
     //@Transactional
     private void addClientMigrationEntry(Session session,Org oldOrg, Org newOrg, Client client, RegistryChange change){
-        ClientManager.checkUserOPFlag(session, oldOrg, newOrg, client.getIdOfClientGroup(), client);
-        ClientMigration migration = new ClientMigration(client, newOrg, oldOrg);
-        migration.setComment(ClientMigration.MODIFY_IN_REGISTRY.concat(String.format(" (ид. ОО=%s)", change.getIdOfOrg())));
-        if(client.getClientGroup() != null) {
-            migration.setOldGroupName(client.getClientGroup().getGroupName());
-        }
-        migration.setNewGroupName(change.getGroupName());
-        session.save(migration);
+        ClientManager.addClientMigrationEntry(session, oldOrg, newOrg, client,
+                ClientMigration.MODIFY_IN_REGISTRY.concat(String.format(" (ид. ОО=%s)", change.getIdOfOrg())), change.getGroupName());
     }
 
     //@Transactional
