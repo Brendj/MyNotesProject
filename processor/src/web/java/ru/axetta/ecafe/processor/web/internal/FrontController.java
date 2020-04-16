767660379305e57067edad96532bb649e7076828
Ivan Semenov <semenov@axetta.ru>
2020-04-16 16:14:12 +0300

    #488

--- processor/src/web/java/ru/axetta/ecafe/processor/web/internal/FrontController.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/internal/FrontController.java
@@ -1553,7 +1553,7 @@ public class FrontController extends HttpServlet {
 
                 if (secondRegisterAllowed && exCard.getState() != CardState.BLOCKED.getValue()) {
                     cardService.blockAndReset(exCard.getCardNo(), exCard.getOrg().getIdOfOrg(),
-                            exCard.getClient().getIdOfClient(), false, CardResponseItem.USED_IN_ANOTHER_ORG);
+                            exCard.getClient() == null ? null : exCard.getClient().getIdOfClient(), false, CardResponseItem.USED_IN_ANOTHER_ORG);
                 } else {
                     cardService.updateTransitionState(exCard, CardTransitionState.GIVEN_AWAY.getCode());
                     persistenceSession.update(exCard);
