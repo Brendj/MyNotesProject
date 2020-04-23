13bc51ac535844b31c99d1fc5158f9b34e1c0aa3
Ivan Semenov <semenov@axetta.ru>
2020-04-23 17:04:58 +0300

    Перенос скрипта миграции из ветки задачи

--- processor/src/core/java/db/update_2.2.213-2.2.214.sql
+++ processor/src/core/java/db/update_2.2.213-2.2.214.sql
@@ -132,3 +132,35 @@ where relation in (6, 7);
 
 --475 Добавление колонки, которая показывает, отправлено ли клиенту уведомление об окончании данной льготы
 ALTER TABLE public.cf_client_dtiszn_discount_info ADD sendnotification bool NULL;
+
+ALTER TABLE cf_kf_organization_registry
+    ADD COLUMN subordination_value VARCHAR(512),
+    ADD COLUMN founder VARCHAR(512),
+    DROP COLUMN subordination_id;
+
+ALTER TABLE cf_orgs
+    ADD COLUMN subordination VARCHAR(512),
+    ADD COLUMN founder VARCHAR(512),
+    ADD COLUMN organizationIdFromNSI BIGINT;
+
+WITH ids AS (
+    SELECT global_id AS organizationIdFromNSI,
+           eo_id     AS ekisID
+    FROM cf_kf_organization_registry
+)
+UPDATE cf_orgs AS o
+SET organizationIdFromNSI = ids.organizationIdFromNSI
+FROM ids
+WHERE o.ekisid = ids.ekisID;
+
+ALTER TABLE cf_orgregistrychange
+    ADD COLUMN subordination VARCHAR(512),
+    ADD COLUMN founder VARCHAR(512),
+    ADD COLUMN subordinationFrom VARCHAR(512),
+    ADD COLUMN founderFrom VARCHAR(512);
+
+ALTER TABLE cf_orgregistrychange_item
+    ADD COLUMN subordination VARCHAR(512),
+    ADD COLUMN founder VARCHAR(512),
+    ADD COLUMN subordinationFrom VARCHAR(512),
+    ADD COLUMN founderFrom VARCHAR(512);
