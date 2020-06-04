1cb47318f41a94651c69a85cf7285db3c0a5998c
Ivan Semenov <semenov@axetta.ru>
2020-06-04 13:52:54 +0300

    #515 Для метода putPreorderComplex добавить ошибку

--- processor/src/web/java/ru/axetta/ecafe/processor/web/partner/preorder/PreorderDAOService.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/partner/preorder/PreorderDAOService.java
@@ -724,8 +724,12 @@ public class PreorderDAOService {
             MenuDetail menuDetail = getMenuDetail(client, idOfMenu, date);
             regularPreorderSelect.setParameter("itemCode", menuDetail.getItemCode());
         }
-        RegularPreorder regularPreorder = (RegularPreorder) regularPreorderSelect.getSingleResult();
-        if (regularPreorder.getDeletedState()) throw new RegularAlreadyDeleted("Для выбранного комплекса или блюда не настроен повтор заказа");
+        RegularPreorder regularPreorder;
+        try {
+            regularPreorder = (RegularPreorder) regularPreorderSelect.getSingleResult();
+        } catch (NoResultException e) {
+            throw new RegularAlreadyDeleted("Для выбранного комплекса или блюда не настроен повтор заказа");
+        }
         deleteRegularPreorderInternal((Session)em.getDelegate(), regularPreorder, PreorderState.OK, guardianMobile, RegularPreorderState.CHANGE_BY_USER);
     }
 
