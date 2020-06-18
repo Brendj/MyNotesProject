11c0c536b71da1979f4dcf988c6bd71ee718cc9c
Olga Petrova <petrova@axetta.ru>
2020-06-18 16:03:56 +0300

    Добавление комментариев ( #499 )

--- processor/src/core/java/db/update_2.2.217-2.2.218.sql
+++ processor/src/core/java/db/update_2.2.217-2.2.218.sql
@@ -11,7 +11,7 @@ ALTER TABLE cf_taloon_approval
     ADD COLUMN bywebsupplier boolean NOT NULL DEFAULT false;
 
 COMMENT ON COLUMN cf_taloon_approval.complexid IS 'Идентификатор комплекса';
-COMMENT ON COLUMN cf_taloon_approval.bywebsupplier IS 'Комплекс от веб-поставщика (false - нет, true - да';
+COMMENT ON COLUMN cf_taloon_approval.bywebsupplier IS 'Комплекс от веб-поставщика (false - нет, true - да)';
 
 -- 499: добавление нового поля в таблицу детализации комплексов предзаказа
 
@@ -28,4 +28,4 @@ ALTER TABLE cf_taloon_preorder
     ADD COLUMN bywebsupplier boolean NOT NULL DEFAULT false;
 
 COMMENT ON COLUMN cf_taloon_preorder.idofdish IS 'Идентификатор блюда от веб-поставщика';
-COMMENT ON COLUMN cf_taloon_preorder.bywebsupplier IS 'Комплекс от веб-поставщика (false - нет, true - да';
+COMMENT ON COLUMN cf_taloon_preorder.bywebsupplier IS 'Комплекс от веб-поставщика (false - нет, true - да)';
