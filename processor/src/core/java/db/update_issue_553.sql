6e3cc033dc4b5c9fa639204d862d0a08fc00d26c
Ivan Semenov <semenov@axetta.ru>
2020-06-18 11:02:46 +0300

    #553

--- processor/src/core/java/db/update_issue_553.sql
+++ processor/src/core/java/db/update_issue_553.sql
@@ -1,3 +1,5 @@
 
 -- Убираем ограничение уникальности
 alter table cf_wt_dishes drop constraint cf_wt_dishes_code_contragent;
+
+alter table cf_wt_dishes alter column code set data type varchar (32);
