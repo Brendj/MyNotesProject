4f5935cf878f50a964a56ddd6f54de45d27fe70d
Vitaly Kalashnikov <v.kalashnikov@i-teco.ru>
2020-07-13 11:04:39 +0300

    Удален лишний скрипт #477 #481

--- processor/src/core/java/db/update_issue_477_481.sql
+++ processor/src/core/java/db/update_issue_477_481.sql
@@ -2,6 +2,4 @@
  * Copyright (c) 2020. Axetta LLC. All Rights Reserved.
  */
 
-CREATE UNIQUE INDEX cf_client_meshguid_uk ON cf_clients (meshguid);
-
 ALTER TABLE CF_RegistryChange ADD COLUMN meshGUID varchar(36);
