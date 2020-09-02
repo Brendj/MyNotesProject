a0449febb18f194d4d503b3141bd1b36f016a0ab
Artem Voinov <voynov@i-teco.ru>
2020-09-02 17:35:14 +0300

    fix сообщения

--- processor/src/mesh/card-sync/src/main/java/ru/iteco/cardsync/service/CardProcessorService.java
+++ processor/src/mesh/card-sync/src/main/java/ru/iteco/cardsync/service/CardProcessorService.java
@@ -135,7 +135,7 @@ public class CardProcessorService {
                 }
                 if (cardActionClient.getCard() != null) {
                     cardService.unblockCard(cardActionClient.getCard());
-                    cardActionClientService.writeRecord(cardActionRequest, "Карты клиента успешно разблокированы", cardActionClient.getClient());
+                    cardActionClientService.writeRecord(cardActionRequest, "Карта клиента успешно разблокирована", cardActionClient.getClient());
                 }
                 else
                 {
