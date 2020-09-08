28fa87acc9e81e65414322d89c1ea1534bec4fb6
Ivan Semenov <semenov@axetta.ru>
2020-09-08 18:45:18 +0300

    Release v229

--- processor/src/core/java/db/update_2.2.228-2.2.229.sql
+++ processor/src/core/java/db/update_2.2.228-2.2.229.sql
@@ -4,23 +4,21 @@
 
 -- Пакет обновлений 229
 
---615
+ALTER TABLE cf_cr_cardactionrequests RENAME COLUMN id TO idcardactionrequest;
 
-ALTER TABLE public.cf_cr_cardactionrequests RENAME COLUMN id TO idcardactionrequest;
+ALTER TABLE cf_cr_cardactionrequests ADD previdcardrequest int8 NULL;
 
-ALTER TABLE public.cf_cr_cardactionrequests ADD previdcardrequest int8 NULL;
-
-CREATE SEQUENCE public.cf_cr_cardactionrequests_idcardactionrequest_seq
+CREATE SEQUENCE cf_cr_cardactionrequests_idcardactionrequest_seq
     INCREMENT BY 1
     MINVALUE 1
     MAXVALUE 9223372036854775807
     START 1;
 
-ALTER TABLE public.cf_cr_cardactionrequests ALTER COLUMN idcardactionrequest SET DEFAULT nextval('cf_cr_cardactionrequests_idcardactionrequest_seq'::regclass);
+ALTER TABLE cf_cr_cardactionrequests ALTER COLUMN idcardactionrequest SET DEFAULT nextval('cf_cr_cardactionrequests_idcardactionrequest_seq');
 
-DROP SEQUENCE public.cf_cr_cardactionrequests_id_seq;
+DROP SEQUENCE cf_cr_cardactionrequests_id_seq;
 
-CREATE SEQUENCE public.cf_cr_cardactionclient_idcardactionclient_seq;
+CREATE SEQUENCE cf_cr_cardactionclient_idcardactionclient_seq;
 
 create TABLE public.cf_cr_cardactionclient (
     idcardactionclient int8 DEFAULT nextval('cf_cr_cardactionclient_idcardactionclient_seq') NOT NULL,
@@ -33,21 +31,23 @@ create TABLE public.cf_cr_cardactionclient (
 );
 
 --613
-CREATE SEQUENCE public.cf_card_sync_idcardsync_seq
+CREATE SEQUENCE cf_card_sync_idcardsync_seq
     INCREMENT BY 1
     MINVALUE 1
     MAXVALUE 9223372036854775807
     START 1;
 
-CREATE TABLE public.cf_card_sync (
+CREATE TABLE cf_card_sync (
     idcardsync int8 DEFAULT nextval('cf_card_sync_idcardsync_seq') NOT NULL,
     idofcard int8 NULL,
     idoforg int8 NULL,
     statechange int8 NULL
 );
 
-alter table public.cf_card_sync add primary key (idcardsync);
-alter table public.cf_cr_cardactionclient add primary key (idcardactionclient);
-alter table public.cf_cr_cardactionrequests add primary key (idcardactionrequest);
+alter table cf_card_sync add primary key (idcardsync);
+alter table cf_cr_cardactionclient add primary key (idcardactionclient);
+alter table cf_cr_cardactionrequests add primary key (idcardactionrequest);
 
-CREATE INDEX cf_cr_cardactionrequests_requestid_idx ON public.cf_cr_cardactionrequests (requestid);
+CREATE INDEX cf_cr_cardactionrequests_requestid_idx ON cf_cr_cardactionrequests (requestid);
+
+--! ФИНАЛИЗИРОВАН 08.09.2020, НЕ МЕНЯТЬ
