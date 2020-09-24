1e0db70ab0c17cd5376174563bc76ca467860151
Ivan Semenov <semenov@axetta.ru>
2020-09-24 18:18:04 +0300

    Release v233

--- processor/src/core/java/db/update_2.2.232-2.2.233.sql
+++ processor/src/core/java/db/update_2.2.232-2.2.233.sql
@@ -2,6 +2,8 @@
 --! после финализации они уберутся
 --! Информация для разработчика -- информация для пользователя
 
--- Пакет обновлений issue 701
+-- Пакет обновлений 233
 
 CREATE INDEX cf_complexinfo_menudate_idoforg_idx ON cf_complexinfo USING btree (idoforg, menudate);
+
+--! ФИНАЛИЗИРОВАН 24.09.2020, НЕ МЕНЯТЬ
