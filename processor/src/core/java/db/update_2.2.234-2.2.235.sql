6a39812bb3b88fd60da2b2476e7ea1fa079bd774
Ivan Semenov <semenov@axetta.ru>
2020-10-01 19:01:17 +0300

    Release v235

--- processor/src/core/java/db/update_2.2.234-2.2.235.sql
+++ processor/src/core/java/db/update_2.2.234-2.2.235.sql
@@ -2,6 +2,8 @@
 --! после финализации они уберутся
 --! Информация для разработчика -- информация для пользователя
 
--- Пакет обновлений issue 659
+-- Пакет обновлений 235
 
 CREATE INDEX cf_preorder_linkod_idoforg_idoforder_idx ON cf_preorder_linkod USING btree(idoforg, idoforder);
+
+--! ФИНАЛИЗИРОВАН 01.10.2020, НЕ МЕНЯТЬ
