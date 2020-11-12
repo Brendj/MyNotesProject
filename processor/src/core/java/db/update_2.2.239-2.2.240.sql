fc590dc433d99a8f38aa522bbcc60182d79d4853
Ivan Semenov <semenov@axetta.ru>
2020-11-12 18:18:19 +0300

    Release v240

--- processor/src/core/java/db/update_2.2.239-2.2.240.sql
+++ processor/src/core/java/db/update_2.2.239-2.2.240.sql
@@ -29,7 +29,93 @@ COMMENT ON COLUMN public.cf_cr_cardactionrequests.previdcardrequest IS 'Иден
 
 --756
 drop table cf_GuardSan;
-ALTER TABLE public.cf_clients DROP COLUMN guardsan;
+ALTER TABLE cf_clients DROP COLUMN guardsan;
 
 --551
-ALTER TABLE public.cf_complex_schedules ALTER COLUMN groupsids TYPE varchar USING cast(groupsids as varchar);
+ALTER TABLE cf_complex_schedules ALTER COLUMN groupsids TYPE varchar USING cast(groupsids as varchar);
+
+DROP TABLE IF EXISTS cf_plan_orders;
+CREATE TABLE cf_plan_orders
+(
+  idofplanorder BIGSERIAL PRIMARY KEY,
+  idoforg bigint ,
+  groupname character varying(256),
+  idofclient bigint,
+  plandate bigint NOT NULL,
+  idofcomplex bigint,
+  complexname character varying(256),
+  useridrequesttopay bigint,
+  topay integer NOT NULL,
+  idoforder bigint,
+  useridconfirmtopay bigint,
+  idofdiscountrule bigint,
+  createdate bigint NOT NULL DEFAULT cast(extract(epoch from now()) * 1000 as bigint),
+  lastupdate bigint NOT NULL DEFAULT cast(extract(epoch from now()) * 1000 as bigint),
+  CONSTRAINT planorders_unique_constraint UNIQUE (idoforg, idofclient, plandate, idofcomplex),
+  CONSTRAINT useridrequesttopay_fk FOREIGN KEY (useridrequesttopay)
+  REFERENCES public.cf_users (idofuser) MATCH SIMPLE
+  ON UPDATE NO ACTION
+  ON DELETE NO ACTION
+  NOT VALID,
+  CONSTRAINT useridconfirmtopay_fk FOREIGN KEY (useridconfirmtopay)
+  REFERENCES public.cf_users (idofuser) MATCH SIMPLE
+  ON UPDATE NO ACTION
+  ON DELETE NO ACTION
+  NOT VALID,
+  CONSTRAINT idofrule_fk FOREIGN KEY (idofdiscountrule)
+  REFERENCES public.cf_discountrules (idofrule) MATCH SIMPLE
+  ON UPDATE NO ACTION
+  ON DELETE NO ACTION
+  NOT VALID,
+  CONSTRAINT idofclient_fk FOREIGN KEY (idofclient)
+  REFERENCES public.cf_clients (idofclient) MATCH SIMPLE
+  ON UPDATE NO ACTION
+  ON DELETE NO ACTION
+  NOT VALID
+)
+WITH (
+OIDS = FALSE
+);
+
+COMMENT ON TABLE cf_plan_orders IS 'План питания';
+
+alter table cf_smartwatchs add column vendor varchar(128) not null default 'Geoplaner';
+
+CREATE TABLE cf_smartwatch_vendor
+(
+  idofvendor          bigserial primary key,
+  name                varchar(128) not null unique,
+  apikey              varchar(36)  not null unique,
+  enableService       bool         not null default true,
+  cardsigncertnum     integer,
+  enablePushes        bool         not null default false,
+  entereventsEndPoint varchar(512),
+  purchasesendpoint   varchar(512),
+  paymentendpoint     varchar(512)
+);
+
+COMMENT ON TABLE cf_smartwatch_vendor IS 'Поставщики смарт-часов';
+COMMENT ON COLUMN cf_smartwatch_vendor.idofvendor IS 'ID';
+COMMENT ON COLUMN cf_smartwatch_vendor.name IS 'Название поставщика';
+COMMENT ON COLUMN cf_smartwatch_vendor.apikey IS 'Ключ доступа к REST-контроллеру';
+COMMENT ON COLUMN cf_smartwatch_vendor.enableService IS 'Флаг разрешение на обслуживание в REST-контроллере';
+COMMENT ON COLUMN cf_smartwatch_vendor.cardsigncertnum IS 'Номер сертификата (ключа ЭЦП часов как карты)';
+COMMENT ON COLUMN cf_smartwatch_vendor.enablePushes IS 'Разрешение отправлять оповещения';
+COMMENT ON COLUMN cf_smartwatch_vendor.entereventsEndPoint IS 'Адрес для отправки проходов';
+COMMENT ON COLUMN cf_smartwatch_vendor.purchasesendpoint IS 'Адрес для отправки покупок';
+COMMENT ON COLUMN cf_smartwatch_vendor.paymentendpoint IS 'Адрес для отправки пополнения счёта';
+
+ALTER TABLE cf_smartwatchs
+  ADD COLUMN idofvendor integer references cf_smartwatch_vendor(idofvendor) on delete set null;
+
+alter table cf_clients
+  add column idOfVendor bigint references cf_smartwatch_vendor(idofvendor) on delete set null;
+
+alter table cf_geoplaner_notifications_journal
+  add column idOfVendor bigint references cf_smartwatch_vendor(idofvendor);
+
+alter table cf_client_dtiszn_discount_info add column archivedate bigint;
+COMMENT ON COLUMN cf_client_dtiszn_discount_info.archivedate IS 'Дата архивации льготы';
+
+alter table cf_applications_for_food add column archivedate bigint;
+COMMENT ON COLUMN cf_applications_for_food.archivedate IS 'Дата архивации заявления';
--- processor/src/core/java/db/update_issue_728.sql
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright (c) 2020. Axetta LLC. All Rights Reserved.
- */
-
-alter table cf_smartwatchs add column vendor varchar(128) not null default 'Geoplaner';
