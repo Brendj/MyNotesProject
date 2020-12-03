13268991eee3c45a6e5f32e3a05edc4218b8a9fc
Vitaly Kalashnikov <v.kalashnikov@i-teco.ru>
2020-12-03 18:17:45 +0300

    Release v243

--- processor/src/core/java/db/update_2.2.242-2.2.243.sql
+++ processor/src/core/java/db/update_2.2.242-2.2.243.sql
@@ -2,6 +2,11 @@
  * Copyright (c) 2020. Axetta LLC. All Rights Reserved.
  */
 
+--! Пока скрипт не финализирован рекомендовано писать очистку добавляемых колонок таблиц.
+--! после финализации они уберутся
+--! Информация для разработчика -- информация для пользователя
+
+-- Пакет обновлений 243
 
 alter table cf_wt_category_items drop constraint cf_wt_category_items_description_uk;
 
@@ -14,16 +19,16 @@ create unique index cf_wt_categories_description_uk
 CREATE OR REPLACE FUNCTION wt_category_items_drop_ref_proc() RETURNS trigger
     LANGUAGE 'plpgsql' AS
 '
-DECLARE
-    id bigint;
-BEGIN
+    DECLARE
+        id bigint;
+    BEGIN
     id := NEW.idofcategoryitem;
     IF (NEW.deletestate = 1) THEN
-        DELETE FROM cf_wt_dish_categoryitem_relationships
-        WHERE idofcategoryitem = id;
+    DELETE FROM cf_wt_dish_categoryitem_relationships
+    WHERE idofcategoryitem = id;
     END IF;
     RETURN NEW;
-end
+    end
 ';
 
 CREATE TRIGGER wt_category_items_drop_ref
@@ -35,17 +40,17 @@ EXECUTE PROCEDURE wt_category_items_drop_ref_proc();
 CREATE OR REPLACE FUNCTION wt_categories_drop_ref_proc() RETURNS trigger
     LANGUAGE 'plpgsql' AS
 '
-DECLARE
-    id bigint;
-BEGIN
+    DECLARE
+        id bigint;
+    BEGIN
     id := NEW.idofcategory;
     IF (NEW.deletestate = 1) THEN
-        UPDATE cf_wt_dishes as dish
-        SET idofcategory = NULL
-        WHERE dish.idofcategory = id;
+    UPDATE cf_wt_dishes as dish
+    SET idofcategory = NULL
+    WHERE dish.idofcategory = id;
     END IF;
     RETURN NEW;
-end
+    end
 ';
 
 CREATE TRIGGER wt_categories_drop_ref
@@ -53,3 +58,5 @@ CREATE TRIGGER wt_categories_drop_ref
     ON cf_wt_categories
     FOR EACH ROW
 EXECUTE PROCEDURE wt_categories_drop_ref_proc();
+
+--! ФИНАЛИЗИРОВАН 03.12.2020, НЕ МЕНЯТЬ
