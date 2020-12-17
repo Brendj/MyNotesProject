7213ac7c0a42a8f813afc70163fbe893caa858fc
Vitaly Kalashnikov <v.kalashnikov@i-teco.ru>
2020-12-17 17:34:55 +0300

    unfinished

---
+++ processor/src/mesh/msp-kafka/src/main/java/ru/iteco/msp/repo/ClientDTSZNDiscountInfoRepo.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020. Axetta LLC. All Rights Reserved.
+ */
+
+package ru.iteco.msp.repo;
+
+import ru.iteco.msp.models.ClientDTSZNDiscountInfo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientDTSZNDiscountInfoRepo extends JpaRepository<ClientDTSZNDiscountInfo, Long> {
+
+}
