c5150d73791a357a224ceffe10bb87a5d60ba36f
semenov <semenov@axetta.ru>
2020-12-18 11:14:31 +0300

    #714 Переход на Java версии 1.8 и использование Spring boot версии 2

--- processor/copy_resources.cmd
+++ processor/copy_resources.cmd
@@ -1,2 +1,6 @@
 robocopy src\core\java\ru\axetta\ecafe\processor\core\persistence src\main\resources\hibernate *.hbm.xml /S /MOV
 robocopy src\core\java\db src\main\resources\db * /S /MOV
+
+ mvn install:install-file -Dfile=C:\Git\ProcessingSB\processor\build\lib\cryptopro\JCP.jar -DgroupId=CryptoPro -DartifactId=ru.CryptoPro.JCP -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+ mvn install:install-file -Dfile=C:\Git\ProcessingSB\processor\build\lib\cryptopro\XMLDSigRI.jar -DgroupId=CryptoPro -DartifactId=ru.CryptoPro.JCPxml.dsig -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+ mvn install:install-file -Dfile=C:\Git\ProcessingSB\processor\build\lib\cryptopro\JCPxml.jar -DgroupId=CryptoPro -DartifactId=ru.CryptoPro.JCPxml -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
