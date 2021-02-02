3c5290589492a9bd01f64db7f7622288c6289db1
Vitaly Kalashnikov <v.kalashnikov@i-teco.ru>
2021-02-02 18:54:44 +0300

    Добавление в релиз 889 задачи

--- processor/src/core/java/db/update_2.2.249-2.2.250.sql
+++ processor/src/core/java/db/update_2.2.249-2.2.250.sql
@@ -22,4 +22,31 @@ COMMENT ON COLUMN cf_client_discount_history.idOfClient IS 'Ссылка на к
 COMMENT ON COLUMN cf_client_discount_history.idOfCategoryDiscount IS 'Ссылка на измененную льготу';
 COMMENT ON COLUMN cf_client_discount_history.comment IS 'Комментарий';
 
+-- Пакет обновлений issue 889
+
+--Создаем отдельные сиквенсы для каждой записи из таблицы cf_do_versions и инициализируем сиквенсы значениями версий из этой таблицы
+
+CREATE OR REPLACE FUNCTION create_do_versions_sequences() RETURNS INTEGER
+    LANGUAGE 'plpgsql' AS '
+    DECLARE
+        cname varchar;
+    query_str varchar;
+    seq_name varchar;
+BEGIN
+
+    for cname in select distributedobjectclassname FROM cf_do_versions
+    loop
+    seq_name = ''DO_VERSION_'' || cname || ''_seq'';
+    query_str = ''create sequence '' || seq_name;
+    execute query_str;
+
+    query_str = E''select setval(\'''' || seq_name || E''\'', (select coalesce(max(currentversion), 0) + 1 from cf_do_versions where upper(distributedobjectclassname) = \'''' || upper(cname) || E''\'')) '';
+    execute query_str;
+    end loop;
+
+    return 0;
+    END ';
+
+select create_do_versions_sequences();
+
 --! ФИНАЛИЗИРОВАН 02.02.2021, НЕ МЕНЯТЬ
