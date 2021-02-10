2996461a9d4d7c42fbc71fc1382f7e5c092b7157
Artem Voinov <voynov@i-teco.ru>
2021-02-10 16:06:12 +0300

    Решение по задаче (#891)

--- processor/src/web/java/ru/axetta/ecafe/processor/web/partner/integra/dataflow/ClientSummaryBaseListResult.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/partner/integra/dataflow/ClientSummaryBaseListResult.java
@@ -4,6 +4,10 @@
 
 package ru.axetta.ecafe.processor.web.partner.integra.dataflow;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 import java.util.List;
 
 /**
@@ -13,9 +17,18 @@ import java.util.List;
  * Time: 13:27
  * To change this template use File | Settings | File Templates.
  */
-public class ClientSummaryBaseListResult extends Result {
-    private List<ClientSummaryBase> clientSummary;
+@XmlType(propOrder = {
+        "clientSummary",
+        "resultCode",
+        "description"
+})
 
+@XmlRootElement(name="clientSummaryBaseListResult")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ClientSummaryBaseListResult {
+    private List<ClientSummaryBase> clientSummary;
+    public Long resultCode;
+    public String description;
 
     public ClientSummaryBaseListResult(List<ClientSummaryBase> clientSummary, Long resultCode, String description) {
         this.clientSummary = clientSummary;
@@ -30,5 +43,21 @@ public class ClientSummaryBaseListResult extends Result {
 
     public void setClientSummary(List<ClientSummaryBase> clientSummary) {
         this.clientSummary = clientSummary;
+    }
+
+    public Long getResultCode() {
+        return this.resultCode;
+    }
+
+    public void setResultCode(Long resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
