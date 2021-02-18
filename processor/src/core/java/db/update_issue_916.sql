a8c62fd44d5b083a7a4e4395b14e8075e0aeb048
Ivan Semenov <semenov@axetta.ru>
2021-02-18 10:24:08 +0300

    #916 Реализовать скрипт для увеличения кол-ва символов в параметрах, содержащих краткое название адреса здания ОО

--- processor/src/core/java/db/update_issue_916.sql
+++ processor/src/core/java/db/update_issue_916.sql
@@ -3,10 +3,14 @@
 --! Информация для разработчика -- информация для пользователя
 
 -- Пакет обновлений issue 916
-ALTER TABLE cf_orgregistrychange ALTER COLUMN short_address TYPE character varying(512),
+ALTER TABLE cf_orgregistrychange ALTER COLUMN address TYPE character varying(512),
+  ALTER COLUMN addressfrom TYPE character varying(512),
+  ALTER COLUMN short_address TYPE character varying(512),
   ALTER COLUMN short_addressfrom TYPE character varying(512);
 
-ALTER TABLE cf_orgregistrychange_item ALTER COLUMN short_address TYPE character varying(512),
+ALTER TABLE cf_orgregistrychange_item ALTER COLUMN address TYPE character varying(512),
+  ALTER COLUMN addressfrom TYPE character varying(512),
+  ALTER COLUMN short_address TYPE character varying(512),
   ALTER COLUMN short_addressfrom TYPE character varying(512);
 
 ALTER TABLE cf_orgs ALTER COLUMN address TYPE character varying(512),
