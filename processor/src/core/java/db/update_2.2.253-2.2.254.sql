fbe2fa1f24813ebb74a89d43f40eb58520f12f36
Vitaly Kalashnikov <v.kalashnikov@i-teco.ru>
2021-03-04 18:20:27 +0300

    Сборка пакетов миграций

---
+++ processor/src/core/java/db/update_2.2.253-2.2.254.sql
@@ -0,0 +1,32 @@
+--! Пока скрипт не финализирован рекомендовано писать очистку добавляемых колонок таблиц.
+--! после финализации они уберутся
+--! Информация для разработчика -- информация для пользователя
+
+-- Пакет обновлений 254
+
+-- Миграции по задаче issue 930
+alter table cf_orgregistrychange add column globalId bigint,
+                                 add column globalIdFrom bigint;
+
+alter table cf_orgregistrychange_item add column globalId bigint,
+                                      add column globalIdFrom bigint;
+
+COMMENT ON COLUMN cf_orgregistrychange.globalId IS 'Идентификатор НСИ-3';
+COMMENT ON COLUMN cf_orgregistrychange.globalIdFrom IS 'Идентификатор НСИ-3 в ИСПП';
+COMMENT ON COLUMN cf_orgregistrychange_item.globalId IS 'Идентификатор НСИ-3';
+COMMENT ON COLUMN cf_orgregistrychange_item.globalIdFrom IS 'Идентификатор НСИ-3 в ИСПП';
+
+-- Миграции БД при обновлении веб АРМа ПП до v 1.25
+drop trigger complex_org_inc_ver on cf_wt_complexes_org;
+drop trigger menu_org_inc_ver on cf_wt_menu_org;
+drop trigger org_group_org_inc_ver on cf_wt_org_group_relations;
+drop trigger org_group_org_inc_ver on cf_wt_org_groups;
+drop function complex_org_inc_ver();
+drop function menu_org_inc_ver();
+drop function org_group_org_inc_ver();
+
+alter table cf_wt_org_relation_aud
+    add column versionofcomplex bigint,
+    add column versionofmenu    bigint;
+
+--! ФИНАЛИЗИРОВАН 04.03.2021, НЕ МЕНЯТЬ
