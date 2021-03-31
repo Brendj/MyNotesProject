5c89c4bc6a6f446d52fc4642785116a0e77aec50
e.akmukov <akmukov@axetta.ru>
2021-03-31 23:59:31 +0300

    убрал пустой класс

--- processor/src/web/java/ru/axetta/ecafe/processor/web/partner/schoolapi/discounts/service/SchoolApiDiscountsService.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2021. Axetta LLC. All Rights Reserved.
- */
-
-package ru.axetta.ecafe.processor.web.partner.schoolapi.discounts.service;
-
-public interface SchoolApiDiscountsService {
-
-}
