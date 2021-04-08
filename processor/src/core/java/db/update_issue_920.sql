4b1a3569788e9b1970d3d005dc2de88ff85521eb
Artem Voinov <voynov@i-teco.ru>
2021-04-08 18:32:15 +0300

    Release v259

--- processor/src/core/java/db/update_issue_920.sql
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2021. Axetta LLC. All Rights Reserved.
- */
-
-
-ALTER TABLE cf_enterevents ADD COLUMN longCardId BIGINT;
-
-COMMENT ON COLUMN CF_EnterEvents.longCardId IS 'Длинный UID карты';
--- processor/src/core/java/db/update_issue_970.sql
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2021. Axetta LLC. All Rights Reserved.
- */
-
-
-ALTER TABLE cf_orgs
-    ADD COLUMN governmentContract BOOLEAN;
-
-COMMENT ON COLUMN CF_Orgs.governmentContract IS 'Наличие государственного контракта';
