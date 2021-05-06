9b2192904632edb88c7ad62e5a85a18bc95aa3c8
Kalashnikov <v.kalashnikov@i-teco.ru>
2021-05-06 17:40:33 +0300

    Release v262

--- processor/src/core/java/db/update_2.2.261-2.2.262.sql
+++ processor/src/core/java/db/update_2.2.261-2.2.262.sql
@@ -1,5 +1,46 @@
--- Пакет обновлений 260
+/*
+ * Copyright (c) 2021. Axetta LLC. All Rights Reserved.
+ */
 
+-- Пакет обновлений issue 997
+
+alter table cf_specialdates
+    add column staffguid character varying(36),
+    add column armlastupdate bigint,
+    add column createddate bigint,
+    add column lastupdate bigint;
+
+comment on column cf_specialdates.staffguid is 'Гуид сотрудника';
+comment on column cf_specialdates.armlastupdate is 'Дата-время изменений на АРМе';
+comment on column cf_specialdates.createddate is 'Дата создания записи';
+comment on column cf_specialdates.lastupdate is 'Дата последнего изменения';
+
+CREATE TABLE cf_specialdates_history
+(
+    idofspecialdatehistory bigserial NOT NULL,
+    idoforg bigint NOT NULL,
+    date bigint NOT NULL,
+    isweekend integer NOT NULL,
+    deleted integer NOT NULL,
+    version bigint NOT NULL,
+    comment character varying(256) NOT NULL DEFAULT '',
+    idoforgowner bigint NOT NULL,
+    idofclientgroup bigint,
+    staffguid character varying(36),
+    armlastupdate bigint,
+    createddate bigint,
+    CONSTRAINT cf_specialdates_history_pk PRIMARY KEY (idofspecialdatehistory),
+    CONSTRAINT cf_specialdates_history_idoforg_fk FOREIGN KEY (idoforg)
+        REFERENCES cf_orgs (idoforg) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT cf_specialdates_history_idoforgowner_fk FOREIGN KEY (idoforgowner)
+        REFERENCES cf_orgs (idoforg) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+comment on table cf_specialdates_history is 'История изменений календаря дней питания';
+
+-- Пакет обновлений issue 1013
 CREATE SEQUENCE CF_SyncHistory_Exceptions_Id_Gen_seq INCREMENT BY 128;
 select setval('CF_SyncHistory_Exceptions_Id_Gen_seq', (select coalesce(max(IdOfSyncHistoryException), 0) + 1 from CF_SyncHistory_Exceptions));
 
@@ -104,3 +145,5 @@ select setval('CF_Contragents_Id_Gen_seq', (select coalesce(max(IdOfContragent),
 
 CREATE SEQUENCE cf_wt_discountrules_Id_Gen_seq INCREMENT BY 3;
 select setval('cf_wt_discountrules_Id_Gen_seq', (select coalesce(max(IdOfRule), 0) + 1 from cf_wt_discountrules));
+
+--! ФИНАЛИЗИРОВАН 06.05.2021, НЕ МЕНЯТЬ
