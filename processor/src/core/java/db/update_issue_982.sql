924c16d67a52b03e6650314303124526f381675b
Kalashnikov <v.kalashnikov@i-teco.ru>
2021-07-23 12:51:17 +0300

    fix #982 Правка описания поля cf_enterevents.childpasschecker в БД

---
+++ processor/src/core/java/db/update_issue_982.sql
@@ -0,0 +1,5 @@
+/*
+ * Copyright (c) 2021. Axetta LLC. All Rights Reserved.
+ */
+
+comment on column cf_enterevents.childpasschecker is 'Признак того, кто сделал отметку о проходе за представителя (опционально. Возможные значения: 0 – охранник, 1 – воспитатель)';
