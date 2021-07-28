ce07bac23e2914f9bbea2e055b51b34a8a28d4fc
Ivan Semenov <semenov@axetta.ru>
2021-07-28 17:46:33 +0300

    Release v267 удаление лишних скриптов

--- processor/src/core/java/db/update_issue_1044.sql
+++
@@ -1,4 +0,0 @@
--- Пакет обновлений issue 1044
-
-alter table cf_info_messages add column mtype integer not null default 0;
-comment on column cf_info_messages.mtype is 'Тип сообщения: 0 - для толстого арма, 1 - для веб арма';
