88e696ea8717fcd9c0c2781aee7ac55377474673
Ivan Semenov <semenov@axetta.ru>
2021-08-05 11:34:18 +0300

    #1035 Не удалять регуляр, если по нему есть хотя бы 1 предзаказ со state = 0 или 5 на 14 дней вперед

--- processor/src/web/java/ru/axetta/ecafe/processor/web/partner/preorder/PreorderDAOService.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/partner/preorder/PreorderDAOService.java
@@ -2001,8 +2001,8 @@ public class PreorderDAOService {
         if (!CalendarUtils.betweenDate(new Date(), regularPreorder.getStartDate(), regularPreorder.getEndDate())) return;
         Query query = em.createQuery("select pc.idOfPreorderComplex from PreorderComplex pc join pc.preorderMenuDetails pmd "
                 + "where (pc.regularPreorder = :regularPreorder or pmd.regularPreorder = :regularPreorder) "
-                + "and pc.preorderDate > :date and (pc.deletedState = false or pc.state in :states) and pc.idOfGoodsRequestPosition is null "
-                + "and (pmd.deletedState = false or pmd.state in :states)");
+                + "and pc.preorderDate > :date and (pc.deletedState = false or pc.state in (:states)) and pc.idOfGoodsRequestPosition is null "
+                + "and (pmd.deletedState = false or pmd.state in (:states))");
         query.setParameter("regularPreorder", regularPreorder);
         query.setParameter("date", new Date());
         List<PreorderState> states = Arrays.asList(PreorderState.OK, PreorderState.NOT_ENOUGH_BALANCE, PreorderState.CHANGED_CALENDAR);
