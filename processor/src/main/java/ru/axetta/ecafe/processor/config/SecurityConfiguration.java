904402b263aec63d43d2b69d8518875e0272dae2
semenov <semenov@axetta.ru>
2021-11-24 18:27:37 +0300

    #714 Перевод страниц - правка багов, работа арма со сверкой и картами

--- processor/src/main/java/ru/axetta/ecafe/processor/config/SecurityConfiguration.java
+++ processor/src/main/java/ru/axetta/ecafe/processor/config/SecurityConfiguration.java
@@ -1,11 +1,15 @@
 package ru.axetta.ecafe.processor.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ru.axetta.ecafe.processor.beans.authentication.provider.ProcessingJaasAuthenticationProvider;
 import ru.axetta.ecafe.processor.core.persistence.User;
@@ -32,8 +36,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         security.authorizeRequests().antMatchers("/javax.faces.resource/**")
                 .permitAll()
                 .antMatchers("/back-office/styles.css").permitAll();
-
-        security.authorizeRequests().antMatchers("/soap/**").permitAll();
 
         security/*.requestMatchers()
                 .antMatchers("/processor/sync", "/processor/back-office")
@@ -64,4 +66,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         security.cors();
     }
 
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                org.springframework.security.core.userdetails.User.withDefaultPasswordEncoder()
+                        .username("us")
+                        .password("pa")
+                        .roles("US")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
