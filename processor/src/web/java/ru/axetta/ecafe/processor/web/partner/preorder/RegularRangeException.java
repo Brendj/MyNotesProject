4d1dc5874ce8c7444ecadd5517b340910a362386
saparov_a <saparov@sck.ru>
2022-01-28 14:58:42 +0300

    Решение по задаче https://yt.iteco.dev/issue/ISPP-313

---
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/partner/preorder/RegularRangeException.java
@@ -0,0 +1,7 @@
+package ru.axetta.ecafe.processor.web.partner.preorder;
+
+public class RegularRangeException extends Exception {
+    public RegularRangeException(String message) {
+        super(message);
+    }
+}
