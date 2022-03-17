a903f3fe7770c6a22b60228c11814f2a98c153f0
Artem Voinov <voynov@i-teco.ru>
2022-03-17 14:49:51 +0300

    403 ошибка при недоступности фудбокса в орг (#ispp-366)

--- processor/src/web/java/ru/axetta/ecafe/processor/web/partner/meals/MealsController.java
+++ processor/src/web/java/ru/axetta/ecafe/processor/web/partner/meals/MealsController.java
@@ -135,7 +135,7 @@ public class MealsController extends Application {
             logger.error("У организации не включен функционал фудбокса");
             result.setCode(ResponseCodes.RC_NOT_FOUND_ORG.getCode().toString());
             result.setDescription(ResponseCodes.RC_NOT_FOUND_ORG.toString());
-            return Response.status(HttpURLConnection.HTTP_BAD_REQUEST).entity(result).build();
+            return Response.status(HttpURLConnection.HTTP_FORBIDDEN).entity(result).build();
         }
         if (!client.getFoodboxAvailability()) {
             logger.error("У клиента не включен функционал фудбокса");
@@ -551,7 +551,7 @@ public class MealsController extends Application {
             logger.error("У организации не включен функционал фудбокса");
             result.setCode(ResponseCodes.RC_NOT_FOUND_ORG.getCode().toString());
             result.setDescription(ResponseCodes.RC_NOT_FOUND_ORG.toString());
-            return Response.status(HttpURLConnection.HTTP_BAD_REQUEST).entity(result).build();
+            return Response.status(HttpURLConnection.HTTP_FORBIDDEN).entity(result).build();
         }
         if (!client.getFoodboxAvailability()) {
             logger.error("У клиента не включен функционал фудбокса");
