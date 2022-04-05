5637afefe88140f749bcdf3f1cc3687d159f7d49
semenov <semenov@axetta.ru>
2022-04-05 16:26:26 +0300

    https://yt.iteco.dev/issue/ISPP-726

--- processor/src/core/java/ru/axetta/ecafe/processor/core/card/CardBlockPeriodConfig.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/card/CardBlockPeriodConfig.java
@@ -1,12 +1,16 @@
 package ru.axetta.ecafe.processor.core.card;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 public class CardBlockPeriodConfig {
     public static final String PARAM_BASE = "ecafe.processor.card.registration.block.period";
-    private List<BlockPeriodCardTypes> list;
+    private final List<BlockPeriodCardTypes> list;
+    private static final Logger logger = LoggerFactory.getLogger(CardBlockPeriodConfig.class);
 
     /*
     Параметры в конфигурации:
@@ -15,21 +19,29 @@ public class CardBlockPeriodConfig {
     * */
     public CardBlockPeriodConfig(Properties properties) {
         this.list = new ArrayList<>();
-        for (int n=0;;++n) {
-            String nameParam = PARAM_BASE + "." + n;
-            if (!properties.containsKey(nameParam)) break;
-            String param = properties.getProperty(nameParam);
-            String[] arr = param.split(":");
-            BlockPeriodCardTypes blockPeriodCardTypes = new BlockPeriodCardTypes()
-                    .cardTypes(arr[0])
-                    .period(arr[1]);
-            list.add(blockPeriodCardTypes);
+        try {
+            for (int n = 0; ; ++n) {
+                String nameParam = PARAM_BASE + "." + n;
+                if (!properties.containsKey(nameParam)) break;
+                String param = properties.getProperty(nameParam);
+                String[] arr = param.split(":");
+                BlockPeriodCardTypes blockPeriodCardTypes = new BlockPeriodCardTypes()
+                        .cardTypes(arr[0])
+                        .period(arr[1]);
+                list.add(blockPeriodCardTypes);
+            }
+        } catch (Exception e) {
+            logger.error("Error in init CardBlockPeriodConfig: ", e);
         }
     }
 
+    public List<BlockPeriodCardTypes> getList() {
+        return list;
+    }
+
     public static class BlockPeriodCardTypes {
-        List<Integer> cardTypes;
-        Integer period;
+        private List<Integer> cardTypes;
+        private Integer period;
 
         public BlockPeriodCardTypes cardTypes(String cardTypes) {
             String[] arr = cardTypes.split(",");
@@ -44,6 +56,14 @@ public class CardBlockPeriodConfig {
         public BlockPeriodCardTypes period(String period) {
             this.period = Integer.valueOf(period);
             return this;
+        }
+
+        public List<Integer> getCardTypes() {
+            return cardTypes;
+        }
+
+        public Integer getPeriod() {
+            return period;
         }
     }
 }
