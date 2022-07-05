1375432d49e2e52150b7f86562da16d36fd9c888
Artem Voinov <voynov@i-teco.ru>
2022-07-05 12:57:55 +0300

    Release v290

--- processor/src/main/resources/db/update_2.2.289-2.2.290.sql
+++ processor/src/main/resources/db/update_2.2.289-2.2.290.sql
@@ -1,5 +1,12 @@
--- Пакет обновлений issue 1013
+-- Пакет обновлений v 290
 
+--962
+CREATE INDEX cf_log_services_createddate_idx
+    ON cf_log_services USING btree(createddate)
+
+--1013
 insert into cf_clientgroups(idoforg, idofclientgroup, groupname)
 select t.idoforg, 1100000060, 'Выбывшие' from cf_orgs t
 where not exists(select * from cf_clientgroups cg where cg.idoforg = t.idoforg and cg.idofclientgroup = 1100000060);
+
+--! ФИНАЛИЗИРОВАН 05.07.2022, НЕ МЕНЯТЬ
