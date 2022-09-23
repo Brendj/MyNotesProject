46ac067f2410043bc3497abf1cd5d67ef87c699d
semenov <semenov@axetta.ru>
2022-09-23 16:37:24 +0300

    https://yt.iteco.dev/issue/ISPP-1118 Получить идентификаторы СУДИР представителей обучающегося. Проактив МоС, п.3.3 БФТ

---
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/proactive/service/AupdNotExistsException.java
@@ -0,0 +1,10 @@
+package ru.axetta.ecafe.processor.core.proactive.service;
+
+public class AupdNotExistsException extends Exception {
+    public AupdNotExistsException() {
+    }
+
+    public AupdNotExistsException(String message) {
+        super(message);
+    }
+}
