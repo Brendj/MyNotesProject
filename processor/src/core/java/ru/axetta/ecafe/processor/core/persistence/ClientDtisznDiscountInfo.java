cce0219750ffba8da1a0fcbf58b05df4adba4a30
semenov <semenov@axetta.ru>
2022-09-29 15:08:35 +0300

    https://yt.iteco.dev/issue/ISPP-1057 доработки по синхре секции ClientDiscountsDTSZN

--- processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/ClientDtisznDiscountInfo.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/ClientDtisznDiscountInfo.java
@@ -53,12 +53,18 @@ public class ClientDtisznDiscountInfo implements Comparable {
     @Override
     public int compareTo(Object o) {
         ClientDtisznDiscountInfo item = (ClientDtisznDiscountInfo) o;
+        if (this.isArchivedNullSafe() && !item.isArchivedNullSafe()) return -1;
+        if (!this.isArchivedNullSafe() && item.isArchivedNullSafe()) return 1;
         int res = this.dateEnd.compareTo(item.getDateEnd());
         if (res == 0) {
             return DiscountManager.getDiscountPriority(this.dtisznCode).compareTo(item.getDtisznCode().intValue());
         } else {
             return res;
         }
+    }
+
+    public boolean isArchivedNullSafe() {
+        return this.archived != null && this.archived;
     }
 
     @Override
