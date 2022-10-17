59e2de35df1230ea18a97663b5b93f6277c338c0
tingaev <v.tingaev@iteco.mobi>
2022-10-17 17:34:43 +0300

    Решение по задаче https://yt.iteco.dev/issue/ISPP-1275 Исправлена конвертация номера

--- processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/Client.java
+++ processor/src/core/java/ru/axetta/ecafe/processor/core/persistence/Client.java
@@ -345,7 +345,7 @@ public class Client {
             return mobilePhone;
         }
         mobilePhone = mobilePhone.replaceAll("[+ \\-()]", "");
-        if (mobilePhone.startsWith("8")) {
+        if (mobilePhone.startsWith("8") && mobilePhone.length() != 10) {
             mobilePhone = "7" + mobilePhone.substring(1);
         }
         if (mobilePhone.length() == 10) {
