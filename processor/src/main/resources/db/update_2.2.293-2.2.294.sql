ce40ab96001aca756f09b88a213443a5b73290c9
Artem Voinov <voynov@i-teco.ru>
2022-10-27 16:24:03 +0300

    Добавлены миграции по задаче 1009

--- processor/src/main/resources/db/update_2.2.293-2.2.294.sql
+++ processor/src/main/resources/db/update_2.2.293-2.2.294.sql
@@ -9,4 +9,24 @@ COMMENT ON COLUMN cf_client_dtiszn_discount_info.updated_at
 
 ALTER TABLE cf_orgs ADD COLUMN disableSocCardsReg BOOLEAN NOT NULL DEFAULT TRUE;
 
+--1009
+CREATE SEQUENCE public.cf_foodbox_org_lock_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    START 1;
+
+CREATE TABLE public.cf_foodbox_org_lock (
+   orglockid int8 DEFAULT nextval('cf_foodbox_org_lock_seq') NOT NULL,
+   idoforg int8 NULL,
+   "version" int8 NULL,
+   currentversion int8 NULL,
+   CONSTRAINT org_lock_pk PRIMARY KEY (orglockid)
+);
+
+COMMENT ON TABLE public.cf_foodbox_org_lock IS 'Таблица синхронизации параллельных заказов фудбокса';
+COMMENT ON COLUMN public.cf_foodbox_org_lock.idoforg IS 'Идентификатор организации (ссылка на cf_orgs)';
+COMMENT ON COLUMN public.cf_foodbox_org_lock."version" IS 'Номер последнего заказа в очереди на исполнение';
+COMMENT ON COLUMN public.cf_foodbox_org_lock.currentversion IS 'Номер последнего исполненного заказа';
+
 --! ФИНАЛИЗИРОВАН 27.10.2022, НЕ МЕНЯТЬ
