110416ca0054b967ccecf212874397b163ec57fe
semenov <semenov@axetta.ru>
2022-11-07 15:55:08 +0300

    Release v295

--- processor/src/main/resources/db/update_2.2.294-2.2.295.sql
+++ processor/src/main/resources/db/update_2.2.294-2.2.295.sql
@@ -105,3 +105,15 @@ CREATE INDEX cf_mezhved_response_document_id_idx ON public.cf_mezhved_response_d
 
 alter table cf_client_dtiszn_discount_info
   add column appointedMSP integer;
+
+  CREATE TABLE cf_mezhved_kafka_error (
+    idofmezhvedkafkaerror bigserial NOT NULL,
+    msg varchar NOT NULL,
+    topic varchar NOT NULL,
+    error varchar NOT NULL,
+    idofapplicationforfood int8 NOT NULL,
+    "type" int4 NULL,
+    createdate int8 NULL,
+    updatedate int8 NULL,
+    CONSTRAINT ccf_mezhved_kafka_error_pk PRIMARY KEY (idofmezhvedkafkaerror)
+);
